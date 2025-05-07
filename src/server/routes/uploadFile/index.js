ddbb3482440bbfbeab9088c942a6004d33cc6624
asker <askersultanov666@gmail.com>
2025-05-07 20:37:49 +0300

    add storage

--- src/server/routes/uploadFile/index.js
+++ src/server/routes/uploadFile/index.js
@@ -1,4 +1,14 @@
+var multer = require("multer");
 var { Router } = require("express");
+
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "/var/report_uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
 var router = Router();
 
