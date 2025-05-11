c54e29a423cf3a60b39034a0d26b5acbec99bc04
asker <askersultanov666@gmail.com>
2025-05-11 15:07:41 +0300

    rm try catch in getReportData

--- src/server/routes/root/controllers/getReportData.js
+++ src/server/routes/root/controllers/getReportData.js
@@ -1,16 +1,12 @@
 var getXLSXData = require("../services/getXLSXData");
 
 var getReportData = async (req, res, next) => {
-  try {
-    var filePath =
-      "/home/phosphorus/financial-report/wb_report_7-5-2025_14-2.xlsx";
+  var filePath =
+    "/home/phosphorus/financial-report/wb_report_7-5-2025_14-2.xlsx";
 
-    var data = await getXLSXData(filePath);
+  var data = await getXLSXData(filePath);
 
-    return res.json({ data });
-  } catch (e) {
-    next(e);
-  }
+  return res.json({ data });
 };
 
 module.exports = getReportData;
