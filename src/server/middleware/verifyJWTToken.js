c2dd795826b1c2d99daafed774663e6e53e76461
asker <askersultanov666@gmail.com>
2025-05-11 15:31:49 +0300

    add npm cookie-parser, jsonwebtoken &&  func verifyJWTToken

---
+++ src/server/middleware/verifyJWTToken.js
@@ -0,0 +1,25 @@
+var JWT = require("jsonwebtoken");
+
+var verifyJWTToken = async (req, res, next) => {
+  try {
+    var token = req.cookies?.token;
+
+    if (!token) {
+      return res.sendFile(
+        join(__dirname, "../../public/html/auth/authForm.html")
+      );
+    }
+
+    var user = JWT.verify(token, env.secretKey);
+
+    if (user.role == "user") {
+      return next();
+    }
+
+    return next();
+  } catch (e) {
+    res.clearCookie("token");
+    next(e);
+  }
+};
+export default verifyJWTToken;
