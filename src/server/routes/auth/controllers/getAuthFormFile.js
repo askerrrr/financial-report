a6316260dc3bbe1a5e1193bdfc6b7faa16c9adb5
asker <askersultanov666@gmail.com>
2025-05-11 15:06:19 +0300

    rm try catch in getAuthFormFile

--- src/server/routes/auth/controllers/getAuthFormFile.js
+++ src/server/routes/auth/controllers/getAuthFormFile.js
@@ -1,13 +1,6 @@
 var { join } = require("node:path");
 
-var getAuthFormFile = async (req, res, next) => {
-  try {
-    return res.sendFile(
-      join(__dirname, "../../../../public/html/auth/authForm.html")
-    );
-  } catch (e) {
-    next(e);
-  }
-};
+var getAuthFormFile = async (req, res, next) =>
+  res.sendFile(join(__dirname, "../../../../public/html/auth/authForm.html"));
 
 module.exports = getAuthFormFile;
