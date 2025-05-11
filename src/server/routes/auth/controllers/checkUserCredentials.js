096e6b9d0b4813dc660cee18fd100c9b5a5f3921
asker <askersultanov666@gmail.com>
2025-05-11 15:06:43 +0300

    rm try catch in checkUserCredentials

--- src/server/routes/auth/controllers/checkUserCredentials.js
+++ src/server/routes/auth/controllers/checkUserCredentials.js
@@ -3,25 +3,21 @@ var checkCredentials = require("../services/checkCredentials");
 var checkUserCredentials = async (req, res, next) => {
   var { getUser } = req.app.locals.userCollectionServices();
 
-  try {
-    var userData = req.body;
+  var userData = req.body;
 
-    var userDataFromDB = await getUser(userData.login);
+  var userDataFromDB = await getUser(userData.login);
 
-    if (!userDataFromDB) {
-      return res.sendStatus(404);
-    }
-
-    var successAuth = await checkCredentials(userData, userDataFromDB);
-
-    if (!successAuth) {
-      return res.sendStatus(403);
-    }
-
-    return res.status(200).json({ redirectUrl: "/" });
-  } catch (e) {
-    next(e);
+  if (!userDataFromDB) {
+    return res.sendStatus(404);
   }
+
+  var successAuth = await checkCredentials(userData, userDataFromDB);
+
+  if (!successAuth) {
+    return res.sendStatus(403);
+  }
+
+  return res.status(200).json({ redirectUrl: "/" });
 };
 
 module.exports = checkUserCredentials;
