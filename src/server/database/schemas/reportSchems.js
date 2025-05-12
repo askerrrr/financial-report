65073a18d9009685f8f0af64bf78387e914b89bd
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-12 14:59:30 +0300

    add report schema

---
+++ src/server/database/schemas/reportSchems.js
@@ -0,0 +1,38 @@
+var { Schema } = require("mongoose");
+
+var numOptions = [
+  {
+    type: Number,
+    required: true,
+  },
+];
+
+var strOptions = [
+  {
+    type: String,
+    required: true,
+  },
+];
+
+var reportSchema = new Schema({
+  itemName: strOptions,
+  article: strOptions,
+  WBSalesAmount: numOptions,
+  qty: numOptions,
+  buyoutPrice: numOptions,
+  deliveryCost: numOptions,
+  refundCost: numOptions,
+  numberOfReturns: numOptions,
+  files: numOptions,
+  allowances,
+  summaryData: {
+    totalSum: { type: Number, required: true },
+    storageCost: { type: Number, required: true },
+    payoutsPerProduct: numOptions,
+    differentDeductions: { type: Number, required: true },
+    paidAcceptanceOfGoods: { type: Number, required: true },
+  },
+  averageCost: numOptions,
+});
+
+module.exports = reportSchema;
