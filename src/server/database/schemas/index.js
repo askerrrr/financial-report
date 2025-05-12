65f10f019436b6e57f06654ee9099fa91a5e9943
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-12 15:18:55 +0300

    add userId in schema

--- src/server/database/schemas/index.js
+++ src/server/database/schemas/index.js
@@ -2,6 +2,7 @@ var { Schema } = require("mongoose");
 var reportSchema = require("./reportSchems");
 
 var schema = new Schema({
+  userId: { type: String, required: true },
   login: { type: String, required: true },
   passwd: { type: String, required: true },
   reports: { type: [reportSchema], required: false },
