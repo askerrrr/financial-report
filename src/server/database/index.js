2ae53d14b78c0f19c256790ff11b127346cb8474
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-12 15:54:30 +0300

    add createReport in userCollectionService

--- src/server/database/index.js
+++ src/server/database/index.js
@@ -1,12 +1,14 @@
 var collection = require("./connection/index");
 
-var createUser = require("./services/createUser");
 var getUser = require("./services/getUser");
+var createUser = require("./services/createUser");
+var createReport = require("./services/createReport");
 
 var userCollectionServices = () => {
   return {
     createUser: (data) => createUser(collection, data),
     getUser: (login) => getUser(collection, login),
+    createReport: (userId, report) => createReport(collection, userId, report),
   };
 };
 
