b98b4d8e821e49ad0993e05c456fe13385240b44
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-13 14:43:44 +0300

    import userSchema

--- src/server/database/connection/index.js
+++ src/server/database/connection/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-var reportSchema = require("../schemas/index");
+var { userSchema, reportSchema } = require("../schemas/index");
 
 var reportDB = mongoose.createConnection("mongodb://127.0.0.1:27017/reports");
 
