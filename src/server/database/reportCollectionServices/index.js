23f54e60d0f4cc14a74d42be1b2c26c26581496f
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-13 15:35:48 +0300

    added services as property

--- src/server/database/reportCollectionServices/index.js
+++ src/server/database/reportCollectionServices/index.js
@@ -3,4 +3,12 @@ var { reportCollection } = require("../connection/index");
 var createReport = require("./services/createReport");
 var createReportsEntity = require("./services/createReportsEntity");
 
-var reportCollectionServices = async () => {};
+var reportCollectionServices = async () => {
+  return {
+    createReport: (userId, report) =>
+      createReport(reportCollection, userId, report),
+
+    createReportsEntity: (userId) =>
+      createReportsEntity(reportCollection, userId),
+  };
+};
