62b967dedad3720e7735fe8737bdba5c239f41bf
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-13 14:44:39 +0300

    added userCollection

--- src/server/database/connection/index.js
+++ src/server/database/connection/index.js
@@ -3,6 +3,8 @@ var { userSchema, reportSchema } = require("../schemas/index");
 
 var reportDB = mongoose.createConnection("mongodb://127.0.0.1:27017/reports");
 
+var userCollection = reportDB.model("User", userSchema);
+
 var reportCollection = reportDB.model("Report", reportSchema);
 
 module.exports = reportCollection;
