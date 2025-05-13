e76ba20e81be611ab0b626cd465f840abbae949e
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-13 14:42:38 +0300

    added userSchema

--- src/server/database/schemas/index.js
+++ src/server/database/schemas/index.js
@@ -1,6 +1,13 @@
 var { Schema } = require("mongoose");
 var reportSchema = require("./reportSchems");
 
+var userSchema = new Schema({
+  userId: { type: String, required: true },
+  login: { type: String, required: true },
+  passwd: { type: String, required: true },
+  reports: { type: [reportSchema], required: false },
+});
+
 var reportSchema = new Schema({
   userId: { type: String, required: true },
   reports: { type: [reportSchema], required: false },
