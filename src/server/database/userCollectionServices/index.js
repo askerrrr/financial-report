a1c9d803a125c585c8bbb0c10431d32d88424061
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-13 15:26:46 +0300

    rm report services from userCollectionServices

--- src/server/database/userCollectionServices/index.js
+++ src/server/database/userCollectionServices/index.js
@@ -1,18 +1,12 @@
-var { userCollection, reportCollection } = require("../connection/index");
+var { userCollection } = require("../connection/index");
 
 var getUser = require("./services/getUser");
 var createUser = require("./services/createUser");
-var createReport = require("./services/createReport");
-var createReportsEntity = require("./services/createReportsEntity");
 
 var userCollectionServices = () => {
   return {
     createUser: (data) => createUser(userCollection, data),
     getUser: (login) => getUser(userCollection, login),
-    createReport: (userId, report) =>
-      createReport(reportCollection, userId, report),
-    createReportsEntity: (userId) =>
-      createReportsEntity(reportCollection, userId),
   };
 };
 
