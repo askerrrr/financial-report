9415f191b218681a36a824899e1588a402faf248
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-13 15:37:55 +0300

    import reportCollectionServices

--- src/server/app.js
+++ src/server/app.js
@@ -3,6 +3,7 @@ var { join } = require("node:path");
 var { mkdir } = require("node:fs/promises");
 var cookieParser = require("cookie-parser");
 var userCollectionServices = require("./database/userCollectionServices/index");
+var reportCollectionServices = require("./database/reportCollectionServices/index");
 
 var app = express();
 
