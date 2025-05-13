2e9ff40eb4b5202564a9886d9ce4e768c7cb12ca
asker <askersultanov666@gmail.com>
2025-05-13 19:22:48 +0300

    added _id: false by reportDetailSchema

--- src/server/database/schemas/reportSchems.js
+++ src/server/database/schemas/reportSchems.js
@@ -14,25 +14,28 @@ var strOptions = [
   },
 ];
 
-var reportDetailSchema = new Schema({
-  itemName: strOptions,
-  article: strOptions,
-  WBSalesAmount: numOptions,
-  qty: numOptions,
-  buyoutPrice: numOptions,
-  deliveryCost: numOptions,
-  refundCost: numOptions,
-  numberOfReturns: numOptions,
-  files: numOptions,
-  allowances: numOptions,
-  summaryData: {
-    totalSum: { type: Number, required: true },
-    storageCost: { type: Number, required: true },
-    payoutsPerProduct: numOptions,
-    differentDeductions: { type: Number, required: true },
-    paidAcceptanceOfGoods: { type: Number, required: true },
+var reportDetailSchema = new Schema(
+  {
+    itemName: strOptions,
+    article: strOptions,
+    WBSalesAmount: numOptions,
+    qty: numOptions,
+    buyoutPrice: numOptions,
+    deliveryCost: numOptions,
+    refundCost: numOptions,
+    numberOfReturns: numOptions,
+    files: numOptions,
+    allowances: numOptions,
+    summaryData: {
+      totalSum: { type: Number, required: true },
+      storageCost: { type: Number, required: true },
+      payoutsPerProduct: numOptions,
+      differentDeductions: { type: Number, required: true },
+      paidAcceptanceOfGoods: { type: Number, required: true },
+    },
+    averageCost: numOptions,
   },
-  averageCost: numOptions,
-});
+  { _id: false }
+);
 
 module.exports = reportDetailSchema;
