e7180eb52d91370d0976898a39de31f2c4bbc87c
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-13 13:07:09 +0300

    added a report record to the database

--- src/server/routes/uploadFile/controllers/writeReport.js
+++ src/server/routes/uploadFile/controllers/writeReport.js
@@ -8,7 +8,13 @@ var writeReport = async (req, res, next) => {
   if (req.file) {
     var data = await getXLSXData(req.file.path);
 
-    console.log("data: ", data);
+    var reportSuccessfullyRecorded = await createReport(userId, data);
+
+    if (!reportSuccessfullyRecorded) {
+      return res
+        .status(500)
+        .json({ msg: "Возникла ошибка при загрузке файла" });
+    }
 
     res.status(200).json({ msg: "success upload" });
   }
