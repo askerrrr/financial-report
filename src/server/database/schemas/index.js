27bbcc9903d5f13651fe62b7d1c64e2df0b02e86
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-13 14:41:18 +0300

    rm login, passwd from reportSchema

--- src/server/database/schemas/index.js
+++ src/server/database/schemas/index.js
@@ -3,8 +3,6 @@ var reportSchema = require("./reportSchems");
 
 var reportSchema = new Schema({
   userId: { type: String, required: true },
-  login: { type: String, required: true },
-  passwd: { type: String, required: true },
   reports: { type: [reportSchema], required: false },
 });
 
