c0749d979da86932d08cda3aa9ee37d7ed49e93b
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-13 13:47:04 +0300

    added login and password hashing

--- src/server/database/services/createUser.js
+++ src/server/database/services/createUser.js
@@ -1,5 +1,11 @@
-var createUser = async (collection, { login, passwd, userId }) => {
+var argon2 = require("argon2");
+
+var createUser = async (collection, userData) => {
   try {
+    var userId = userData.userId;
+    var login = await argon2.hash(userData.login, "secret");
+    var passwd = await argon2.hash(userData.passwd, "secret");
+
     var user = await collection.insertOne({ login, passwd, userId });
 
     var result = await user.save();
