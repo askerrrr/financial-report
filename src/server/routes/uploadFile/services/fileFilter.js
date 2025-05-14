8777fc1afb9015c275a4d76198630257387be958
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-14 14:53:46 +0300

    added FileMimeTypeError throw

--- src/server/routes/uploadFile/services/fileFilter.js
+++ src/server/routes/uploadFile/services/fileFilter.js
@@ -1,5 +1,12 @@
-module.exports = (req, file, cb) =>
-  file.mimetype ==
-  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    ? cb(null, true)
-    : cb(null, false);
+const { FileMimeTypeError } = require("../../../customError/customError");
+
+module.exports = (req, file, cb) => {
+  var validMimeType =
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+  if (file.mimetype == validMimeType) {
+    return cb(null, true);
+  }
+
+  throw new FileMimeTypeError();
+};
