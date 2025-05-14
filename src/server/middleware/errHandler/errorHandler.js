a4c2c149255067f487a9c192887af964a395f3d8
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-14 14:49:40 +0300

    added FileMimeTypeError handling

--- src/server/middleware/errHandler/errorHandler.js
+++ src/server/middleware/errHandler/errorHandler.js
@@ -1,9 +1,14 @@
 var multer = require("multer");
+var { FileMimeTypeError } = require("../../customError/customError");
 
 var errorHandler = async (e, req, res, next) => {
   if (e instanceof multer.MulterError) {
     console.log("MulterError ", e);
     return res.sendStatus(500);
+  }
+
+  if (e instanceof FileMimeTypeError) {
+    return res.status(500).json({ msg: "Некорректный тип файла" });
   }
 
   console.log("AppError: ", e);
