37124c008b645de271558e359375549f116a42e0
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-15 14:19:45 +0300

    added DatabaseError

--- src/server/customError/customError.js
+++ src/server/customError/customError.js
@@ -1,1 +1,12 @@
-module.exports = {};
+class DatabaseError extends Error {
+  constructor(userId, e, message) {
+    super(message);
+    this.userId = userId;
+    this.cause = e?.cause ?? "";
+    this.stack = e?.stack ?? "";
+    this.message = e?.message || message;
+    this.name = this.constructor.name;
+  }
+}
+
+module.exports = { DatabaseError };
