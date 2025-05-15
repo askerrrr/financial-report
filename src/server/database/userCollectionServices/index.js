52d597302a3104e77964554aa647e39014e9369b
asker <askersultanov666@gmail.com>
2025-05-15 21:46:46 +0300

    added getUserById as userCollectionServices property

--- src/server/database/userCollectionServices/index.js
+++ src/server/database/userCollectionServices/index.js
@@ -2,13 +2,18 @@ var { userCollection } = require("../connection/index");
 
 var getUser = require("./services/getUser");
 var createUser = require("./services/createUser");
+var getUserById = require("./services/getUserByUserId");
 var getReportsByUserId = require("./services/getReportsByUserId");
 
 var userCollectionServices = () => {
   return {
     createUser: (data, reportsEntityObjectId) =>
       createUser(userCollection, data, reportsEntityObjectId),
+
     getUser: (login) => getUser(userCollection, login),
+
+    getUserById: (userId) => getUserById(userCollection, userId),
+
     getReportsByUserId: (userId) => getReportsByUserId(userCollection, userId),
   };
 };
