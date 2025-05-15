e406833fc73897b26e73889c3e801bb97fe841b1
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-15 14:16:44 +0300

    rm FileMimeTypeError

--- src/server/customError/customError.js
+++ src/server/customError/customError.js
@@ -1,9 +1,1 @@
-class FileMimeTypeError extends Error {
-  constructor(message) {
-    super(message);
-
-    this.message = "Invalid file mimetype";
-  }
-}
-
-module.exports = { FileMimeTypeError };
+module.exports = {};
