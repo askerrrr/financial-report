95bc91b4c2714440d383181db7dc11984d5be8b8
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-16 09:59:46 +0300

    relocated getReportsByUserId: userCollectionSchema => reportCollectionSchema

--- src/server/database/userCollectionServices/index.js
+++ src/server/database/userCollectionServices/index.js
@@ -3,7 +3,6 @@ var { userCollection } = require("../connection/index");
 var getUser = require("./services/getUser");
 var createUser = require("./services/createUser");
 var getUserById = require("./services/getUserByUserId");
-var getReportsByUserId = require("./services/getReportsByUserId");
 
 var userCollectionServices = () => {
   return {
@@ -13,8 +12,6 @@ var userCollectionServices = () => {
     getUser: (login) => getUser(userCollection, login),
 
     getUserById: (userId) => getUserById(userCollection, userId),
-
-    getReportsByUserId: (userId) => getReportsByUserId(userCollection, userId),
   };
 };
 
