67480b2b6abc8f3e75dd0449bba34e8b6349b657
asker <askersultanov666@gmail.com>
2025-05-17 17:22:11 +0300

    search report by id, return report

--- src/server/database/reportCollectionServices/services/getReportById.js
+++ src/server/database/reportCollectionServices/services/getReportById.js
@@ -2,6 +2,10 @@ var getReportById = async (collection, userId, id) => {
   var data = await collection.findOne({ userId, "reports.id": id });
 
   var reports = data.reports;
+
+  var report = reports.find((report) => report.id == id);
+
+  return report;
 };
 
 module.exports = getReportById;
