15f8acae0bb9ae1345d5b9e4c8ed168ff0fa77cc
asker <askersultanov666@gmail.com>
2025-05-17 16:02:23 +0300

    getting reportsDto in getReportsDto

--- src/server/routes/root/services/getReportsDto.js
+++ src/server/routes/root/services/getReportsDto.js
@@ -1,1 +1,13 @@
-var getReportsDto = async (data) => {};
+var getReportsDto = async (reports) => {
+  var data = [];
+
+  for (var report of reports) {
+    data.push({
+      id: report.id,
+      date: report.date,
+      period: report.period ?? "",
+    });
+  }
+
+  return data;  
+};
