00378e58e458cb367e52ff1a5973a80782690122
asker <askersultanov666@gmail.com>
2025-05-17 11:18:46 +0300

    in the getReportsData function, sending data via res.json() has been added

--- src/server/routes/root/controllers/getReportsData.js
+++ src/server/routes/root/controllers/getReportsData.js
@@ -2,6 +2,10 @@ var getReportsData = async (req, res, next) => {
   var userId = req.app.locals.userId;
 
   var { getReportsByUserId } = req.app.locals.reportCollectionServices();
+
+  var reports = await getReportsByUserId(userId);
+
+  return res.json({ reports });
 };
 
 module.exports = getReportsData;
