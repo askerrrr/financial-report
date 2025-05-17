2c2519a220c5e23739371f95d1afb74d1ea8882a
asker <askersultanov666@gmail.com>
2025-05-17 19:01:35 +0300

    added path to get report.html

--- src/server/routes/reports/index.js
+++ src/server/routes/reports/index.js
@@ -2,6 +2,8 @@ var { Router } = require("express");
 
 var router = Router({ caseSensitive: true, strict: true });
 
+router.get("./:id", require("./controllers/getReportHTML"));
+
 router.get("/:userId/:id", require("./controllers/getReport"));
 
 module.exports = router;
