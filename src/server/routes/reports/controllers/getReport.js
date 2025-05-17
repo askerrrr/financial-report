e250ff3c147d22e97f900fd64e1f9a7a7323343e
asker <askersultanov666@gmail.com>
2025-05-17 17:25:49 +0300

    extracted getReportById from req.app.locals.reportCollectionServices()

--- src/server/routes/reports/controllers/getReport.js
+++ src/server/routes/reports/controllers/getReport.js
@@ -1,5 +1,7 @@
 var getReport = async (req, res, next) => {
   var { userId, id } = req.pamams;
+
+  var { getReportById } = req.app.locals.reportCollectionServices();
 };
 
 module.exports = getReport;
