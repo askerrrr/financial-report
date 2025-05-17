4dccc0afd1d6094de35d44e0c1fd31e2c1838657
asker <askersultanov666@gmail.com>
2025-05-17 17:13:47 +0300

    added rout path for /reports

--- src/server/routes/reports/index.js
+++ src/server/routes/reports/index.js
@@ -2,4 +2,6 @@ var { Router } = require("express");
 
 var router = Router({ caseSensitive: true, strict: true });
 
+router.get("/:userId/:id", require("./controllers/getReport"));
+
 module.exports = router;
