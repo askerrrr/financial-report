4bf5fd4bd3cb5c13972537b4f3207782850dc132
asker <askersultanov666@gmail.com>
2025-05-17 14:25:24 +0300

    imported getReportCreationDate into createRowForReports

--- src/public/js/index/row/createRowForReports.js
+++ src/public/js/index/row/createRowForReports.js
@@ -1,5 +1,6 @@
 import getReportPeriod from "./services/getReportPeriod.js";
 import createLinkToTheReport from "./services/createLinkToTheReport.js";
+import getReportCreationDate from "./services/getReportCreationDate.js";
 
 var table = document.getElementById("reports");
 var tbody = document.createElement("tbody");
