c7116e1b77ba94ff2f667a20b92bb89d398bef5d
asker <askersultanov666@gmail.com>
2025-05-17 14:11:33 +0300

    getting table using document.getElementById

--- src/public/js/index/row/createRowForReports.js
+++ src/public/js/index/row/createRowForReports.js
@@ -1,5 +1,7 @@
 import createLinkToTheReport from "./services/createLinkToTheReport.js";
 
+var table = document.getElementById("reports");
+
 var createRowForReports = async (reports) => {
   for (var report of reports) {
   }
