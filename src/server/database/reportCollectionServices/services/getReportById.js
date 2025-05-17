eed34ae4eecc46c46a783ddd4245b1202a810a65
asker <askersultanov666@gmail.com>
2025-05-17 17:20:57 +0300

    getting reports from data.reports into

--- src/server/database/reportCollectionServices/services/getReportById.js
+++ src/server/database/reportCollectionServices/services/getReportById.js
@@ -1,5 +1,7 @@
 var getReportById = async (collection, userId, id) => {
   var data = await collection.findOne({ userId, "reports.id": id });
+
+  var reports = data.reports;
 };
 
 module.exports = getReportById;
