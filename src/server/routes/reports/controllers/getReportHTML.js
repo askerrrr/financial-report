cdaa70b1e28d4e1a53a51eb8393f2f1aa24fb235
asker <askersultanov666@gmail.com>
2025-05-17 19:00:43 +0300

    exported getReportHTML

--- src/server/routes/reports/controllers/getReportHTML.js
+++ src/server/routes/reports/controllers/getReportHTML.js
@@ -3,3 +3,5 @@ var { join } = require("node:path");
 var getReportHTML = async (req, res, next) => {
   res.sendFile(join(__dirname, "../../../../public/report/report.html"));
 };
+
+module.exports = getReportHTML;
