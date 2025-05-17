b516971ef854cda0c8680e8c002eb241b678b67f
asker <askersultanov666@gmail.com>
2025-05-17 14:33:55 +0300

    added getting linkToTheReport from createLinkToTheReport into getReportCreationDate

--- src/public/js/index/row/services/getReportCreationDate.js
+++ src/public/js/index/row/services/getReportCreationDate.js
@@ -3,7 +3,7 @@ import createLinkToTheReport from "./createLinkToTheReport.js";
 var td = document.createElement("td");
 
 var getReportCreationDate = async (id, date) => {
-  td.append(date);
+  var linkToTheReport = await createLinkToTheReport(id, date);
 
   return td;
 };
