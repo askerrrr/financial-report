5a5e5b2a23032c66ce782e4388c8ebb1410fa351
asker <askersultanov666@gmail.com>
2025-05-17 19:00:19 +0300

    added sending html file to getReport

--- src/server/routes/reports/controllers/getReportHTML.js
+++ src/server/routes/reports/controllers/getReportHTML.js
@@ -1,3 +1,5 @@
 var { join } = require("node:path");
 
-var getReportHTML = async (req, res, next) => {};
+var getReportHTML = async (req, res, next) => {
+  res.sendFile(join(__dirname, "../../../../public/report/report.html"));
+};
