d5a12a694b5cc049f3aded1c4d32c4ff48cecf6f
asker <askersultanov666@gmail.com>
2025-05-17 14:28:28 +0300

    refactoring createLinkToTheReport, rm td, return form

--- src/public/js/index/row/services/createLinkToTheReport.js
+++ src/public/js/index/row/services/createLinkToTheReport.js
@@ -1,6 +1,5 @@
 var button = document.createElement("button");
 var form = document.createElement("form");
-var td = document.createElement("td");
 
 var createLinkToTheReport = async (id) => {
   button.append(id);
@@ -8,9 +7,7 @@ var createLinkToTheReport = async (id) => {
   form.append(button);
   form.action = "/reports/" + id;
 
-  td.append(form);
-
-  return td;
+  return form;
 };
 
 export default createLinkToTheReport;
