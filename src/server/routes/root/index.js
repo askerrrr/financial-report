43140aa067e6ad5a7d6452456c73e82a260686d2
asker <askersultanov666@gmail.com>
2025-05-17 11:22:49 +0300

    added userId parameter for path /api, root router

--- src/server/routes/root/index.js
+++ src/server/routes/root/index.js
@@ -3,6 +3,6 @@ var { Router } = require("express");
 var router = Router();
 
 router.get("/", require("./controllers/getIndexHTML"));
-router.get("/api", require("./controllers/getReportsData"));
+router.get("/api/:userId", require("./controllers/getReportsData"));
 
 module.exports = router;
