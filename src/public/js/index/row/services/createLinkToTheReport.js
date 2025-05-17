1ff9853a5f05d89c44d3cdb17a3f40ed7b696e12
asker <askersultanov666@gmail.com>
2025-05-17 14:07:34 +0300

    create link with form, add form to td, return td

--- src/public/js/index/row/services/createLinkToTheReport.js
+++ src/public/js/index/row/services/createLinkToTheReport.js
@@ -1,4 +1,14 @@
 var button = document.createElement("button");
 var form = document.createElement("form");
+var td = document.createElement("td");
 
-var createLinkToTheReport = async (id) => {};
+var createLinkToTheReport = async (id) => {
+  button.append(id);
+
+  form.append(button);
+  form.action = "/reports/" + id;
+
+  td.append(form);
+
+  return td;
+};
