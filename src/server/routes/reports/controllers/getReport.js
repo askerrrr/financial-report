f02221e79ebb40bc11d09283ce3dfd9e6db2a516
asker <askersultanov666@gmail.com>
2025-05-17 17:27:07 +0300

    getting report with getReportById and sending with res.json

--- src/server/routes/reports/controllers/getReport.js
+++ src/server/routes/reports/controllers/getReport.js
@@ -2,6 +2,10 @@ var getReport = async (req, res, next) => {
   var { userId, id } = req.pamams;
 
   var { getReportById } = req.app.locals.reportCollectionServices();
+
+  var report = await getReportById(userId, id);
+
+  res.json({ report });
 };
 
 module.exports = getReport;
