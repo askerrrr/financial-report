af70645c1eee9de3811e6ac50e7fcd3bae79023e
asker <askersultanov666@gmail.com>
2025-05-17 14:00:48 +0300

    create button, form into createLinkToTheReport

--- src/public/js/index/row/services/createLinkToTheReport.js
+++ src/public/js/index/row/services/createLinkToTheReport.js
@@ -1,1 +1,4 @@
-var createLinkToTheReport = async () => {};
+var button = document.createElement("button");
+var form = document.createElement("form");
+
+var createLinkToTheReport = async (id) => {};
