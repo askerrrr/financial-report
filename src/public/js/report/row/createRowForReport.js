d4c0ec24ddad68f1f04079c2103a5c40afe4629f
asker <askersultanov666@gmail.com>
2025-05-18 12:17:30 +0300

    getting reportId by window.location.pathname

--- src/public/js/report/row/createRowForReport.js
+++ src/public/js/report/row/createRowForReport.js
@@ -1,5 +1,9 @@
 var userId = document.cookie.split("=")[1];
 
+var pathParts = window.location.pathname.split("/");
+
+var reportId = pathParts.at(-1);
+
 var getReportData = async (id) => await fetch();
 
 var createRowForReport = async (report) => {};
