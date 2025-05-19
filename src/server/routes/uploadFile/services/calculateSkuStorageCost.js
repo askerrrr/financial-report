281dbbd59bc28a75927f22012f43be4161e6934e
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-19 15:51:28 +0300

    Add SKU storage cost calculation

--- src/server/routes/uploadFile/services/calculateSkuStorageCost.js
+++ src/server/routes/uploadFile/services/calculateSkuStorageCost.js
@@ -1,1 +1,11 @@
-var calculateSkuStorageCost = async (data) => {};
+var calculateSkuStorageCost = async (data) => {
+  var skuStorageCost = data.qty.map((qty) => {
+    var result = (data.summaryData.storageCost / data.totalProducts) * qty + "";
+
+    return +result.slice(0, 5);
+  });
+
+  data.skuStorageCost = skuStorageCost;
+
+  return data;
+};
