d581717fb2121314b3d82f4f4d1b76b09b944d7c
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-19 16:02:44 +0300

    added shorteningStr

--- src/server/routes/uploadFile/services/calculateSkuStorageCost.js
+++ src/server/routes/uploadFile/services/calculateSkuStorageCost.js
@@ -1,8 +1,16 @@
+var shorteningStr = (str) => {
+  var [start, end] = str.split(".");
+
+  return start + "." + end.slice(0, 3);
+};
+
 var calculateSkuStorageCost = async (data) => {
   var skuStorageCost = data.qty.map((qty) => {
     var result = (data.summaryData.storageCost / data.totalProducts) * qty + "";
 
-    return +result.slice(0, 5);
+    var cost = shorteningStr(result);
+
+    return cost;
   });
 
   data.skuStorageCost = skuStorageCost;
