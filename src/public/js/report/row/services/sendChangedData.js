185d8dda741b60d192541254cb8573f5a466a118
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-23 14:33:03 +0300

    added userid & reportId into body

--- src/public/js/report/row/services/sendChangedData.js
+++ src/public/js/report/row/services/sendChangedData.js
@@ -3,12 +3,12 @@ import getCookieByName from "../../../index/services/getCookieByName.js";
 var userId = await getCookieByName("userId");
 var reportId = window.location.pathname.split("/").at(-1); // await getCookieByName("reportId");
 
-var url = "/reports/" + userId + "/" + reportId;
+var url = "/reports/change";
 
 var sendChangedData = async (data, index, fieldName) => {
   var res = await fetch(url, {
     method: "POST",
-    body: JSON.stringify({ data, index, fieldName }),
+    body: JSON.stringify({ userId, reportId, data, index, fieldName }),
     headers: { "Content-Type": "application/json" },
   });
 
