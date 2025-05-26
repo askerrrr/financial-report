6ec9cace2bbd5cf0da10a3d2907b40e860939287
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-26 15:12:03 +0300

    element change implemented in updateItems

--- src/server/database/reportCollectionServices/services/updateItems.js
+++ src/server/database/reportCollectionServices/services/updateItems.js
@@ -1,3 +1,14 @@
-var updateItems = async (collection, userId, reportId, items) => {};
+var updateItems = async (collection, userId, reportId, items) => {
+  var result = await collection.updateOne(
+    { userId, "reports.id": reportId },
+    {
+      $set: {
+        "reports.$.items": items,
+      },
+    }
+  );
+
+  return result.modifiedCount;
+};
 
 module.exports = updateItems;
