d626bfb7428dc39216270e8fd5091550d80f348d
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-30 14:02:10 +0300

    added updateWBToken as a tokenCollectionServices method

--- src/server/database/tokenCollectionService/index.js
+++ src/server/database/tokenCollectionService/index.js
@@ -1,8 +1,12 @@
 var { tokenCollection } = require("../connection/index");
+var updateWBToken = require("./services/updateWBToken");
 var createTokenCollectionEntity = require("./services/createTokenCollectionEntity");
 
 var tokenCollectionServices = () => {
   return {
+    updateWBToken: (userId, token) =>
+      updateWBToken(tokenCollection, userId, token),
+
     createTokenCollectionEntity: (userId) =>
       createTokenCollectionEntity(tokenCollection, userId),
   };
