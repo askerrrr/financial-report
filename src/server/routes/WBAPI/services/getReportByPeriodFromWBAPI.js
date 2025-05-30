e6029f5369ffbab4515df7c0be868e96410299f7
asker <askersultanov666@gmail.com>
2025-05-30 21:48:54 +0300

    added service getReportByPeriodFromWBAPI

---
+++ src/server/routes/WBAPI/services/getReportByPeriodFromWBAPI.js
@@ -0,0 +1,22 @@
+var url =
+  "https://statistics-api.wildberries.ru/api/v5/supplier/reportDetailByPeriod?";
+
+var getReportByPeriodFromWBAPI = async (dateFrom, dateTo, token) => {
+  var dateFromParam = "dateFrom=" + dateFrom;
+  var dateToParam = "dateTo=" + dateTo;
+
+  var res = await fetch(url + dateFromParam + dateToParam, {
+    method: "GET",
+    headers: { Authorization: "Bearer " + token },
+  });
+
+  if (!res.ok) {
+    return res.status;
+  }
+
+  var report = await res.json();
+
+  return report;
+};
+
+module.exports = getReportByPeriodFromWBAPI;
