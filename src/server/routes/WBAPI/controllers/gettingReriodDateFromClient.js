398238d377c58776f7a7f883d4c854832080de1b
asker <askersultanov666@gmail.com>
2025-05-30 22:02:50 +0300

    implemented getWBTokenByUserId, getReportByPeriodFromWBAPI into gettingReriodDateFromClient, passed the report to the next router

--- src/server/routes/WBAPI/controllers/gettingReriodDateFromClient.js
+++ src/server/routes/WBAPI/controllers/gettingReriodDateFromClient.js
@@ -1,3 +1,17 @@
-var gettingReriodDateFromClient = async (req, res, next) => {};
+var getReportByPeriodFromWBAPI = require("../services/getReportByPeriodFromWBAPI");
+
+var gettingReriodDateFromClient = async (req, res, next) => {
+  var { getWBTokenByUserId } = req.app.locals.tokenCollectionServices();
+
+  var { userId, dateFrom, dateTo } = req.body;
+
+  var token = await getWBTokenByUserId(userId);
+
+  var report = await getReportByPeriodFromWBAPI(dateFrom, dateTo, token);
+
+  req.report = report;
+
+  next();
+};
 
 module.exports = gettingReriodDateFromClient;
