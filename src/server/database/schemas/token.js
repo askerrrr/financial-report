0c0ae803204c8933a061f10ffbcc919e1cf14977
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-30 13:12:41 +0300

    change tokenScheme options for token field: required:false, added field default:"

--- src/server/database/schemas/token.js
+++ src/server/database/schemas/token.js
@@ -2,7 +2,7 @@ var { Schema } = require("mongoose");
 
 var tokenSchema = new Schema({
   userId: { type: String, required: true },
-  token: { type: String, required: true },
+  token: { type: String, required: false, default: "" },
 });
 
 module.exports = tokenSchema;
