1f4a60377e5ab01f75f4ef22505702b4bbe056a5
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-05-30 13:05:16 +0300

    added tokenCollection

--- src/server/database/connection/index.js
+++ src/server/database/connection/index.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+var tokenSchema = require("../schemas/token");
 var { userSchema, reportSchema } = require("../schemas/index");
 
 var reportDB = mongoose.createConnection("mongodb://127.0.0.1:27017/reports");
@@ -7,4 +8,6 @@ var userCollection = reportDB.model("User", userSchema);
 
 var reportCollection = reportDB.model("Report", reportSchema);
 
-module.exports = { userCollection, reportCollection };
+var tokenCollection = reportDB.model("Token", tokenSchema);
+
+module.exports = { userCollection, reportCollection, tokenCollection };
