5f122008bffe17909c9c67e323a2ad92c04d0b23
asker <askersultanov666@gmail.com>
2025-05-31 20:14:10 +0300

    added file & func: getQuantityPerItem

---
+++ src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/getQuantityPerItem.js
@@ -0,0 +1,9 @@
+var getQuantityPerItem = async (data, itemName) => {
+  var items = data.filter((e) => e.sa_name == itemName);
+
+  var qty = items.reduce((acc, i) => acc + i.quantity, 0);
+
+  return qty;
+};
+
+module.exports = getQuantityPerItem;
