0fa3aeb5a869465ed7d39f2d0bf4051dfc68d6e4
asker <askersultanov666@gmail.com>
2025-05-31 20:15:59 +0300

    added file & func: calcAverageRetailPricePerItem

---
+++ src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcAverageRetailPricePerItem.js
@@ -0,0 +1,11 @@
+var calcAverageRetailPricePerItem = async (data, quantity, itemName) => {
+  var items = data.filter((e) => e.sa_name == itemName && e.retail_price !== 0);
+
+  var retailPrices = items.reduce((acc, e) => acc + e.retail_price, 0);
+
+  var averageRetailPrice = retailPrices / quantity;
+
+  return averageRetailPrice;
+};
+
+module.exports = calcAverageRetailPricePerItem;
