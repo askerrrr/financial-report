837862d6f5ec4256d6ce8b999eccf2e6d62db38f
asker <askersultanov666@gmail.com>
2025-05-31 20:40:19 +0300

    change url into "sendPeriodDate"

--- src/public/js/index/services/sendPeriodDate.js
+++ src/public/js/index/services/sendPeriodDate.js
@@ -3,7 +3,7 @@ import getCookieByName from "./getCookieByName.js";
 var userId = await getCookieByName("userId");
 
 var sendPeriodDate = async (dateFrom, dateTo) => {
-  var res = await fetch("/wbapi", {
+  var res = await fetch("/reports/wbapi", {
     method: "POST",
     body: JSON.stringify({ userId, dateFrom, dateTo }),
     headers: { "Content-Type": "application/json" },
