5c540b3eb4fa5439177a164846103d9e08985223
asker <askersultanov666@gmail.com>
2025-06-01 17:42:10 +0300

    extracted func "createReport" from req.app.locals.reportCollectionServices(), use "createReport"

--- src/server/routes/reports/controllers/writeReportFromWBAPI.js
+++ src/server/routes/reports/controllers/writeReportFromWBAPI.js
@@ -1,11 +1,15 @@
 var parseReport = require("../services/writeAndCalcReportDataFromWBAPI/parseReport");
 
 var writeReportFromWBAPI = async (req, res, next) => {
+  var { createReport } = req.app.locals.reportCollectionServices();
+
   var { report, dateFrom, dateTo } = req.reportData;
 
   var userId = req.app.locals.userId;
 
   var parsedReport = await parseReport(report, dateFrom, dateTo);
+
+  var successfullWrite = await createReport(userId, report);
 };
 
 module.exports = writeReportFromWBAPI;
