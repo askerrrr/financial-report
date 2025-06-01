4201afbc8bdb1d15f6e939ad6d4562fb376d7d6d
asker <askersultanov666@gmail.com>
2025-06-01 17:52:16 +0300

    added options "{ _id: false }" for schemas

--- src/server/database/schemas/report.js
+++ src/server/database/schemas/report.js
@@ -3,34 +3,40 @@ var { Schema } = require("mongoose");
 var numOptions = { type: Number, default: 0 };
 var strOptions = { type: String, required: true };
 
-var itemsSchema = new Schema({
-  itemName: strOptions,
-  qty: numOptions,
-  costPrice: numOptions,
-  returnAmount: numOptions,
-  revenuePerItem: numOptions,
-  averageRetailPrice: numOptions,
-  averageStorageCost: numOptions,
-  finesPerItem: numOptions,
-  netProfitPerItem: numOptions,
-  netProfitMargin: numOptions,
-  finalNetProfitPerItem: numOptions,
-  averageFinalNetProfitPerItem: numOptions,
-  averageNetProfitPerItem: numOptions,
-});
+var itemsSchema = new Schema(
+  {
+    itemName: strOptions,
+    qty: numOptions,
+    costPrice: numOptions,
+    returnAmount: numOptions,
+    revenuePerItem: numOptions,
+    averageRetailPrice: numOptions,
+    averageStorageCost: numOptions,
+    finesPerItem: numOptions,
+    netProfitPerItem: numOptions,
+    netProfitMargin: numOptions,
+    finalNetProfitPerItem: numOptions,
+    averageFinalNetProfitPerItem: numOptions,
+    averageNetProfitPerItem: numOptions,
+  },
+  { _id: false }
+);
 
-var reportSchema = new Schema({
-  reportId: strOptions,
-  dateFrom: strOptions,
-  dateTo: strOptions,
-  totalRevenue: numOptions,
-  totalSold: numOptions,
-  totalFines: numOptions,
-  totalNetProfit: numOptions,
-  totalNetProfitMargin: numOptions,
-  totalStorageCost: numOptions,
-  totalDeliveryCost: numOptions,
-  items: [{ type: itemsSchema, required: true }],
-});
+var reportSchema = new Schema(
+  {
+    reportId: strOptions,
+    dateFrom: strOptions,
+    dateTo: strOptions,
+    totalRevenue: numOptions,
+    totalSold: numOptions,
+    totalFines: numOptions,
+    totalNetProfit: numOptions,
+    totalNetProfitMargin: numOptions,
+    totalStorageCost: numOptions,
+    totalDeliveryCost: numOptions,
+    items: [{ type: itemsSchema, required: true }],
+  },
+  { _id: false }
+);
 
 module.exports = reportSchema;
