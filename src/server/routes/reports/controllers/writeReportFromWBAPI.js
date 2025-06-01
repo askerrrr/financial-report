8bac547fd9b935af8ddee0011a1653e274cb8dc4
asker <askersultanov666@gmail.com>
2025-06-01 13:35:05 +0300

    imported func "parseReport" into controller "writeReportFromWBAPI"

--- src/server/routes/reports/controllers/writeReportFromWBAPI.js
+++ src/server/routes/reports/controllers/writeReportFromWBAPI.js
@@ -1,5 +1,9 @@
+var parseReport = require("../services/writeAndCalcReportDataFromWBAPI/parseReport");
+
 var writeReportFromWBAPI = async (req, res, next) => {
   var report = req.report;
+
+  var parsedReport = await parseReport(report);
 };
 
 module.exports = writeReportFromWBAPI;
