4dffdd229a2dbed2399f482ed517b5511d58123f
asker <askersultanov666@gmail.com>
2025-06-01 13:03:52 +0300

    added calcTotalNetProfitMargin

---
+++ src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcTotalNetProfitMargin.js
@@ -0,0 +1,11 @@
+var calcTotalNetProfitMargin = async (totalRevenue, totalNetProfit) => {
+  if (typeof totalNetProfit !== "number" || totalNetProfit == 0) {
+    return "Расчет только после ввода себестоимости";
+  }
+
+  var totalNetProfitMargin = (totalNetProfit * 100) / totalRevenue;
+
+  return totalNetProfitMargin;
+};
+
+module.exports = calcTotalNetProfitMargin;
