cbadd053bbc171d3fc9431b6aee14ebdd59d59a3
asker <askersultanov666@gmail.com>
2025-06-03 21:28:27 +0300

    fixed the search report by id

--- src/server/database/reportCollectionServices/services/updateItems.js
+++ src/server/database/reportCollectionServices/services/updateItems.js
@@ -3,7 +3,7 @@ var { DatabaseError } = require("../../../customError/customError");
 var updateItems = async (collection, userId, reportId, items) => {
   try {
     var result = await collection.updateOne(
-      { userId, "reports.id": reportId },
+      { userId, "reports.reportId": reportId },
       {
         $set: {
           "reports.$.items": items,
--- src/server/database/reportCollectionServices/services/updateReportPeriod.js
+++ src/server/database/reportCollectionServices/services/updateReportPeriod.js
@@ -3,7 +3,7 @@ var { DatabaseError } = require("../../../customError/customError");
 var updateReportPeriod = async (collecton, userId, reportId, period) => {
   try {
     var result = await collecton.updateOne(
-      { userId, "reports.id": reportId },
+      { userId, "reports.reportId": reportId },
       {
         $set: { "reports.$.period": period },
       }
