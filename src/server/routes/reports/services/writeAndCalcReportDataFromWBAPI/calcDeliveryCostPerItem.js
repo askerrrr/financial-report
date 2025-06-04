e34011be38fcfdf2b2b30d4e2c283431a1a26934
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-06-04 09:33:11 +0300

    update calcDeliveryCostPerItem

--- src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcDeliveryCostPerItem.js
+++ src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcDeliveryCostPerItem.js
@@ -1,9 +1,9 @@
 var calcDeliveryCostPerItem = async (data, itemName) => {
   var items = data.filter((e) => e.sa_name == itemName);
 
-  var deliveryCostPerItem = items.reduce((acc, i) => acc + i.delivery_rub);
+  var deliveryCostPerItem = items.reduce((acc, i) => acc + i.delivery_rub, 0);
 
-  return deliveryCostPerItem;
+  return Math.round(deliveryCostPerItem);
 };
 
 module.exports = calcDeliveryCostPerItem;
