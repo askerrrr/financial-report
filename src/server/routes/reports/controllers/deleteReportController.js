2c3353101b98c40551b2c953ac12cc3760d2f50b
asker <askersultanov666@gmail.com>
2025-06-06 22:09:43 +0300

    added controller "deleteReportController"

---
+++ src/server/routes/reports/controllers/deleteReportController.js
@@ -0,0 +1,11 @@
+var deleteReportController = async (req, res, next) => {
+  var { deleteReportFromDb } = req.app.locals.reportCollectionServices();
+
+  var { userId, reportId } = req.params;
+
+  var successDeleteReport = await deleteReportFromDb(userId, reportId);
+
+  return successDeleteReport ? res.sendStatus(200) : res.sendStatus(304);
+};
+
+module.exports = deleteReportFromDb;
