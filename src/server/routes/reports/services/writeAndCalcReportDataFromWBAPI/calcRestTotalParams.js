8df55009ba7c0f5e6049ae55b8ead1e64a4d1876
asker <askersultanov666@gmail.com>
2025-06-09 22:27:10 +0300

    implemented func "calcTotalNetProfitMargin" into func "calcRestTotalParams"

--- src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcRestTotalParams.js
+++ src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcRestTotalParams.js
@@ -1,7 +1,13 @@
 var calcTotalFinalNetProfit = require("./calcServices/calcTotalFinalNetProfit");
+var calcTotalNetProfitMargin = require("./calcServices/calcTotalNetProfitMargin");
 
 var calcRestTotalParams = async (rest, items) => {
   rest.totalFinalNetProfit = await calcTotalFinalNetProfit(items);
+
+  rest.totalNetProfitMargin = await calcTotalNetProfitMargin(
+    rest.totalRetailAmount,
+    rest.totalFinalNetProfit
+  );
 
   return { ...rest, items };
 };
