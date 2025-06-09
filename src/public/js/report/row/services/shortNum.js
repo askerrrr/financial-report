c3ba871f5d9adf6f9d1c8e05285fabb5794edb54
asker <askersultanov666@gmail.com>
2025-06-09 19:36:51 +0300

    update func "shortNum"

--- src/public/js/report/row/services/shortNum.js
+++ src/public/js/report/row/services/shortNum.js
@@ -1,6 +1,12 @@
 var shortNum = async (num) => {
   var str = num + "";
 
+  var strIncludesDot = str.split("").includes(".");
+
+  if (!strIncludesDot) {
+    return +str;
+  }
+
   var [start, end] = str.split(".");
 
   return [start, end.slice(0, 3)].join(".");
