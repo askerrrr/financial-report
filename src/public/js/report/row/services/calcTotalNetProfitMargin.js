81a43b9343a19c43675cf73e2fb147e2d25885cf
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-06-09 11:50:51 +0300

    implemented func shortnum into func calcTotalNetProfitMargin

--- src/public/js/report/row/services/calcTotalNetProfitMargin.js
+++ src/public/js/report/row/services/calcTotalNetProfitMargin.js
@@ -1,4 +1,12 @@
-var calcTotalNetProfitMargin = async (totalRetailAmount, totalFinalNetProfit) =>
-  (totalFinalNetProfit * 100) / totalRetailAmount;
+import shortNum from "./shortNum.js";
+
+var calcTotalNetProfitMargin = async (
+  totalRetailAmount,
+  totalFinalNetProfit
+) => {
+  var totalNetProfitMargin = (totalFinalNetProfit * 100) / totalRetailAmount;
+
+  return await shortNum(totalNetProfitMargin);
+};
 
 export default calcTotalNetProfitMargin;
