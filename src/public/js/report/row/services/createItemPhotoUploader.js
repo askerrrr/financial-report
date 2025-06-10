9fe3977e5991fce5fbef993a4e62a9f55bc9b546
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-06-10 14:19:58 +0300

    added func createItemPhotoUploader

---
+++ src/public/js/report/row/services/createItemPhotoUploader.js
@@ -0,0 +1,46 @@
+var createInputElement = async (id, name) => {
+  var input = document.createElement("input");
+  input.id = id;
+  input.name = name;
+  input.type = "file";
+  input.multiple = true;
+  input.accept = ".jpg, .jpeg, .png";
+
+  return input;
+};
+
+var createLabelElement = async (id) => {
+  var label = document.createElement("label");
+  label.htmlFor = id;
+  label.class = "item-photo-uploader";
+
+  return label;
+};
+
+var createFormElement = async (id) => {
+  var form = document.createElement("form");
+  form.id = id;
+
+  return form;
+};
+
+var createSpanElement = async () => {
+  var span = document.createElement("span");
+
+  return span;
+};
+
+var createItemPhotoUploader = async (id, name) => {
+  var input = await createInputElement(id, name);
+  var span = await createSpanElement();
+
+  var label = await createLabelElement(id);
+  label.append(input, span);
+
+  var form = await createFormElement(id);
+  form.append(label);
+
+  return form;
+};
+
+export default createItemPhotoUploader;
