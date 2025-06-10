1220f3c4f3c7e6cfd304ff435916e259b559642f
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-06-10 14:40:07 +0300

    update func filefilter

--- src/server/routes/reports/services/downloadFileServices/fileFilter.js
+++ src/server/routes/reports/services/downloadFileServices/fileFilter.js
@@ -1,8 +1,12 @@
 module.exports = (req, file, cb) => {
-  var validMimeType =
-    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+  var validMimeType;
 
-  return file.mimetype == validMimeType
-    ? cb(null, (req.fileMimeTypeIsValid = true))
-    : cb(null, (req.fileMimeTypeIsValid = false));
+  if (req.path == "/upload/file" || req.path == "/upload/files") {
+    validMimeType =
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+    return file.mimetype == validMimeType
+      ? cb(null, (req.fileMimeTypeIsValid = true))
+      : cb(null, (req.fileMimeTypeIsValid = false));
+  }
 };
