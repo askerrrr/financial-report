6ef485370a2ae703693dbc853a2a94bc55ca729f
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-06-11 10:23:42 +0300

    add new arg oldPath to func moveFileToReportFolder, filed newPath

--- src/server/routes/reports/services/differentServices/moveFileToReportFolder.js
+++ src/server/routes/reports/services/differentServices/moveFileToReportFolder.js
@@ -2,12 +2,10 @@ var { rename } = require("node:fs/promises");
 
 var getFileExtension = async (fileName) => fileName.split(".").at(-1);
 
-var moveFileToReportFolder = async (reportId, itemName, fileName) => {
-  var oldPath = "/var/temporary-photo-storage/" + fileName;
-
+var moveFileToReportFolder = async (reportId, itemName, fileName, oldPath) => {
   var fileExtension = await getFileExtension(fileName);
 
-  var newPath = `/var/report_items_photos/${reportId}/${itemName}.${fileExtension}`;
+  var newPath = `/var/report_items_photos/reportId_${reportId}/${itemName}.${fileExtension}`;
 
   await rename(oldPath, newPath);
 };
