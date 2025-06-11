c5e4b2ff63cfb48be4115f5822f1d67e6c771b9a
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-06-11 10:36:45 +0300

    implemented func moveFileToReportFolder in itemPhotoUpload controller

--- src/server/routes/reports/controllers/itemPhotoUpload.js
+++ src/server/routes/reports/controllers/itemPhotoUpload.js
@@ -1,3 +1,14 @@
-var itemPhotoUpload = async (req, res, next) => {};
+var moveFileToReportFolder = require("../services/differentServices/moveFileToReportFolder");
+
+var itemPhotoUpload = async (req, res, next) => {
+  var { reportId, itemname } = req.params;
+
+  var filePath = req.file.path;
+  var fileName = req.file.originalname;
+
+  await moveFileToReportFolder(reportId, itemname, fileName, filePath);
+
+  return res.sendStatus(200);
+};
 
 module.exports = itemPhotoUpload;
