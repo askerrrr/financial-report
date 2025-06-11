337346c20bd58182700074da32aaab9322b281f8
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-06-11 09:18:24 +0300

    added func sendItemPhoto, implemented func sendItemPhoto in func createInputElement, added event change

--- src/public/js/report/row/services/createItemPhotoUploader.js
+++ src/public/js/report/row/services/createItemPhotoUploader.js
@@ -1,3 +1,16 @@
+var sendItemPhoto = async (reportId, itemName, imgData) => {
+  var res = await fetch(`/reports/item-photo-upload/${reportId}/${itemName}`, {
+    method: "POST",
+    body: imgData,
+  });
+
+  if (!res.ok) {
+    return alert("Не удалось загрузить изображение");
+  }
+
+  alert("Изображение сохранено");
+};
+
 var createInputElement = async (id, name) => {
   var input = document.createElement("input");
   input.id = "input-" + id + "-" + name;
@@ -6,6 +19,16 @@ var createInputElement = async (id, name) => {
   input.multiple = false;
   input.accept = "image/jpg, image/jpeg, image/png";
   input.style.display = "none";
+
+  input.addEventListener("change", async (e) => {
+    e.preventDefault();
+
+    var uploadFormData = new FormData();
+
+    uploadFormData.append("item-photo", input.files[0]);
+
+    await sendItemPhoto(id, name, uploadFormData);
+  });
 
   return input;
 };
