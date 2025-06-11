995a213dcffa3424aa1c5ef786d0a0a2d0ff540b
asker <askersultanov666@gmail.com>
2025-06-11 22:34:34 +0300

    update func "getImageToBase64", added try-catch

--- src/server/routes/reports/services/differentServices/getImageToBase64.js
+++ src/server/routes/reports/services/differentServices/getImageToBase64.js
@@ -1,11 +1,17 @@
 var { readFile } = require("node:fs/promises");
 
 var getImageToBase64 = async (filePath) => {
-  var fileData = await readFile(filePath);
+  try {
+    var fileData = await readFile(filePath);
 
-  var base64 = fileData.toString("base64");
+    var base64 = fileData.toString("base64");
 
-  return base64;
+    return base64;
+  } catch (e) {
+    if (e.code == "ENOENT") {
+      return null;
+    }
+  }
 };
 
 module.exports = getImageToBase64;
