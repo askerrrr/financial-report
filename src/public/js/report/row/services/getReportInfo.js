27a613aa3747d8c780be4836e4efc3f7533da2eb
asker <askersultanov666@gmail.com>
2025-06-12 21:24:13 +0300

    changed the format of the report period output into func "getReportInfo"

--- src/public/js/report/row/services/getReportInfo.js
+++ src/public/js/report/row/services/getReportInfo.js
@@ -2,6 +2,9 @@ var getReportInfo = async ({ dateFrom, dateTo }) => {
   var div = document.createElement("div");
   div.id = "report-info";
 
+  dateFrom = dateFrom.split("-").join(".");
+  dateTo = dateTo.split("-").join(".");
+
   var reportInfo = `Отчет от ${dateFrom} до ${dateTo}`;
 
   div.append(reportInfo);
