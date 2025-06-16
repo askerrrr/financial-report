1310b6f481ddaf27dfe0795489b0bf08617f2808
asker <askersultanov666@gmail.com>
2025-06-16 22:35:05 +0300

    update func "getItemsName"

--- src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/getItemsName.js
+++ src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/getItemsName.js
@@ -1,5 +1,7 @@
 var getItemsName = async (data) => {
-  var items = data.filter((e) => e.sa_name !== "");
+  var items = data.filter(
+    (e) => e.doc_type_name == "Продажа" && e.supplier_oper_name == "Продажа"
+  );
 
   var itemsName = items.map((e) => e.sa_name);
 
