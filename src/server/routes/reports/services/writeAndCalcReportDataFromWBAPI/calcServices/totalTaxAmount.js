6e9d65e53adedcd9911596eccc62288dbe92272e
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-06-18 14:20:29 +0300

    implement func shorNum in func calcTotalTaxAmount

--- src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcServices/totalTaxAmount.js
+++ src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcServices/totalTaxAmount.js
@@ -1,10 +1,15 @@
+var shortNum = require("../shortNum");
+
 var calcTotalTaxAmount = async (totalRetailAmount, taxRate) => {
   if (taxRate) {
     return (totalRetailAmount * taxRate) / 100;
   }
 
   var defaultTaxRate = 6;
-  return (totalRetailAmount * defaultTaxRate) / 100;
+
+  var totalTaxAmount = (totalRetailAmount * defaultTaxRate) / 100;
+
+  return await shortNum(totalTaxAmount);
 };
 
 module.exports = calcTotalTaxAmount;
