84e0b6a12953a71986c7cf1223f0eea8dd28a65b
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-06-18 14:23:06 +0300

    implement func shorNum in func calcTotalRetailAmount

--- src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcServices/totalRetailAmount.js
+++ src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcServices/totalRetailAmount.js
@@ -1,7 +1,9 @@
+var shortNum = require("../shortNum");
+
 var calcTotalRetailAmount = async (report) => {
   var totalRetailAmount = report.reduce((acc, i) => acc + i.retail_amount, 0);
 
-  return totalRetailAmount;
+  return await shortNum(totalRetailAmount);
 };
 
 module.exports = calcTotalRetailAmount;
