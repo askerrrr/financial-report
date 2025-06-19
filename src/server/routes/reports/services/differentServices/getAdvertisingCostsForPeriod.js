738211a62ad9e97fdc56532f7d2c172133d4adea
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-06-19 14:17:06 +0300

    added a service for getting data about advertising campaigns, func getAdvertisingCostsForPeriod

---
+++ src/server/routes/reports/services/differentServices/getAdvertisingCostsForPeriod.js
@@ -0,0 +1,25 @@
+var { WBAPIError } = require("../../../../customError/customError");
+
+var calculateTotalAdCampaignCosts = async (data) =>
+  date.reduce((acc, i) => acc + i.updSum, 0);
+
+var getAdvertisingCostsForPeriod = async (dateFrom, dateTo, token) => {
+  var url = `https://advert-api.wildberries.ru/adv/v1/upd?from=${dateFrom}&to=${dateTo}`;
+
+  var res = await fetch(url, {
+    method: "GET",
+    headers: { Authorization: "Bearer " + token },
+  });
+
+  if (!res.ok) {
+    throw new WBAPIError(userId, res.status, res.statusText);
+  }
+
+  var data = await res.json();
+
+  var totalAdCampaignCosts = await calculateTotalAdCampaignCosts(data);
+
+  return totalAdCampaignCosts;
+};
+
+module.exports = getAdvertisingCostsForPeriod;
