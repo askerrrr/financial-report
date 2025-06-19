7596ccfcd9d91585743dd811a3511af8f7419930
asker <askersultanov666@gmail.com>
2025-06-19 21:57:15 +0300

    implement service "calcProductCosts" into func "calcRestTotalParams"

--- src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcServices/restTotalParams.js
+++ src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcServices/restTotalParams.js
@@ -1,8 +1,11 @@
+var calcProductCosts = require("./productCosts");
 var calcTotalFinalNetProfit = require("./totalFinalNetProfit");
 var calcTotalNetProfitMargin = require("./totalNetProfitMargin");
 
 var calcRestTotalParams = async (rest, skus) => {
   rest.totalFinalNetProfit = await calcTotalFinalNetProfit(skus);
+
+  rest.productCosts = await calcProductCosts(skus);
 
   rest.totalNetProfitMargin = await calcTotalNetProfitMargin(
     rest.totalRetailAmount,
