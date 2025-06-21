631ef7550eeef2dbd70031523d76c0bc03650520
asker <askersultanov666@gmail.com>
2025-06-21 16:04:36 +0300

    added connection to "reportingPeriodsSchema"

--- src/server/database/connection/index.js
+++ src/server/database/connection/index.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 var tokenSchema = require("../schemas/token");
 var { userSchema, reportSchema } = require("../schemas/index");
+var reportingPeriodsSchema = require("../schemas/reportingPeriodsSchema");
 
 var reportDB = mongoose.createConnection("mongodb://127.0.0.1:27017/reports");
 
@@ -10,4 +11,11 @@ var reportCollection = reportDB.model("Report", reportSchema);
 
 var tokenCollection = reportDB.model("Token", tokenSchema);
 
-module.exports = { userCollection, reportCollection, tokenCollection };
+var reportingPeriod = reportDB.model("ReportingPeriod", reportingPeriodsSchema);
+
+module.exports = {
+  userCollection,
+  reportCollection,
+  tokenCollection,
+  reportingPeriod,
+};
