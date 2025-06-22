6622bba44c96ba87f93452c30d78bd9fdfa01bab
asker <askersultanov666@gmail.com>
2025-06-22 19:26:09 +0300

    added service "getReportingPeriods", implement "getReportingPeriods" as "reportingPeriodsCollectionServices" method

--- src/server/database/reportingPeriodsCollectionServices/index.js
+++ src/server/database/reportingPeriodsCollectionServices/index.js
@@ -1,9 +1,13 @@
 var { reportingPeriodsCollection } = require("../connection");
+var getReportingPeriods = require("./services/getReportingPeriods");
 var createReportingPeriodsEntity = require("./services/createReportingPeriodsCollectionEntity");
 
 var reportingPeriodsCollectionServices = {
   createReportingPeriodsEntity: (userId) =>
     createReportingPeriodsEntity(reportingPeriodsCollection, userId),
+
+  getReportingPeriods: (userId) =>
+    getReportingPeriods(reportingPeriodsCollection, userId),
 };
 
 module.exports = reportingPeriodsCollectionServices;
---
+++ src/server/database/reportingPeriodsCollectionServices/services/getReportingPeriods.js
@@ -0,0 +1,13 @@
+var { DatabaseError } = require("../../../customError/customError");
+
+var getReportingPeriods = async (collection, userId) => {
+  try {
+    var reportingPeriods = await collection.findOne({ userId }).exec();
+
+    return reportingPeriods;
+  } catch (e) {
+    throw new DatabaseError(userId, e);
+  }
+};
+
+module.exports = getReportingPeriods;
