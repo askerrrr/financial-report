569257fc3977f3c402d2fe048b3e0bfc191aa5dd
asker <askersultanov666@gmail.com>
2025-06-29 14:05:38 +0300

    imported func "organizeReportsByPeriod" to "writeReportFromWBAPI" controller

--- src/server/routes/reports/controllers/writeReportFromWBAPI.js
+++ src/server/routes/reports/controllers/writeReportFromWBAPI.js
@@ -1,4 +1,5 @@
 var parseReport = require("../services/writeAndCalcReportDataFromWBAPI/index");
+var organizeReportsByPeriod = require("../services/reportPeriodsStructure");
 
 var writeReportFromWBAPI = async (req, res, next) => {
   var { createReport } = req.app.locals.reportCollectionServices;
