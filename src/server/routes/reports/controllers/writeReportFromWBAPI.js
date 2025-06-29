1ab49b858ade8362f7cc128402b7707a80dfea49
asker <askersultanov666@gmail.com>
2025-06-29 14:23:38 +0300

    implement func "updateReportPeriods", "organizeReportsByPeriod", "getReportingPeriods"

--- src/server/routes/reports/controllers/writeReportFromWBAPI.js
+++ src/server/routes/reports/controllers/writeReportFromWBAPI.js
@@ -3,6 +3,8 @@ var organizeReportsByPeriod = require("../services/reportPeriodsStructure");
 
 var writeReportFromWBAPI = async (req, res, next) => {
   var { createReport } = req.app.locals.reportCollectionServices;
+  var { getReportingPeriods, updateReportsPeriods } =
+    req.app.locals.reportingPeriodsCollectionServices;
 
   var { dateTo, dateFrom, report, paidStorageReport, totalAdCampaignCosts } =
     req.reportData;
@@ -20,6 +22,17 @@ var writeReportFromWBAPI = async (req, res, next) => {
   var reportId = report[0].realizationreport_id;
 
   parsedReport.reportId = reportId;
+
+  var { years } = await getReportingPeriods(userId);
+
+  var updatedReportPeriods = await organizeReportsByPeriod(
+    dateFrom,
+    dateTo,
+    reportId,
+    years
+  );
+
+  await updateReportsPeriods(userId, updatedReportPeriods);
 
   var successfullWrite = await createReport(userId, parsedReport);
 
