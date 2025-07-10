d9caba6ea80a2036531ffe79051259b11a1813e8
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-07-10 14:34:17 +0300

    update controller downloadReportAsXLSX

--- src/server/routes/reports/controllers/downloadReportAsXLSX.js
+++ src/server/routes/reports/controllers/downloadReportAsXLSX.js
@@ -1,5 +1,20 @@
 var downloadReportAsXLSX = async (req, res, next) => {
-    var {userId, reportId} = 
+  var { reportId } = req.params;
+  var { getReportById } = req.app.locals.reportCollectionServices;
+
+  var userId = req.app.locals.userId;
+
+  var report = await getReportById(userId, id);
+
+  var buffer;
+
+  return res
+    .setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+    .setHeader("Content-Disposition", 'attachment; filename="download.txt"')
+    .send(buffer);
 };
 
 module.exports = downloadReportAsXLSX;
