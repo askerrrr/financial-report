058bf2707fad98860ad4a77b4c21824a622052a8
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-07-10 15:46:53 +0300

    implemented createTotalsSheet func in getReportAsXLSXBuffer func

--- src/server/routes/reports/services/getReportAsXLSXBuffer/index.js
+++ src/server/routes/reports/services/getReportAsXLSXBuffer/index.js
@@ -1,3 +1,14 @@
-var getReportAsXLSXBuffer = async (report) => {};
+var ExcelJS = require("exceljs");
+var createTotalsSheet = require("./services/createTotalsSheet");
+
+var getReportAsXLSXBuffer = async (report) => {
+  var workbook = new ExcelJS.Workbook();
+
+  workbook = await createTotalsSheet(report, workbook);
+
+  var buffer = await workbook.xlsx.writeBuffer();
+
+  return buffer;
+};
 
 module.exports = getReportAsXLSXBuffer;
