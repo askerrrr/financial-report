25fd0b5ac4fda67a307c1481b8a7f05a4a2b08ef
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-07-10 15:49:59 +0300

    added func createSKUsSheet to getReportAsXLSXBuffer services, implemented func createSKUsSheet in getReportAsXLSXBuffer func

--- src/server/routes/reports/services/getReportAsXLSXBuffer/index.js
+++ src/server/routes/reports/services/getReportAsXLSXBuffer/index.js
@@ -1,9 +1,11 @@
 var ExcelJS = require("exceljs");
+var createSKUsSheet = require("./services/createSKUsSheet");
 var createTotalsSheet = require("./services/createTotalsSheet");
 
 var getReportAsXLSXBuffer = async (report) => {
   var workbook = new ExcelJS.Workbook();
 
+  workbook = await createSKUsSheet(report, workbook);
   workbook = await createTotalsSheet(report, workbook);
 
   var buffer = await workbook.xlsx.writeBuffer();
---
+++ src/server/routes/reports/services/getReportAsXLSXBuffer/services/createSKUsSheet.js
@@ -0,0 +1,7 @@
+var createSKUsSheet = async (report, workbook) => {
+  var sheet = workbook.addWorksheet("Лист 1");
+
+  return workbook;
+};
+
+module.exports = createSKUsSheet;
