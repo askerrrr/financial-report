fb7424aa5b342c82661de8a3bec39d2932be9150
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-07-11 12:25:18 +0300

    added file & func getRequiredSKUFieldsName to reportAsXLSXBuffer services

---
+++ src/server/routes/reports/services/reportAsXLSXBuffer/services/getRequiredSKUFieldsName.js
@@ -0,0 +1,34 @@
+var requiredSKUsFieldsName = [
+  "skuName",
+  "qty",
+  "costPrice",
+  "returnAmount",
+  "averageRetailPrice",
+  "deliveryCost",
+  "deductionOrPayment",
+  "storageCost",
+  "acceptance",
+  "netProfit",
+  "netProfitMargin",
+  "finalNetProfit",
+];
+
+var getRequiredSKUFieldsName = async () =>
+  skus.map((sku) => {
+    return {
+      skuName: sku.skuName,
+      qty: sku.qty,
+      costPrice: sku.costPrice,
+      returnAmount: sku.returnAmount,
+      averageRetailPrice: sku.averageRetailPrice,
+      deliveryCost: sku.deliveryCostPerSKU,
+      deductionOrPayment: sku.deductionOrPayment,
+      storageCost: sku.storageCostPerSKU,
+      acceptance: sku.acceptancePerSKU,
+      netProfit: sku.netProfitPerSKU,
+      netProfitMargin: sku.netProfitMargin,
+      finalNetProfit: sku.finalNetProfitPerSKU,
+    };
+  });
+
+module.exports = { getRequiredSKUFieldsName, requiredSKUsFieldsName };
