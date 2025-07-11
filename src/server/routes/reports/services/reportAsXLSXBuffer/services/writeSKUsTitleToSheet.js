f5347aaa3d3cec85c40726343cdf753da0f9b571
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-07-11 15:16:57 +0300

    added styles to skusSheet

--- src/server/routes/reports/services/reportAsXLSXBuffer/services/writeSKUsTitleToSheet.js
+++ src/server/routes/reports/services/reportAsXLSXBuffer/services/writeSKUsTitleToSheet.js
@@ -15,13 +15,31 @@ var titles = [
   "Итого",
 ];
 
+var fontStyles = {
+  bold: true,
+  font: 10,
+};
+
+var alignmentStyles = {
+  vertical: "middle",
+  horizontal: "center",
+  wrapText: true,
+};
+
 var writeSKUsTitleToSheet = async (sheet) => {
   var cellNum = 1;
-
+  var columnNum = 1;
   for (var i = 0; i < columns.length; i++) {
     var cellName = columns[i] + cellNum;
 
+    sheet.getColumn(columnNum).width = 17;
+
+    sheet.getCell(cellName).font = fontStyles;
+    sheet.getCell(cellName).alignment = alignmentStyles;
+
     sheet.getCell(cellName).value = titles[i];
+
+    columnNum++;
   }
 
   return sheet;
--- src/server/routes/reports/services/reportAsXLSXBuffer/services/writeSKUsToSheet.js
+++ src/server/routes/reports/services/reportAsXLSXBuffer/services/writeSKUsToSheet.js
@@ -2,6 +2,11 @@ var { requiredSKUsFieldsName } = require("./getRequiredSKUFieldsName");
 
 var columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"];
 
+var alignmentStyles = {
+  vertical: "middle",
+  horizontal: "center",
+};
+
 var writeSKUsToSheet = async (sheet, skus) => {
   var cellNum = 2;
 
@@ -10,6 +15,8 @@ var writeSKUsToSheet = async (sheet, skus) => {
       var cellName = columns[j] + cellNum;
 
       var skuFieldValue = skus[i][requiredSKUsFieldsName[j]];
+
+      sheet.getCell(cellName).alignment = alignmentStyles;
 
       sheet.getCell(cellName).value = skuFieldValue;
     }
