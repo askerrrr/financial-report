15627ab72f194983b7ae584bcdbf8a7d8091fd9e
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-07-11 13:38:24 +0300

    implemented functions writeSKUsToSheet, writeSKUsTitleToSheet, getRequiredSKUFieldsName in createSKUsSheet func

--- src/server/routes/reports/services/reportAsXLSXBuffer/services/createSKUsSheet.js
+++ src/server/routes/reports/services/reportAsXLSXBuffer/services/createSKUsSheet.js
@@ -1,5 +1,14 @@
+var { writeSKUsToSheet } = require("./writeSKUsToSheet");
+var writeSKUsTitleToSheet = require("./writeSKUsTitleToSheet");
+var { getRequiredSKUFieldsName } = require("./getRequiredSKUFieldsName");
+
 var createSKUsSheet = async (report, sheet) => {
-  return workbook;
+  var skus = await getRequiredSKUFieldsName(report.skus);
+
+  sheet = await writeSKUsToSheet(sheet, skus);
+  sheet = await writeSKUsTitleToSheet(sheet);
+
+  return sheet;
 };
 
 module.exports = createSKUsSheet;
