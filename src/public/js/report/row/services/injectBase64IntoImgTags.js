ea32838ec517ff33f7a3bacb061675019bc62e32
asker <askersultanov666@gmail.com>
2025-07-12 17:33:43 +0300

    fixed src atribut to img tag

--- src/public/js/report/row/services/injectBase64IntoImgTags.js
+++ src/public/js/report/row/services/injectBase64IntoImgTags.js
@@ -2,7 +2,7 @@ var injectBase64IntoImgTags = async (imageCollection) => {
   for (var { skuName, base64 } of imageCollection) {
     var imgTagId = "img-" + skuName;
 
-    var src = `data:image/png=;base64,${base64}`;
+    var src = `data:image/png;base64,${base64}`;
 
     var img = document.getElementById(imgTagId);
     img.style.display = "block";
