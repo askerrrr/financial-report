b2f5a8ee44e809062fca59bc2490bbba2c686c11
asker <askersultanov666@gmail.com>
2025-07-17 21:33:03 +0300

    added & implemented func "deleteEmptyMonth" into "showReportsTree"

--- src/public/js/index/showReportsTree.js
+++ src/public/js/index/showReportsTree.js
@@ -3,6 +3,21 @@ import openLastDetails from "./services/openLastDetails.js";
 import showNoReportsMessage from "./services/showNoReportsMessage.js";
 import createReportsTree from "./services/reportTreeBuilder/createReportTree/index.js";
 
+var deleteEmptyMonth = async () => {
+  var tableBodies = document.querySelectorAll("tbody");
+
+  var year, month;
+
+  for (var tbody of tableBodies) {
+    if (tbody.childNodes.length === 0) {
+      year = tbody.id.split("_")[2];
+      month = tbody.id.split("_")[4];
+
+      document.getElementById(`reports_container_${year}_${month}`).remove();
+    }
+  }
+};
+
 var showReportsTree = async () => {
   var { reports, years } = await getReportsData();
 
@@ -13,6 +28,8 @@ var showReportsTree = async () => {
   await createReportsTree(years, reports);
 
   await openLastDetails(years);
+
+  await deleteEmptyMonth();
 };
 
 export default showReportsTree;
