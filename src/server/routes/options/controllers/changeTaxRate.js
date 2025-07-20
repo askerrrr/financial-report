fbab396af782502ebd5994abb4e8aa8707e0a36d
asker <askersultanov666@gmail.com>
2025-07-20 10:27:14 +0300

    added logic to "changeTaxRate" controller

--- src/server/routes/options/controllers/changeTaxRate.js
+++ src/server/routes/options/controllers/changeTaxRate.js
@@ -1,6 +1,16 @@
 var changeTaxRate = async (req, res, next) => {
+  var { changeTaxRateToDb } = req.app.locals.optionsCollectonServices;
+
   var { taxRate } = req.body;
   var userId = req.app.locals.userId;
+
+  var successChange = await changeTaxRateToDb(userId, taxRate);
+
+  if (!successChange) {
+    return res.sendStatus(304);
+  }
+
+  return res.sendStatus(200);
 };
 
 module.exports = changeTaxRate;
