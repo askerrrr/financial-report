fb930e063351b04e8621ce114811c87bc684d02b
root <root@LAPTOP-345T3R3Q.localdomain>
2025-07-20 12:57:07 +0300

    added property "mandatory_insurance_premiums" to optionsSchema

--- src/server/database/schemas/options.js
+++ src/server/database/schemas/options.js
@@ -3,6 +3,7 @@ var { Schema } = require("mongoose");
 var optionsSchema = new Schema({
   userId: { type: String, required: true },
   taxRate: { type: Number, default: 6 },
+  mandatory_insurance_premiums: {type: Number, default: 0}
 });
 
 module.exports = optionsSchema;
