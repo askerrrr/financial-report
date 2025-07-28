682aa261474cf6ec146abf6fad1106d3b155f7d4
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-07-28 14:40:20 +0300

    added property paidtaxamount to taxparamsschema

--- src/server/database/schemas/taxParams.js
+++ src/server/database/schemas/taxParams.js
@@ -3,6 +3,7 @@ var { Schema } = require("mongoose");
 var taxParamsSchema = new Schema({
   userId: { type: String, required: true },
   taxRate: { type: Number, default: 6 },
+  paidTaxAmount: { type: Number, default: 0 },
   insuranceFeePercentage: { type: Number, default: 10 },
   paidInsurancePremiums: { type: Number, default: 0 },
   mandatoryInsurancePremiums: { type: Number, default: 0 },
