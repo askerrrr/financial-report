827f91a0155aa2a0fbbde4689639855a549d3a3f
asker <askersultanov666@gmail.com>
2025-07-30 22:44:02 +0300

    refactoring "calcTotalFinalProfit"

--- src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcServices/totalFinalProfit.js
+++ src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcServices/totalFinalProfit.js
@@ -1,12 +1,4 @@
-var shortNum = require("../shortNum");
-
-var calcTotalFinalProfit = async (skus) => {
-  var totalFinalProfit = skus.reduce(
-    (acc, sku) => acc + sku.finalProfitPerSKU,
-    0
-  );
-
-  return await shortNum(totalFinalProfit);
-};
+var calcTotalFinalProfit = async (skus) =>
+  skus.reduce((acc, sku) => acc + sku.finalProfitPerSKU, 0);
 
 module.exports = calcTotalFinalProfit;
