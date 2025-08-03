9e5e2672392f9e2aee7421ec424a698b80296027
asker <askersultanov666@gmail.com>
2025-08-03 21:04:57 +0300

    change "tax_params" collection structure

--- src/server/database/schemas/taxParams.js
+++ src/server/database/schemas/taxParams.js
@@ -1,14 +1,21 @@
 var { Schema } = require("mongoose");
 
+var taxYear = new Schema(
+  {
+    year: { type: Number, required: true },
+    taxRate: { type: Number, default: 6 },
+    paidTaxAmount: { type: Number, default: 0 },
+    insuranceFeePercentage: { type: Number, default: 10 },
+    paidInsuranceFee: { type: Number, default: 0 },
+    isInsuranceFeePaid: { type: Boolean, default: false },
+    mandatoryInsuranceFee: { type: Number, default: 0 },
+  },
+  { _id: false }
+);
+
 var taxParamsSchema = new Schema({
   userId: { type: String, required: true },
-  year: { type: Number, required: false },
-  taxRate: { type: Number, default: 6 },
-  paidTaxAmount: { type: Number, default: 0 },
-  insuranceFeePercentage: { type: Number, default: 10 },
-  paidInsuranceFee: { type: Number, default: 0 },
-  isInsuranceFeePaid: { type: Boolean, default: false },
-  mandatoryInsuranceFee: { type: Number, default: 0 },
+  years: [{ type: taxYear, required: false }],
 });
 
 module.exports = taxParamsSchema;
