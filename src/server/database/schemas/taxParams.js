e1571a1e849c33a464bcded515d766ddaeeac7bf
asker <askersultanov666@gmail.com>
2025-08-03 17:15:51 +0300

    added "year" property to tax_params collection

--- src/server/database/schemas/taxParams.js
+++ src/server/database/schemas/taxParams.js
@@ -2,6 +2,7 @@ var { Schema } = require("mongoose");
 
 var taxParamsSchema = new Schema({
   userId: { type: String, required: true },
+  year: { type: Number, required: false },
   taxRate: { type: Number, default: 6 },
   paidTaxAmount: { type: Number, default: 0 },
   insuranceFeePercentage: { type: Number, default: 10 },
