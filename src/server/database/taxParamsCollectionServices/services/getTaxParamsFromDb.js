1d5f24d0239a145dfab1c586bf7a58e632a9fbee
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-08-04 14:22:35 +0300

    fixed getTaxParamsFromDb

--- src/server/database/taxParamsCollectionServices/services/getTaxParamsFromDb.js
+++ src/server/database/taxParamsCollectionServices/services/getTaxParamsFromDb.js
@@ -2,7 +2,7 @@ var { DatabaseError } = require("../../../customError");
 
 var getTaxParamsFromDb = async (collection, userId, year) => {
   try {
-    var { years } = await collection.findOne({ userId, "years.year": year });
+    var { years } = await collection.findOne({ userId });
 
     if (year) {
       return years.find((date) => date.year == year);
