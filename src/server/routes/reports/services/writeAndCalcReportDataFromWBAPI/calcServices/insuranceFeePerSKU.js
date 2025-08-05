e7140ce7215e8668e21112dd3bb230a7ace1796f
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-08-05 11:04:09 +0300

    update func calcInsuranceFeePerSKU

--- src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcServices/insuranceFeePerSKU.js
+++ src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcServices/insuranceFeePerSKU.js
@@ -4,6 +4,10 @@ var calcInsuranceFeePerSKU = async (
   finalProfitPerSKU,
   insuranceFeePercentage
 ) => {
+  if (insuranceFeePercentage == 0) {
+    return 0;
+  }
+
   var insuranceFee = (finalProfitPerSKU * insuranceFeePercentage) / 100;
 
   return await shortNum(insuranceFee);
