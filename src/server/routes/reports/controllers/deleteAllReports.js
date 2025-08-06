2f3216a6d4f730070f777ac274d79a38440f1933
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-08-06 10:26:50 +0300

    update controller deleteAllReports, implemented functions deleteAllReportsByUserId, deleteTaxYears

--- src/server/routes/reports/controllers/deleteAllReports.js
+++ src/server/routes/reports/controllers/deleteAllReports.js
@@ -1,11 +1,18 @@
 var deleteAllReports = async (req, res, next) => {
   var { deleteAllReportsByUserId } = req.app.locals.reportCollectionServices;
+  var { deleteReportsTreeByUserId } =
+    req.app.locals.reportsTreeCollectionServices;
+  var { deleteTaxYears } = req.app.locals.taxParamsCollectionServices;
 
   var { userId } = req.params;
 
-  var successDeleted = await deleteAllReportsByUserId(userId);
+  var taxYearsIdDeleted = await deleteTaxYears(userId);
+  var reportsIsDeleted = await deleteAllReportsByUserId(userId);
+  var reportTreeIsDeleted = await deleteReportsTreeByUserId(userId);
 
-  return successDeleted ? res.sendStatus(200) : res.sendStatus(304);
+  return taxYearsIdDeleted && reportsIsDeleted && reportTreeIsDeleted
+    ? res.sendStatus(200)
+    : res.sendStatus(304);
 };
 
 module.exports = deleteAllReports;
