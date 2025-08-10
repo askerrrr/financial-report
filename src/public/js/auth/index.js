992e6e7a01662e45366c8428e762b6fcaa745239
asker <askersultanov666@gmail.com>
2025-08-10 21:43:02 +0300

    rm file "sendAuthData", moved func "sendAuthData" to "index.js" file

--- src/public/js/auth/index.js
+++ src/public/js/auth/index.js
@@ -1,35 +1,41 @@
-import sendAuthData from "./sendAuthData.js";
+var sendAuthData = async (data) => {
+  var res = await fetch("/auth/check", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return res;
+};
 
 var authFormHandler = async () => {
-  try {
-    var form = document.getElementById("auth-form");
+  var form = document.getElementById("auth-form");
 
-    return form.addEventListener("submit", async (e) => {
-      e.preventDefault();
+  form.onsubmit = async (e) => {
+    e.preventDefault();
 
-      var formData = {};
+    var formData = {};
 
-      new FormData(form).forEach((value, key) => {
-        formData[key] = value;
-      });
-
-      var res = await sendAuthData(formData);
-
-      if (res.status == 404) {
-        return alert(`Нет пользователя c ником  ${formData.login}`);
-      }
-
-      if (res.status == 403) {
-        return alert("Введены неправильные данные");
-      }
-
-      var { redirectUrl } = await res.json();
-
-      window.location.href = redirectUrl;
+    new FormData(form).forEach((value, key) => {
+      formData[key] = value;
     });
-  } catch (e) {
-    console.log("auth error: ", e);
-  }
+
+    var res = await sendAuthData(formData);
+
+    if (res.status == 404) {
+      return alert(`Нет пользователя c ником  ${formData.login}`);
+    }
+
+    if (res.status == 403) {
+      return alert("Введены неправильные данные");
+    }
+
+    var { redirectUrl } = await res.json();
+
+    window.location.href = redirectUrl;
+  };
 };
 
 authFormHandler();
