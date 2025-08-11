3454cb192e4279c131f7418dc30213374afed708
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-08-11 14:07:20 +0300

    update controller recalculateReportsParamsAfterChangingInsuranceFeePergentage, added service recalculateReportsInsuranceFee

--- src/server/routes/taxParams/controllers/recalculateReportsParamsAfterChangingInsuranceFeePergentage.js
+++ src/server/routes/taxParams/controllers/recalculateReportsParamsAfterChangingInsuranceFeePergentage.js
@@ -1,8 +1,18 @@
+var recalculateReportsInsuranceFee = require("../services/recalculateReportsInsuranceFee");
+
 var recalculateReportsParamsAfterChangingInsuranceFeePergentage = async (req, res, next) => {
   var { year, userId, percent } = req.body;
-  var { getTaxParamsFromDb } = req.app.locals.taxParamsCollectionServices;
+  var { getReportsByUserId, saveUpdatedReports } = req.app.locals.reportCollectionServices;
+  var { getTaxParamsFromDb, changeInsuranceFeePercentageToDb, changePaidInsuranceFeeToDb } = req.app.locals.taxParamsCollectionServices;
 
+  var reports = await getReportsByUserId(userId);
   var taxParams = await getTaxParamsFromDb(userId, year);
+
+  var { reports, recalculatedPaidInsuranceFee } = await recalculateReportsInsuranceFee(year, reports, percent, taxParams);
+
+  await saveUpdatedReports(userId, reports);
+  await changeInsuranceFeePercentageToDb(userId, year, percent);
+  await changePaidInsuranceFeeToDb(userId, year, recalculatedPaidInsuranceFee);
 
   return res.sendStatus(200);
 };
