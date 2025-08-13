18a74adc9a0a6d1e9000b110fb76091c59665fb7
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-08-13 11:16:14 +0300

    receiving and sending a report in the getReport controller in the noAuthDecode router

--- src/server/routes/noAuthDecode/controllers/getReport.js
+++ src/server/routes/noAuthDecode/controllers/getReport.js
@@ -1,3 +1,13 @@
-var getReport = async (req, res, next) => {};
+var getReport = async (req, res, next) => {
+  var { id } = req.params;
+
+  var { report } = req.app.locals?.reports.find((report) => report.id === id);
+
+  if (!report) {
+    return res.status(500).json("Произошла ошибка, попробуйте позже");
+  }
+
+  return res.json({ report });
+};
 
 module.exports = getReport;
