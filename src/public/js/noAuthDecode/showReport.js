af6b59c4c9df43e25a8dfe895028b3c858fea1c2
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-08-13 10:31:27 +0300

    added func getReport to showReport.js

--- src/public/js/noAuthDecode/showReport.js
+++ src/public/js/noAuthDecode/showReport.js
@@ -1,3 +1,17 @@
-var showReport = async () => {};
+var getReport = async (id) => {
+  var url = "/no-auth-decode/report/" + id;
+
+  var res = await fetch(url);
+
+  var { report } = await res.json();
+
+  return report;
+};
+
+var showReport = async () => {
+  var id = window.location.pathname.split("/")[3];
+
+  var report = await getReport(id);
+};
 
 showReport();
