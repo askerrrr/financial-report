136e8fbe3e6ef3fd61437add0127e4a42365ec82
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-08-13 11:21:23 +0300

    visual changes in the createReportTable

--- src/public/js/noAuthDecode/createReportTable.js
+++ src/public/js/noAuthDecode/createReportTable.js
@@ -6,47 +6,28 @@ var table = document.getElementById("report");
 var createReportTable = async (report) => {
   var tbody = document.createElement("tbody");
 
-  var { reportId, recordTo, skus, userId } = report;
+  var { reportId, skus, userId } = report;
 
   for (var [skuIndex, sku] of Object.entries(skus)) {
     var tr = document.createElement("tr");
 
     var skuName = await createTdElement(sku.skuName);
-
     var qty = await createTdElement(sku.qty);
-
     var returnAmount = await createTdElement(sku.returnAmountPerSKU);
 
-    var costPriceData = {
-      userId,
-      skuIndex,
-      reportId,
-      year: +recordTo.year,
-      fieldName: "costPrice",
-      costPrice: sku.costPrice,
-    };
+    var costPriceData = { userId, skuIndex, reportId, fieldName: "costPrice", costPrice: sku.costPrice };
 
     var costPriceInputField = await createInputField(costPriceData);
-
     var costPrice = await createTdElement(costPriceInputField);
-
     var retailPrice = await createTdElement(sku.averageRetailPrice);
-
     var deliveryCost = await createTdElement(sku.deliveryCostPerSKU);
-
     var fines = await createTdElement(sku.finesPerSKU);
-
     var storageCostPerSKU = await createTdElement(sku.storageCostPerSKU);
-
     var acceptancePerSKU = await createTdElement(sku.acceptancePerSKU);
-
     var profitPerSKU = await createTdElement(sku.profitPerSKU);
-
     var averageProfitPerSKU = await createTdElement(sku.averageProfitPerSKU);
-
-    var profitMargin = await createTdElement(sku.profitMargin, "profitMargin", skuIndex);
-
-    var finalProfitPerSKU = await createTdElement(sku.finalProfitPerSKU, "finalProfitPerSKU", skuIndex);
+    var profitMargin = await createTdElement(0, "profitMargin", skuIndex);
+    var finalProfitPerSKU = await createTdElement(0, "finalProfitPerSKU", skuIndex);
 
     tr.append(
       skuName,
