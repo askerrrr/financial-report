84559d5181c3e90e1a0535a2ae0166b860d3ac85
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-08-25 12:42:12 +0300

    fixed creation user in the init-mongo.js

--- src/server/database/init-mongo.js
+++ src/server/database/init-mongo.js
@@ -1,5 +1,5 @@
 db.createUser({
-  login: process.env.MONGO_INITDB_ROOT_USERNAME,
+  user: process.env.MONGO_INITDB_ROOT_USERNAME,
   passwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
   roles: [{ role: "root", db: "admin" }],
 });
