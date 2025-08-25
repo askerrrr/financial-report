72205b86d59c79120b3c0c7f610f2820f26942fa
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-08-25 14:38:00 +0300

    moved event saveButton.onlick to createSaveButton.js

--- src/public/js/index/services/reportPeriodUploader/index.js
+++ src/public/js/index/services/reportPeriodUploader/index.js
@@ -1,16 +1,9 @@
 import createModal from "./services/createModal.js";
 import createTitle from "./services/createTitle.js";
-import isFutureDate from "./services/isFutureDate.js";
-import getDateToByDateFrom from "../periodUtils/index.js";
-import sendReportPeriod from "./services/sendReportPeriod.js";
 import createInputField from "./services/createInputField.js";
 import createSaveButton from "./services/createSaveButton.js";
-import { showLoader, deleteLoader } from "./services/loader.js";
 import createCancelButton from "./services/createCancelButton.js";
-import validateReportPeriod from "./services/validateReportPeriod.js";
 import createButtonsContainer from "./services/createButtonsContainer.js";
-import { isMonday } from "../periodUtils/services/getWeekDaysFromMonth.js";
-import insertNewReportToTree from "../reportTreeBuilder/insertNewReportToTree/index.js";
 
 var openReportPeriodModal = async () => {
   var modal = await createModal("modal-overlay");
@@ -19,64 +12,7 @@ var openReportPeriodModal = async () => {
 
   var dateToInput = await createInputField("dateToInput", "конец в формате гггг.мм.дд - 2025.04.27");
 
-  var saveButton = await createSaveButton();
-
-  saveButton.onclick = async () => {
-    document.body.removeChild(modal);
-
-    var dateFrom = dateFromInput.value;
-    var validDateFrom = await validateReportPeriod(dateFrom);
-
-    if (!validDateFrom) {
-      return alert("Начало периода введено некорректно");
-    }
-
-    if (await isFutureDate(validDateFrom)) {
-      return alert("Период введен некорректно");
-    }
-
-    if (!(await isMonday(validDateFrom))) {
-      return alert("Начало периода не является понедельником");
-    }
-
-    var dateTo = dateToInput?.value;
-
-    if (!dateTo) {
-      dateTo = await getDateToByDateFrom(validDateFrom);
-    }
-
-    var validDateTo = await validateReportPeriod(dateTo);
-
-    if (await isFutureDate(validDateTo)) {
-      return alert("Отчет еще не готов...");
-    }
-
-    if (!validDateTo) {
-      return alert("Конец периода введен некорректно");
-    }
-
-    await showLoader();
-
-    var reportData = await sendReportPeriod(validDateFrom, validDateTo);
-
-    if (!reportData) {
-      await deleteLoader();
-
-      return;
-    }
-
-    await deleteLoader();
-
-    await insertNewReportToTree(reportData);
-
-    var confirmed = confirm("Отчет успешно сохранен.\nПерейти к отчету?");
-
-    if (confirmed) {
-      window.location.href = "/reports/" + reportData.reportId;
-    }
-
-    return;
-  };
+  var saveButton = await createSaveButton(modal, dateFromInput, dateToInput);
 
   var cancelButton = await createCancelButton();
 
--- src/public/js/index/services/reportPeriodUploader/services/createSaveButton.js
+++ src/public/js/index/services/reportPeriodUploader/services/createSaveButton.js
@@ -1,7 +1,72 @@
-var createSaveButton = async () => {
+import isFutureDate from "./isFutureDate.js";
+import sendReportPeriod from "./sendReportPeriod.js";
+import { showLoader, deleteLoader } from "./loader.js";
+import getDateToByDateFrom from "../../periodUtils/index.js";
+import validateReportPeriod from "./validateReportPeriod.js";
+import { isMonday } from "../../periodUtils/services/getWeekDaysFromMonth.js";
+import insertNewReportToTree from "../../reportTreeBuilder/insertNewReportToTree/index.js";
+
+var createSaveButton = async (modal, dateFromInput, dateToInput) => {
   var button = document.createElement("button");
   button.className = "modal-button modal-button-save";
   button.textContent = "Сохранить";
+
+  button.onclick = async () => {
+    document.body.removeChild(modal);
+
+    var dateFrom = dateFromInput.value;
+    var validDateFrom = await validateReportPeriod(dateFrom);
+
+    if (!validDateFrom) {
+      return alert("Начало периода введено некорректно");
+    }
+
+    if (await isFutureDate(validDateFrom)) {
+      return alert("Период введен некорректно");
+    }
+
+    if (!(await isMonday(validDateFrom))) {
+      return alert("Начало периода не является понедельником");
+    }
+
+    var dateTo = dateToInput?.value;
+
+    if (!dateTo) {
+      dateTo = await getDateToByDateFrom(validDateFrom);
+    }
+
+    var validDateTo = await validateReportPeriod(dateTo);
+
+    if (await isFutureDate(validDateTo)) {
+      return alert("Отчет еще не готов...");
+    }
+
+    if (!validDateTo) {
+      return alert("Конец периода введен некорректно");
+    }
+
+    await showLoader();
+
+    var reportData = await sendReportPeriod(validDateFrom, validDateTo);
+
+    if (!reportData) {
+      await deleteLoader();
+
+      return;
+    }
+
+    await deleteLoader();
+
+    await insertNewReportToTree(reportData);
+
+    var confirmed = confirm("Отчет успешно сохранен.\nПерейти к отчету?");
+
+    if (confirmed) {
+      window.location.href = "/reports/" + reportData.reportId;
+    }
+
+    return;
+  };
 
   return button;
 };
