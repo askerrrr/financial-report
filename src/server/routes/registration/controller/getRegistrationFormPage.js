f7beedcd234ea601d88656ede3ec4c75bdd26323
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-08-27 09:13:34 +0300

    rename html file

--- src/server/routes/registration/controller/getRegistrationFormPage.js
+++ src/server/routes/registration/controller/getRegistrationFormPage.js
@@ -1,5 +1,5 @@
 var { join } = require("node:path");
 
-var getRegistrationFormPage = async (req, res, next) => res.sendFile(join(__dirname, "../../../../public/html/registration/registrationForm.html"));
+var getRegistrationFormPage = async (req, res, next) => res.sendFile(join(__dirname, "../../../../public/html/registration/index.html"));
 
 module.exports = getRegistrationFormPage;
