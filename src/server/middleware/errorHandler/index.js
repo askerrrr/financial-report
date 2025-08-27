b723ed50d9a0d0d7a2cf9707a0f415b55264a942
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-08-27 09:44:35 +0300

    update func errorHander

--- src/server/middleware/errorHandler/index.js
+++ src/server/middleware/errorHandler/index.js
@@ -2,7 +2,7 @@ var multer = require("multer");
 var { WBAPIError, FormDataError, DatabaseError, ReportNotFoundError, DatabaseConnectionError } = require("../../customError");
 
 var errorHandler = async (e, req, res, next) => {
-  console.log({ msg: e.message, errName: e.name, status: e.status, stack: e.stack, cause: e.cause });
+  console.log({ msg: e.message, errName: e.name, status: e?.status, stack: e.stack, cause: e.cause });
 
   if (e instanceof multer.MulterError) {
     return res.sendStatus(500);
@@ -20,11 +20,11 @@ var errorHandler = async (e, req, res, next) => {
     return res.status(e.status).json({ msg: e.message });
   }
 
-  if(e instanceof FormDataError) {
-    return res.status(e.status).json({msg: e.message, invalidField: e.invalidField})
+  if (e instanceof FormDataError) {
+    return res.status(e.status).json({ msg: e.message, invalidField: e.invalidField });
   }
 
-  res.status(e.status).json({msg: e.message, status: e.status})
+  res.status(e?.status || 500).json({ msg: e.message, status: e?.status || 500 });
 };
 
 module.exports = errorHandler;
