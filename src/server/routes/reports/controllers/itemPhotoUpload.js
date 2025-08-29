238329b659c28f6cb73fdf936f518d81a7a008c9
root <root@LAPTOP-345T3R3Q.localdomain>
2025-08-29 22:17:42 +0300

    remove extra importing

--- src/server/routes/reports/controllers/itemPhotoUpload.js
+++ src/server/routes/reports/controllers/itemPhotoUpload.js
@@ -1,5 +1,4 @@
 var s3 = require("../services/s3");
-var moveFileToUserFolder = require("../services/different/moveFileToUserFolder");
 
 var itemPhotoUpload = async (req, res, next) => {
   var { itemname } = req.params;
@@ -9,9 +8,6 @@ var itemPhotoUpload = async (req, res, next) => {
   
   var objectKey= itemname + "_" + userId;
   await s3.upload(objectKey, buffer);
-
- // await moveFileToUserFolder(userId, itemname, filePath);
-
   return res.sendStatus(200);
 };
 
