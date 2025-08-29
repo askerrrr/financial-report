32d7097603f47e5326cc8b901e68a8bc20cd7ac9
root <root@LAPTOP-345T3R3Q.localdomain>
2025-08-30 00:00:59 +0300

    fixed s3 service "uploadFile"

--- src/server/routes/reports/services/s3/upload.js
+++ src/server/routes/reports/services/s3/upload.js
@@ -4,9 +4,9 @@ var { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 var uploadFile = async (objectKey, buffer) => {
   var client = new S3Client(env.S3Client_OPTIONS);
   var command = new PutObjectCommand({ Bucket: env.bucketName, Key: objectKey, Body: buffer, ContentType: "image/png" });
-  var { httpStatusCode } = await client.send(command);
-  console.log("httpStatusCode: ", httpStatusCode);
-  return httpStatusCode
+  var res  = await client.send(command);
+  console.log("httpStatusCode: ", res['$metadata'].httpStatusCode);
+  return { httpStatusCode: res['$metadata'].httpStatusCode }
 };
 
 module.exports = uploadFile;
