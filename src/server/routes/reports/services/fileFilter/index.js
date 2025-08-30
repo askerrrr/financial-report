e5ba1b69953cb551628a2ca85b523e2c2288a0a9
asker <askersultanov666@gmail.com>
2025-08-30 11:43:50 +0300

    updated multer service "fileFilter"

--- src/server/routes/reports/services/fileFilter/index.js
+++ src/server/routes/reports/services/fileFilter/index.js
@@ -1,20 +1,5 @@
 module.exports = (req, file, cb) => {
-  var validMimeType;
+  var validMimeType = ["image/jpg", "image/jpeg", "image/png"];
 
-  if (req.path == "/upload/file" || req.path == "/upload/files") {
-    validMimeType =
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-
-    return file.mimetype == validMimeType
-      ? cb(null, (req.fileMimeTypeIsValid = true))
-      : cb(null, (req.fileMimeTypeIsValid = false));
-  }
-
-  if (req.path.startsWith("/sku-photo-upload/")) {
-    validMimeType = ["image/jpg", "image/jpeg", "image/png"];
-
-    return validMimeType.includes(file.mimetype)
-      ? cb(null, (req.fileMimeTypeIsValid = true))
-      : cb(null, (req.fileMimeTypeIsValid = false));
-  }
+  return validMimeType.includes(file.mimetype) ? cb(null, (req.fileMimeTypeIsValid = true)) : cb(null, (req.fileMimeTypeIsValid = false));
 };
