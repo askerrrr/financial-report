9233479a4f3df3ef27013b04f9b54bbdb0a37d3c
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-09-01 08:22:50 +0300

    update func collectImagesAsBase64

--- src/server/routes/reports/services/different/collectImagesAsBase64.js
+++ src/server/routes/reports/services/different/collectImagesAsBase64.js
@@ -1,12 +1,10 @@
-var s3 = require('../s3')
+var s3 = require("../s3");
 
 var collectImagesAsBase64 = async (userId, skus) => {
   var array = [];
 
-  for (var { skuName } of skus) {
-    var objectKey = 'skuname=' + skuName + ';' + 'userId=' + userId	
+  for (var { skuName, objectKey } of skus) {
     var base64 = await s3.getFile(objectKey);
-
     array.push({ skuName, base64 });
   }
 
