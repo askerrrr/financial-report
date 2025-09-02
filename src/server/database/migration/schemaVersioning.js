4fc1ddef9e08de4e6a853cbd0a0b498041019981
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-09-02 13:58:50 +0300

    added file schemaVersioning

---
+++ src/server/database/migration/schemaVersioning.js
@@ -0,0 +1,8 @@
+module.exports = {
+  skuSchemaVersion: 1,
+  tokenSchemaVersion: 1,
+  usersSchemaVersion: 1,
+  reportSchemaVersion: 1,
+  taxParamsSchemaVersion: 1,
+  reportsTreeSchemaVersion: 1,
+};
