b06cc8b592bf22b9365b275fe7e52809071680a3
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-09-15 15:29:35 +0300

    added option _id:false to sku schema

--- src/server/database/schemas/skus.js
+++ src/server/database/schemas/skus.js
@@ -1,12 +1,15 @@
 var { Schema } = require("mongoose");
 
-var SKUSchema = new Schema({
-  name: { type: String, required: true },
-  id: { type: Number, required: true },
-  price: { type: Number, default: 0 },
-  discont: { type: Number, default: 0 },
-  discountedPrice: { type: Number, default: 0 },
-});
+var SKUSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    id: { type: Number, required: true },
+    price: { type: Number, default: 0 },
+    discont: { type: Number, default: 0 },
+    discountedPrice: { type: Number, default: 0 },
+  },
+  { _id: false }
+);
 
 var SKUsSchema = new Schema({ userId: { type: String, required: true }, skus: [{ type: SKUSchema, required: false }] });
 
