6dd09f1fc73e866b353f0fb607ba05d52ca63082
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-09-15 15:27:09 +0300

    added param default to sku schema

--- src/server/database/schemas/skus.js
+++ src/server/database/schemas/skus.js
@@ -3,9 +3,9 @@ var { Schema } = require("mongoose");
 var SKUSchema = new Schema({
   name: { type: String, required: true },
   id: { type: Number, required: true },
-  price: { type: Number, required: true },
-  discont: { type: Number, required: true },
-  discountedPrice: { type: Number, required: true },
+  price: { type: Number, default: 0 },
+  discont: { type: Number, default: 0 },
+  discountedPrice: { type: Number, default: 0 },
 });
 
 var SKUsSchema = new Schema({ userId: { type: String, required: true }, skus: [{ type: SKUSchema, required: false }] });
