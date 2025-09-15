c0330264e08afce0981617e4bb511e2eb41ccd78
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-09-15 14:08:53 +0300

    added db service createSKUsEntity, implemented him in the createUser controller

--- src/server/database/collections/skus/index.js
+++ src/server/database/collections/skus/index.js
@@ -1,8 +1,11 @@
 var { skuCollection } = require("../../connections");
+const createSKUsEntity = require("./services/createSKUsEntity");
 var getSKUs = require("./services/getSKUs");
 
 var skusCollectionServices = {
   getSKUs: (userId) => getSKUs(skuCollection, userId),
+
+  createSKUsEntity: (userId) => createSKUsEntity(skuCollection, userId),
 };
 
 module.exports = skusCollectionServices;
