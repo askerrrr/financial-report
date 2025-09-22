cb43817157276cfdf706768b400cc09db1eb476e
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-09-22 15:52:24 +0300

    fixed the nextMonth value when the currentMonth value is less than 9 in the getNextMonth function

--- src/public/js/index/services/periodUtils/services/getNextMonth.js
+++ src/public/js/index/services/periodUtils/services/getNextMonth.js
@@ -1,8 +1,8 @@
 var getNextMonth = (currentMonth) => {
   var nextMonth;
 
-  if (currentMonth > 9) {
-    nextMonth = "0" + currentMonth + 1;
+  if (currentMonth < 9) {
+    nextMonth = "0" + ++currentMonth;
     return nextMonth;
   }
 
