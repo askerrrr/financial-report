8109291d7584b1cebe9af56f86649628c45419dd
root <root@LAPTOP-345T3R3Q.localdomain>
2025-10-12 22:48:28 +0300

    refactoring controller "getReportsData"

--- src/server/routes/index/controllers/getReportsData.js
+++ src/server/routes/index/controllers/getReportsData.js
@@ -7,9 +7,7 @@ var getReportsData = async (req, res, next) => {
   var { getReportsByUserId } = req.app.locals.reportCollectionServices;
   var { getReportsTree } = req.app.locals.reportsTreeCollectionServices;
 
-  var { years } = await getReportsTree(userId);
-
-  var reports = await getReportsByUserId(userId);
+  var [{ years }, reports] = await Promise.all([getReportsTree(userId), getReportsByUserId(userId)]);
 
   var [yearsDto, reportsDto] = await Promise.all([getYearsDto(years), getReportsDto(reports)]);
 
