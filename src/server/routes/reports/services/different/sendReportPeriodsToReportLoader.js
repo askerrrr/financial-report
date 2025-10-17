c5c4c571a9218c6a249e646ab4c177149e74f2ee
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-10-17 10:33:19 +0300

    fixed getting the authorization value in the sendReportPeriodsToReportLoader func

--- src/server/routes/reports/services/different/sendReportPeriodsToReportLoader.js
+++ src/server/routes/reports/services/different/sendReportPeriodsToReportLoader.js
@@ -2,7 +2,7 @@ var sendReportPeriodsToReportLoader = async ({ userId, dateFrom, dateTo }) => {
   var res = await fetch(process.env.REPORT_LOADER_URL, {
     method: "POST",
     body: JSON.stringify({ userId, dateFrom, dateTo }),
-    headers: { "content-type": "application/json", Authorization: "Bearer " + process.envsecretKey },
+    headers: { "content-type": "application/json", Authorization: "Bearer " + process.env.SECRET_KEY },
   });
 
   return { status: res.status };
