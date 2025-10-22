6c55ab096342c331a8deadd52f5612c5637cb25e
root <root@LAPTOP-345T3R3Q.localdomain>
2025-10-22 23:36:17 +0300

    added file/func "getLoadingProgressStatus" to db utils

--- src/server/database/collections/reportLoadingStates/index.js
+++ src/server/database/collections/reportLoadingStates/index.js
@@ -1,7 +1,10 @@
 var { reportLoadingStatesCollection } = require("../../connections");
+
+var getLoadingProgressStatus = require("./services/getLoadingProgressStatus");
 var createReportsLoadingStatesCollectionEntity = require("./services/createReportsLoadingStatesCollectionEntity");
 
 var reportLoadingStatesCollectionServices = {
+  getLoadingProgressStatus: (userId) => getLoadingProgressStatus(reportLoadingStatesCollection, userId),
   createReportsLoadingStatesCollectionEntity: (userId) => createReportsLoadingStatesCollectionEntity(reportLoadingStatesCollection, userId),
 };
 
