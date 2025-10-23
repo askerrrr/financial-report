7cec2deeb4d8b206c2c915439198c0351772502d
root <root@LAPTOP-345T3R3Q.localdomain>
2025-10-23 22:42:05 +0300

    fixed calculating sku qty into "calcQuantityPerSKU"

--- src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcServices/services/quantityPerSKU.js
+++ src/server/routes/reports/services/writeAndCalcReportDataFromWBAPI/calcServices/services/quantityPerSKU.js
@@ -1,3 +1,3 @@
-var calcQuantityPerSKU = async (report) => report.reduce((acc, i) => acc + i.quantity, 0);
+var calcQuantityPerSKU = async (report) => report.filter(item => item.doc_type_name === 'Продажа').reduce((acc, i) => acc + i.quantity, 0);
 
 module.exports = calcQuantityPerSKU;
