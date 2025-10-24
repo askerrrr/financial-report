566b988f91641c8f35afe2452b33a15cc6c0d8e9
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-10-24 14:25:19 +0300

    rename file/func: updateToken => saveWBToken

--- src/server/routes/WBToken/controllers/saveWBToken.js
+++ src/server/routes/WBToken/controllers/saveWBToken.js
@@ -1,6 +1,6 @@
-var updateToken = async (req, res, next) => {
+var saveWBToken = async (req, res, next) => {
   var { token } = req.body;
-    var userId = req.app.locals.userId
+  var userId = req.app.locals.userId;
   var { updateWBToken, getWBTokenByUserId } = req.app.locals.tokenCollectionServices;
 
   var currentToken = await getWBTokenByUserId(userId);
@@ -9,10 +9,9 @@ var updateToken = async (req, res, next) => {
     return res.sendStatus(304);
   }
 
-
   var success = await updateWBToken(userId, token);
 
   return success ? res.sendStatus(200) : res.sendStatus(500);
 };
 
-module.exports = updateToken;
+module.exports = saveWBToken;
