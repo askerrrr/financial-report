e21ccc5ded37bd7609ea8dbf62d5610d6fd34f05
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-10-24 14:47:49 +0300

    created collection to goods

--- src/server/database/connections/index.js
+++ src/server/database/connections/index.js
@@ -2,6 +2,7 @@ var { connection } = require("../index");
 
 var skuCollection = connection.model("Sku", require("../schemas/skus"));
 var userCollection = connection.model("User", require("../schemas/users"));
+var goodsCollection = connection.model("Googs", require("../schemas/goods"));
 var tokenCollection = connection.model("Token", require("../schemas/token"));
 var taxParamsCollection = connection.model("Tax_Param", require("../schemas/taxParams"));
 var reportCollection = connection.model("Report", require("../schemas/reports").reportsSchema);
@@ -11,9 +12,10 @@ var reportLoadingStatesCollection = connection.model("Report_Loading_States", re
 module.exports = {
   skuCollection,
   userCollection,
+  goodsCollection,
   reportCollection,
   tokenCollection,
   taxParamsCollection,
   reportsTreeCollection,
-  reportLoadingStatesCollection
+  reportLoadingStatesCollection,
 };
