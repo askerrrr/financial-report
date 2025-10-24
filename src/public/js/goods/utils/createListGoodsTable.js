a6b4f41030818ed2fd00cbf50cb23eac9e9c212d
root <root@LAPTOP-345T3R3Q.localdomain>
2025-10-24 22:02:17 +0300

    writed the logic for creating "td" elements for listGoods in the "createListGoodsTable" function

--- src/public/js/goods/utils/createListGoodsTable.js
+++ src/public/js/goods/utils/createListGoodsTable.js
@@ -1,3 +1,23 @@
-var createListGoodsTable = async (listGoods) => {};
+import createTdElement from "../../report/table/services/createTdElement.js";
+import createInputField from "../../report/table/services/createInputField.js";
+
+var createListGoodsTable = async (listGoods) => {
+  var tbody = document.getElementById("list-goods-tbody");
+
+  for (var item of listGoods) {
+    var { skuName, price, discount, discountedPrice, clubDiscountedPrice } = item;
+
+    var tr = document.createElement("tr");
+    var skuNameTd = createTdElement(skuName, "skuName");
+    var priceTd = createTdElement(price, "price");
+    var discountTd = createTdElement(discount, "discount");
+    var discountedPriceTd = createTdElement(discountedPrice, "discountedPrice");
+    var clubDiscountedPriceTd = createTdElement(clubDiscountedPrice, "clubDiscountedPrice");
+
+    tr.append(skuNameTd, priceTd, discountTd, discountedPriceTd, clubDiscountedPriceTd);
+
+    tbody.append(tr);
+  }
+};
 
 export default createListGoodsTable;
