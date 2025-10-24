28ec868cc9dcfac5e085b8aedd384ad881ca6305
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-10-24 14:56:31 +0300

    added skuSchema to /database/schemas/goods.js

--- src/server/database/schemas/goods.js
+++ src/server/database/schemas/goods.js
@@ -1,5 +1,17 @@
 var { Schema } = require("mongoose");
 
+var skuSchema = new Schema(
+  {
+    id: { type: Number, required: true },
+    skuName: { type: String, required: true },
+    price: { type: Number, required: false },
+    discount: { type: Number, required: false },
+    clubDiscount: { type: Number, required: false },
+    discountedPrice: { type: Number, required: false },
+  },
+  { _id: false }
+);
+
 var goodsSchema = new Schema({});
 
 module.exports = goodsSchema;
