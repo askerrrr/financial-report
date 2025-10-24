c1ac1cd8ddd9b4d75674b0c4ce7e85754479e5e0
root <root@LAPTOP-345T3R3Q.localdomain>
2025-10-24 22:10:23 +0300

    in the "getListGoods" controller, I wrote the logic for retrieving data from the database and sending it to the client

--- src/server/routes/goods/controllers/getListGoods.js
+++ src/server/routes/goods/controllers/getListGoods.js
@@ -1,3 +1,10 @@
-var getListGoods = async (req, res, next) => {};
+var getListGoods = async (req, res, next) => {
+  var userId = req.params.userId;
+  var { getListGoodsFromDb } = req.app.locals.goodsCollectionServices;
+
+  var { listGoods } = await getListGoodsFromDb(userId);
+
+  return res.json({ listGoods });
+};
 
 module.exports = getListGoods;
