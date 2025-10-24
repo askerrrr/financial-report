c420fd79988afaef5d37ea375d5a10f3b293bebc
root <root@LAPTOP-345T3R3Q.localdomain>
2025-10-24 22:06:53 +0300

    added func "getListGoods" in the "/public/js/goods/index.js", implemented him into "showListGoodsTable" func

--- src/public/js/goods/index.js
+++ src/public/js/goods/index.js
@@ -1,3 +1,24 @@
-var showListGoodsTable = async () => {};
+import createListGoodsTable from "./utils/createListGoodsTable.js";
+
+var getListGoods = async () => {
+  var userId = document.cookie.split("=")[1];
+  var url = "/goods/" + userId;
+
+  var res = await fetch(url);
+
+  if (!res.ok) {
+    alert("some error message");
+    return;
+  }
+
+  var { listGoods } = await res.json();
+
+  return { listGoods };
+};
+
+var showListGoodsTable = async () => {
+  var { listGoods } = await getListGoods();
+  createListGoodsTable(listGoods);
+};
 
 showListGoodsTable();
