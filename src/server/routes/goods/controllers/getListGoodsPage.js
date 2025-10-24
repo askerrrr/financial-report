d474e9f2a381326c3a64adecbf5ce0e30fd478f3
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-10-24 13:54:21 +0300

    implemented the sending of the goods/index.html HTML page

--- src/server/routes/goods/controllers/getListGoodsPage.js
+++ src/server/routes/goods/controllers/getListGoodsPage.js
@@ -1,3 +1,5 @@
-var getListGoodsPage = async (req, res, next) => {};
+var { join } = require("node:path");
+
+var getListGoodsPage = async (req, res, next) => res.sendFile(join(__dirname, "../../../../public/html/goods/index.html"));
 
 module.exports = getListGoodsPage;
