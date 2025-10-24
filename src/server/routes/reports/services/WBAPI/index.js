73fadfe004623c14a64a730c0bb692930a240616
root <root@LAPTOP-345T3R3Q.localdomain>
2025-10-25 00:22:48 +0300

    implemented func "setPricesAndDiscounts" as wbapi object method

--- src/server/routes/reports/services/WBAPI/index.js
+++ src/server/routes/reports/services/WBAPI/index.js
@@ -2,5 +2,6 @@ var wbapi = {};
 
 wbapi.getReports = require("./getReports");
 wbapi.getListGoods = require("./getGoodsListFromWBAPI");
+wbapi.setPricesAndDiscounts = require("./setPriceAndDiscount");
 
 module.exports = wbapi;
