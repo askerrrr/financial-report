7f56bd8882add5dbda0d25a508637793c13cd369
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-10-27 10:30:11 +0300

    added args event, cb to createButton func

--- src/public/js/goods/utils/createButton.js
+++ src/public/js/goods/utils/createButton.js
@@ -1,9 +1,13 @@
-var createButton = (buttonText, className, id) => {
+var createButton = (buttonText, className, id, event, cb) => {
   var button = document.createElement("button");
 
   button.id = id;
   button.className = className;
   button.textContent = buttonText;
+
+  if (event && cb) {
+    button.addEventListener(event, cb);
+  }
 
   return { button };
 };
