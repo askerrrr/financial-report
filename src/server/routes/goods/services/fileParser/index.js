20fb8642074b657d16736719cc1d0bef3a0917c9
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-11-01 09:53:52 +0300

    finished writing the fileParser function and returned the { pricesAndDiscounts } object

--- src/server/routes/goods/services/fileParser/index.js
+++ src/server/routes/goods/services/fileParser/index.js
@@ -4,6 +4,32 @@ var fileParser = async (buffer) => {
   var wb = new Exceljs.Workbook();
   await wb.xlsx.load(buffer);
   var ws = wb.getWorksheet("Лист1");
+
+  columnNum = 2;
+  var pricesAndDiscounts = [];
+
+  while (columnNum) {
+    var row = [];
+
+    ws.getRow(columnNum).eachCell((cell) => {
+      var cellData = cell?.text;
+
+      if (typeof +cellData === "number" && !isNaN(+cellData)) {
+        row.push(+cellData);
+      } else {
+        row.push(cellData);
+      }
+    });
+
+    if (!row.length) {
+      break;
+    }
+
+    pricesAndDiscounts.push({ skuName: row[0], price: row[1], discount: row[2] });
+    columnNum++;
+  }
+
+  return { pricesAndDiscounts };
 };
 
 module.exports = fileParser;
