d2fc639ea4d89f3a703e8909aac6e75b06c26ce6
root <root@LAPTOP-345T3R3Q.localdomain>
2025-11-02 15:02:11 +0300

    the "showListGoodsTable" function was moved to a separate file /public/js/goods/showListGoodsTable.js

--- src/public/js/goods/index.js
+++ src/public/js/goods/index.js
@@ -1,39 +1,9 @@
 import fileUploadHandler from "./fileUploadHandler.js";
-import createListGoodsTable from "./utils/createListGoodsTable.js";
-import enableListGoodsTable from "./utils/enableListGoodsTable.js";
-import enableFlleUploadButton from "./utils/enableFlleUploadButton.js";
-import loadListGoodsButtonHandler from "./loadListGoodsButtonHandler.js";
-import enableLoadListGoodsButton from "./utils/enableLoadListGoodsButton.js";
+import showListGoodsTable from "./showListGoodsTable.js";
 
-var getListGoods = async () => {
-  var userId = document.cookie.split("=")[1];
-  var url = "/goods/api/" + userId;
 
-  var res = await fetch(url);
 
-  if (!res.ok) {
-    alert("some error message");
-    return;
-  }
-
-  var { listGoods } = await res.json();
-
-  return { listGoods };
-};
-
-var showListGoodsTable = async () => {
-  var { listGoods } = await getListGoods();
-
-  if (!listGoods.length) {
-    enableLoadListGoodsButton();
-    await loadListGoodsButtonHandler();
-    return;
-  }
-
-  enableListGoodsTable();
-  enableFlleUploadButton();
-  await createListGoodsTable(listGoods);
-};
 
 showListGoodsTable();
 fileUploadHandler();
+showListGoodsTable()
---
+++ src/public/js/goods/showListGoodsTable.js
@@ -0,0 +1,37 @@
+import createListGoodsTable from "./utils/createListGoodsTable.js";
+import enableListGoodsTable from "./utils/enableListGoodsTable.js";
+import enableFlleUploadButton from "./utils/enableFlleUploadButton.js";
+import loadListGoodsButtonHandler from "./loadListGoodsButtonHandler.js";
+import enableLoadListGoodsButton from "./utils/enableLoadListGoodsButton.js";
+
+var getListGoods = async () => {
+  var userId = document.cookie.split("=")[1];
+  var url = "/goods/api/" + userId;
+
+  var res = await fetch(url);
+
+  if (!res.ok) {
+    alert("some error message");
+    return;
+  }
+
+  var { listGoods } = await res.json();
+
+  return { listGoods };
+};
+
+var showListGoodsTable = async () => {
+  var { listGoods } = await getListGoods();
+
+  if (!listGoods.length) {
+    enableLoadListGoodsButton();
+    await loadListGoodsButtonHandler();
+    return;
+  }
+
+  enableListGoodsTable();
+  enableFlleUploadButton();
+  await createListGoodsTable(listGoods);
+};
+
+export default showListGoodsTable;
