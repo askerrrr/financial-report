e540b37cfbc66d799aa1789178248fc964eddb2f
root <root@LAPTOP-345T3R3Q.localdomain>
2025-11-03 21:39:49 +0300

    implemented db service "setWeeklyPricesAndDiscounts" in the "uploadPricesAndDiscountsFile" controller

--- src/server/routes/goods/controllers/uploadPricesAndDiscountsFile.js
+++ src/server/routes/goods/controllers/uploadPricesAndDiscountsFile.js
@@ -2,12 +2,14 @@ var { readWeeklyPricesFile } = require("../services/weeklyPrices/");
 
 var uploadPricesAndDiscountsFile = async (req, res, next) => {
   var { userId } = req.params;
-  var { getListGoodsFromDb } = req.app.locals.goodsCollectionServices;
+  var { getListGoodsFromDb, setWeeklyPricesAndDiscounts } = req.app.locals.goodsCollectionServices;
   var { listGoods } = await getListGoodsFromDb(userId);
 
   var fileBuffer = req.file.buffer;
 
   var { weeklyPricesAndDiscounts } = await readWeeklyPricesFile(fileBuffer, listGoods);
+  var success = await setWeeklyPricesAndDiscounts(userId, weeklyPricesAndDiscounts);
+  console.log({ success });
   return res.json({ weeklyPricesAndDiscounts });
 };
 
