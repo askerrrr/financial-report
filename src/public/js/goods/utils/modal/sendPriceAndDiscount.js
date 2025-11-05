cf81be6c3a5052dba222f0ef32070c348c2a1359
root <root@LAPTOP-345T3R3Q.localdomain>
2025-11-05 22:55:00 +0300

    added arg "skuName" to "sendPriceAndDiscount" util

--- src/public/js/goods/utils/modal/sendPriceAndDiscount.js
+++ src/public/js/goods/utils/modal/sendPriceAndDiscount.js
@@ -1,4 +1,4 @@
-var sendPriceAndDiscount = async (price, discount, checkedWeekDays) => {
+var sendPriceAndDiscount = async (skuName, price, discount, checkedWeekDays) => {
   var userId = document.cookie.split("=")[1];
 
   var url = "/goods/set-price-or-discount";
@@ -6,7 +6,7 @@ var sendPriceAndDiscount = async (price, discount, checkedWeekDays) => {
   var res = await fetch(url, {
     method: "POST",
     headers: { "content-type": "application/json" },
-    body: JSON.stringify({ userId, price, discount, checkedWeekDays }),
+    body: JSON.stringify({ userId, skuName, price, discount, checkedWeekDays }),
   });
 
   if (!res.ok) {
