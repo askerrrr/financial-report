2457bea941ce7a0f474262a9c3aba6eea63e459c
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-11-05 16:09:06 +0300

    added file/func peekWeekHandler to src\public\js\goods\utils\modal"

--- src/public/js/goods/utils/modal/index.js
+++ src/public/js/goods/utils/modal/index.js
@@ -5,6 +5,7 @@ import createPriceInput from "./createPriceInput.js";
 import createDiscountnput from "./createDiscountnput.js";
 import createDiscountedPriceField from "./createDiscountedPriceField.js";
 import peekWeek from "./peekWeek.js";
+import peekWeekHandler from "./peekWeekHandler.js";
 
 var createModal = (item) => {
   var modalTitle = (document.createElement("h3").textContent = "Артикул: " + item.skuName);
@@ -34,16 +35,7 @@ var createModal = (item) => {
   var saveBtnHandler = {
     event: "click",
     cb: () => {
-      var options = document.querySelectorAll("option");
-      var selectedOptionsValue = [];
-
-      for (var i of options) {
-        if (i.selected) {
-          selectedOptionsValue.push(i.label);
-        }
-      }
-
-      console.log({ selectedOptionsValue });
+      peekWeekHandler();
       modalOverlay.classList.remove("active");
       document.body.style.overflow = "auto";
       modalOverlay.remove();
---
+++ src/public/js/goods/utils/modal/peekWeekHandler.js
@@ -0,0 +1,5 @@
+var peekWeekHandler = async () => {
+  var checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
+};
+
+export default peekWeekHandler;
