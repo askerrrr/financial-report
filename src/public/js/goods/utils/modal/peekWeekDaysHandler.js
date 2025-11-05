48656cbeee61e042692b6d02fd2a51dcef60bcfb
root <root@LAPTOP-345T3R3Q.localdomain>
2025-11-05 18:23:57 +0300

    added code to peekWeekDaysHandler.js

--- src/public/js/goods/utils/modal/peekWeekDaysHandler.js
+++ src/public/js/goods/utils/modal/peekWeekDaysHandler.js
@@ -1,5 +1,18 @@
 var peekWeekDaysHandler = async () => {
-  var checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
+  var checkedCheckboxes = [...document.querySelectorAll("input[type=checkbox]:checked")]
+    .map((item) => item.id)
+    .map(Number);
+
+  if (!checkedCheckboxes.length) {
+    return { checkedWeekDays: [7] };
+  }
+
+  if (checkedCheckboxes.length > 1 && checkedCheckboxes.includes(7)) {
+    console.log("includes 7");
+    return { checkedWeekDays: [7] };
+  }
+
+  return { checkedWeekDays: checkedCheckboxes };
 };
 
 export default peekWeekDaysHandler;
