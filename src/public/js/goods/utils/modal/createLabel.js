496c45b2e4a99f365681cf2bfac16c89f89fea46
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-11-05 16:02:16 +0300

    removed console.log() from src\public\js\goods\utils\modal\createLabel.js

--- src/public/js/goods/utils/modal/createLabel.js
+++ src/public/js/goods/utils/modal/createLabel.js
@@ -5,8 +5,6 @@
 var createLabel = (input, text, wrapStatus) => {
   var label = document.createElement("label");
   label.htmlFor = input.id;
-  console.log(input.id);
-
   label.textContent = text;
 
   if (wrapStatus === "wrapinput - yes") {
