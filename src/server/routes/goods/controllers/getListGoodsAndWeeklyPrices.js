badbf241f9fcb1d2d7864ecb64d89287981dd7aa
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-11-06 14:01:29 +0300

    -implemented db service getWeeklyPricesAndDiscountsFromDb into src\server\routes\goods\controllers\getListGoodsAndWeeklyPrices.js, - added the weeklyPricesAndDiscounts object to be sent to the client in res.json

--- src/server/routes/goods/controllers/getListGoodsAndWeeklyPrices.js
+++ src/server/routes/goods/controllers/getListGoodsAndWeeklyPrices.js
@@ -1,10 +1,12 @@
 var getListGoodsAndWeeklyPrices = async (req, res, next) => {
   var userId = req.params.userId;
   var { getListGoodsFromDb } = req.app.locals.goodsCollectionServices;
+  var { getWeeklyPricesAndDiscountsFromDb } = req.app.locals.weeklyPricesAndDiscountsCollectionServices;
 
   var { listGoods } = await getListGoodsFromDb(userId);
+  var { weeklyPricesAndDiscounts } = await getWeeklyPricesAndDiscountsFromDb(userId);
 
-  return res.json({ listGoods });
+  return res.json({ listGoods, weeklyPricesAndDiscounts });
 };
 
 module.exports = getListGoodsAndWeeklyPrices;
