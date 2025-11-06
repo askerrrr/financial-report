14ed7fe6e2501b3cbc93c33e7a6ff7b65d72aa9e
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-11-06 11:20:37 +0300

    added db service setWeeklyPricesAndDiscountsToDb to src\server\database\collections\weeklyPricesAndDiscounts

--- src/server/database/collections/weeklyPricesAndDiscounts/index.js
+++ src/server/database/collections/weeklyPricesAndDiscounts/index.js
@@ -1,8 +1,11 @@
 var { weeklyPricesAndDiscountsCollection } = require("../../connections/");
 var getWeeklyPricesAndDiscounts = require("./services/getWeeklyPricesAndDiscounts");
+var setWeeklyPricesAndDiscounts = require("./services/setWeeklyPricesAndDiscounts");
 
 var weeklyPricesAndDiscountsCollectionServices = {
   getWeeklyPricesAndDiscountsFromDb: (userId) => getWeeklyPricesAndDiscounts(weeklyPricesAndDiscountsCollection, userId),
+  setWeeklyPricesAndDiscountsToDb: (userId, weeklyPricesAndDiscounts) =>
+    setWeeklyPricesAndDiscounts(weeklyPricesAndDiscountsCollection, userId, weeklyPricesAndDiscounts),
 };
 
 module.exports = weeklyPricesAndDiscountsCollectionServices;
