454c4776daa2c6770a673a8c49d590617392b0cd
root <root@LAPTOP-345T3R3Q.localdomain>
2025-11-09 20:58:12 +0300

    added file/func "getSelectedWeekDayIdFromSelector", implemented him into "peekWeekDays.js"

--- src/public/js/goods/utils/modal/peekWeekDays.js
+++ src/public/js/goods/utils/modal/peekWeekDays.js
@@ -1,15 +1,19 @@
 import createDiv from "./createDiv.js";
 import createLabel from "./createLabel.js";
 import createInput from "./createInput.js";
+import getSelectedWeekDayIdFromSelector from "./getSelectedWeekDayIdFromSelector.js";
 
 var weekDays = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
 
 var peekWeekDays = () => {
   var fieldset = document.createElement("fieldset");
+  var allDaysId = 7;
 
-  var input = createInput(7, "checkbox", "На всю неделю", "checkbox");
+  var input = createInput(allDaysId, "checkbox", "На всю неделю", "checkbox");
   input.checked = true;
+
   var label = createLabel(input, "На всю неделю", "wrapinput - yes");
+
   var div = createDiv();
   div.append(input, label);
 
@@ -18,16 +22,26 @@ var peekWeekDays = () => {
 
   fieldset.append(legeng, div);
 
-  var i = 0;
-  for (var day of weekDays) {
-    input = createInput(i, "checkbox", day, "checkbox");
-    label = createLabel(input, day, "wrapinput - yes");
+  var { selectedWeekDayId } = getSelectedWeekDayIdFromSelector();
+  if (selectedWeekDayId !== 7) {
+    input.checked = false;
+  }
+
+  var inputId = 0;
+  for (var dayName of weekDays) {
+    input = createInput(inputId, "checkbox", dayName, "checkbox");
+
+    if (inputId === selectedWeekDayId) {
+      input.checked = true;
+    }
+
+    label = createLabel(input, dayName, "wrapinput - yes");
     div = createDiv();
     div.append(input, label);
 
     fieldset.append(div);
 
-    i++;
+    inputId++;
   }
 
   return fieldset;
