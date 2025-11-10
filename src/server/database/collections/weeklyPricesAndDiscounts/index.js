aacadd6d9cafa498aa6fa96e50c8fd4374e7d3be
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-11-10 13:44:21 +0300

    added db service updatePriceAndDiscount, func createQueryAndOptions into src\server\database\collections\weeklyPricesAndDiscounts\services\updatePriceAndDiscount.js, implemented new db service into setPriceOrDiscounts controller

--- src/server/database/collections/weeklyPricesAndDiscounts/index.js
+++ src/server/database/collections/weeklyPricesAndDiscounts/index.js
@@ -1,10 +1,14 @@
 var { weeklyPricesAndDiscountsCollection } = require("../../connections/");
+var updatePriceAnddDiscount = require("./services/updatePriceAnddDiscount");
 var getWeeklyPricesAndDiscounts = require("./services/getWeeklyPricesAndDiscounts");
 var setWeeklyPricesAndDiscounts = require("./services/setWeeklyPricesAndDiscounts");
 var createWeeklyPricesAndDiscountsCollectionEntity = require("./services/createWeeklyPricesAndDiscountsCollectionEntity");
 
 var weeklyPricesAndDiscountsCollectionServices = {
   getWeeklyPricesAndDiscountsFromDb: (userId) => getWeeklyPricesAndDiscounts(weeklyPricesAndDiscountsCollection, userId),
+
+  updatePriceAnddDiscount: (userId, nmID, price, discount, checkedWeekDays) =>
+    updatePriceAnddDiscount(weeklyPricesAndDiscountsCollection, userId, nmID, price, discount, checkedWeekDays),
 
   setWeeklyPricesAndDiscountsToDb: (userId, weeklyPricesAndDiscounts) =>
     setWeeklyPricesAndDiscounts(weeklyPricesAndDiscountsCollection, userId, weeklyPricesAndDiscounts),
--- src/server/routes/goods/controllers/setPriceOrDiscounts.js
+++ src/server/routes/goods/controllers/setPriceOrDiscounts.js
@@ -1,3 +1,9 @@
-var setPriceOrDiscounts = async (req, res, next) => {};
+var setPriceOrDiscounts = async (req, res, next) => {
+  var { updatePriceAnddDiscount } = req.app.locals.weeklyPricesAndDiscountsCollectionServices;
+
+  var { userId, skuId, price, discount, checkedWeekDays } = req.body;
+  await updatePriceAnddDiscount(userId, skuId, price, discount, checkedWeekDays);
+  return res.sendStatus(200);
+};
 
 module.exports = setPriceOrDiscounts;
