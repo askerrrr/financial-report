35591b153f44f470052b6fd33c306d9bc334b906
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-11-10 13:54:41 +0300

    updated the getCheckedWeekDays function. If the checkbox with a value for the entire week is selected, I return an array [0, 1, 2, 3, 4, 5, 6], where each value is the index of a day of the week.

--- src/public/js/goods/utils/modal/getCheckedWeekDays.js
+++ src/public/js/goods/utils/modal/getCheckedWeekDays.js
@@ -1,14 +1,18 @@
 var getCheckedWeekDays = async () => {
-  var checkedCheckboxes = [...document.querySelectorAll("input[type=checkbox]:checked")]
-    .map(({ id }) => id)
-    .map(Number);
+  var checkedCheckboxes = [...document.querySelectorAll("input[type=checkbox]:checked")].map(({ id }) => id).map(Number);
+
+  var allWeekDaysIds = [0, 1, 2, 3, 4, 5, 6];
 
   if (!checkedCheckboxes.length) {
-    return { checkedWeekDays: [7] };
+    return { checkedWeekDays: allWeekDaysIds };
+  }
+
+  if (checkedCheckboxes.length === 1 && checkedCheckboxes.includes(7)) {
+    return { checkedWeekDays: allWeekDaysIds };
   }
 
   if (checkedCheckboxes.length > 1 && checkedCheckboxes.includes(7)) {
-    return { checkedWeekDays: [7] };
+    return { checkedWeekDays: allWeekDaysIds };
   }
 
   return { checkedWeekDays: checkedCheckboxes };
