8e654f509f64aff6080330092da7cb02fe8b409e
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-11-12 15:05:02 +0300

    updated func handleNonEmptyEnabledSkus

--- src/public/js/goods/showListGoodsTable.js
+++ src/public/js/goods/showListGoodsTable.js
@@ -30,7 +30,7 @@ var showListGoodsTable = async () => {
     return;
   }
 
-  handleNonEmptyEnabledSkus(enabledSku);
+  handleNonEmptyEnabledSkus(listGoods);
 };
 
 export default showListGoodsTable;
@@ -47,17 +47,16 @@ var handleEmptyEnabledSkus = async function (disabledSku) {
   await createSkusTable(disabledSku, "disabled-skus-tbody");
 };
 
-var handleNonEmptyEnabledSkus = async function (enabledSku) {
+var handleNonEmptyEnabledSkus = async function ({ enabledSku, disabledSku }) {
   enableSkusTable("enabled-skus-table");
+  enableSkusTable("disabled-skus-table");
   enableWeeklyPricesAndDiscountsFlleUploadButton();
   enableDownloadWeeklyPricesAndDiscountsFileButton();
   await createSkusTable(enabledSku, "enabled-skus-tbody");
+  await createSkusTable(disabledSku, "disabled-skus-tbody");
 };
 
-var handleNonEmptyWeeklyPricesAndDiscounts = async function (
-  { enabledSku, disabledSku },
-  weeklyPricesAndDiscounts
-) {
+var handleNonEmptyWeeklyPricesAndDiscounts = async function ({ enabledSku, disabledSku }, weeklyPricesAndDiscounts) {
   var { currentDayName, currentDayIndex } = getCurrentDayMSK();
 
   enableWeekDaysSelector();
