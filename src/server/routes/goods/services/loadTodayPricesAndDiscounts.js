55c7fbb830982b5901d2142723067166b3c8b28f
root <root@LAPTOP-345T3R3Q.localdomain>
2025-11-12 22:40:32 +0300

    added login to "loadTodayPricesAndDiscounts" func

--- src/server/routes/goods/services/loadTodayPricesAndDiscounts.js
+++ src/server/routes/goods/services/loadTodayPricesAndDiscounts.js
@@ -1,3 +1,18 @@
-var loadTodayPricesAndDiscounts = async () => {};
+var getCurrentDayMSK = require('./getCurrentDayMSK');
+var wbapi = require('../../reports/services/WBAPI')
+var {getAllUserWeeklyPricesAndDiscounts} = require("../../../database/collections/weeklyPricesAndDiscounts");
+
+var loadTodayPricesAndDiscounts = async () => {
+    var { currentDayIndex } = getCurrentDayMSK();
+    var data = await getAllUserWeeklyPricesAndDiscounts()
+    
+    for(var {userId, weeklyPricesAndDiscounts } of data) {
+        var todayData = weeklyPricesAndDiscounts[currentDayIndex]
+
+        if(todayData){
+            console.log({todayData})
+        }
+    }
+};
 
 module.exports = loadTodayPricesAndDiscounts;
