a34dc47f205f313006d992d2563be563f937a861
root <root@LAPTOP-345T3R3Q.localdomain>
2025-11-13 20:14:13 +0300

    deleted extra func from "getListGoodsAndWeekplyPrices" controller

--- src/server/routes/goods/controllers/getListGoodsAndWeeklyPrices.js
+++ src/server/routes/goods/controllers/getListGoodsAndWeeklyPrices.js
@@ -2,12 +2,10 @@ var splitSkuByDisabledStatus = require("../services/splitSkuByDisabledStatus");
 
 var getListGoodsAndWeeklyPrices = async (req, res, next) => {
   var userId = req.params.userId;
-  var { getListGoodsFromDb, getAllUserListGoodsIds } = req.app.locals.goodsCollectionServices;
+  var { getListGoodsFromDb } = req.app.locals.goodsCollectionServices;
   var { getWeeklyPricesAndDiscountsFromDb } =
     req.app.locals.weeklyPricesAndDiscountsCollectionServices;
-  var data = await getAllUserListGoodsIds();
 
-  console.log(...data);
   var { listGoods } = await getListGoodsFromDb(userId);
   var { listGoods } = splitSkuByDisabledStatus(listGoods);
   var { weeklyPricesAndDiscounts } = await getWeeklyPricesAndDiscountsFromDb(userId);
