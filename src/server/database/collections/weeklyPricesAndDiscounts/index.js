baaa2c7dff9ebcfa51a0627fef3c14c0882f057b
root <root@LAPTOP-345T3R3Q.localdomain>
2025-11-13 23:25:07 +0300

    added db service "getUploadId", implemented service "getUploadId" as "weeklyPricesAndDiscountsCollectionServices" object method

--- src/server/database/collections/weeklyPricesAndDiscounts/index.js
+++ src/server/database/collections/weeklyPricesAndDiscounts/index.js
@@ -1,5 +1,6 @@
 var { weeklyPricesAndDiscountsCollection } = require("../../connections/");
 var setUploadId  = require('./services/setUploadId')
+var getUploadId = require('./services/getUploadId'  )
 var updatePriceAndDiscount = require("./services/updatePriceAndDiscount");
 var getWeeklyPricesAndDiscounts = require("./services/getWeeklyPricesAndDiscounts");
 var setWeeklyPricesAndDiscounts = require("./services/setWeeklyPricesAndDiscounts");
@@ -13,6 +14,7 @@ var weeklyPricesAndDiscountsCollectionServices = {
   updatePriceAndDiscount: (userId, nmID, price, discount, checkedWeekDays) =>
     updatePriceAndDiscount(weeklyPricesAndDiscountsCollection, userId, nmID, price, discount, checkedWeekDays),
 
+  getUploadId: (userId) => getUploadId(weeklyPricesAndDiscountsCollection, userId),
   setUploadId: (userId, uploadId) => setUploadId(weeklyPricesAndDiscountsCollection, userId, uploadId),
 
   setWeeklyPricesAndDiscountsToDb: (userId, weeklyPricesAndDiscounts) =>
