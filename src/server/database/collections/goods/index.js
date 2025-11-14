cc54fc520fdfe45871c31cc68d386b1504788dca
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-11-14 13:44:53 +0300

    added new db service setPriceUpdateTimestampAndUpdateStatus, implemented new db service as goodsCollectionServices object method

--- src/server/database/collections/goods/index.js
+++ src/server/database/collections/goods/index.js
@@ -3,6 +3,7 @@ var saveListGoodsToDb = require("./services/saveListGoodsToDb");
 var getListGoodsFromDb = require("./services/getListGoodsFromDb");
 var getAllUserListGoodsIds = require("./services/getAllUserListGoodsIds");
 var updateSkuDisableStatus = require("./services/updateSkuDisableStatus");
+var setPriceUpdateTimestampAndUpdateStatus = require("./services/setPriceUpdateTimestampAndUpdateStatus");
 
 var createListGoodsCollectionEntity = require("./services/createListGoodsCollectionEntity");
 
@@ -13,11 +14,11 @@ var goodsCollectionServices = {
 
   saveListGoodsToDb: (userId, listGoods) => saveListGoodsToDb(goodsCollection, userId, listGoods),
 
-  updateSkuDisableStatusToDb: (userId, skuName, disabled) =>
-    updateSkuDisableStatus(goodsCollection, userId, skuName, disabled),
+  updateSkuDisableStatusToDb: (userId, skuName, disabled) => updateSkuDisableStatus(goodsCollection, userId, skuName, disabled),
 
-  createListGoodsCollectionEntity: (userId) =>
-    createListGoodsCollectionEntity(goodsCollection, userId),
+  createListGoodsCollectionEntity: (userId) => createListGoodsCollectionEntity(goodsCollection, userId),
+
+  setPriceUpdateTimestampAndUpdateStatus: (userId, priceData) => setPriceUpdateTimestampAndUpdateStatus(goodsCollection, userId, priceData),
 };
 
 module.exports = goodsCollectionServices;
---
+++ src/server/database/collections/goods/services/setPriceUpdateTimestampAndUpdateStatus.js
@@ -0,0 +1,37 @@
+var getLastModifiedDate = () => new Date(Date.now() + 3 * 60 * 60 * 1000);
+
+var createQuery = (priceData) => {
+  var query = {};
+  var arrayFilters = [];
+
+  var count = 0;
+  for (var { nmID, status } of priceData) {
+    var isPriceUpdated = status === 2;
+
+    /**
+     * 2 - the product is error-free, and the price and/or discount have been updated
+     * https://dev.wildberries.ru/openapi/work-with-products#tag/Ceny-i-skidki/paths/~1api~1v2~1history~1goods~1task/get
+     */
+
+    var priceStatusKey = `listGoods.$[elem${count}].isPriceUpdated`;
+    query[priceStatusKey] = isPriceUpdated;
+
+    var lastUpdated = getLastModifiedDate();
+    var lastUpdatedKey = `listGoods.$[elem${count}].lastUpdated`;
+    query[lastUpdatedKey] = lastUpdated;
+
+    var optionKey = `elem${count}.id`;
+    arrayFilters.push({ [optionKey]: nmID });
+
+    count++;
+  }
+  return { query, arrayFilters };
+};
+
+var setPriceUpdateTimestampAndUpdateStatus = async (collection, userId, priceData) => {
+  var { query, arrayFilters } = createQuery(priceData);
+  var result = await collection.updateOne({ userId }, { $set: query }, { arrayFilters });
+  return result;
+};
+
+module.exports = setPriceUpdateTimestampAndUpdateStatus;
