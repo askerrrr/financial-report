03bbe71cfb1624ee49bd49289014a09acc1fd99c
root <root@LAPTOP-345T3R3Q.localdomain>
2025-11-14 19:06:30 +0300

    update of the "updateDataIntoListGoods" controller: - implemented the "updateSkusFields" database service instead of "saveListGoodsToDb" and "addNewSkusToListGoods" if the "newSkus" array is not empty - implemented the "splitListGoodsByExistence" function instead of "mergeData" - removed the "mergeData" file and function

--- src/server/routes/pricesAndDiscounts/controllers/updateDataIntoListGoods.js
+++ src/server/routes/pricesAndDiscounts/controllers/updateDataIntoListGoods.js
@@ -1,14 +1,15 @@
-var mergeData = require("../services/mergeData");
 var wbapi = require("../../reports/services/WBAPI");
+var splitListGoodsByExistence = require("../services/splitListGoodsByExistence");
 var extractRequiredListGoodsData = require("../../goods/services/extractRequiredListGoodsData");
 
 var updateDataIntoListGoods = async (req, res, next) => {
   var { getWBTokenByUserId } = req.app.locals.tokenCollectionServices;
-  var { getAllUserListGoodsIds, saveListGoodsToDb } = req.app.locals.goodsCollectionServices;
+  var { getAllUserListGoodsIds, addNewSkusToListGoods, updateSkusFields } =
+    req.app.locals.goodsCollectionServices;
 
   var data = await getAllUserListGoodsIds();
 
-  for (var { userId, listGoodsIds, listGoodsIdsAndDisableStatuses } of data) {
+  for (var { userId, listGoodsIds } of data) {
     if (listGoodsIds.length) {
       var token = await getWBTokenByUserId(userId);
 
@@ -19,8 +20,13 @@ var updateDataIntoListGoods = async (req, res, next) => {
       );
 
       var { listGoods } = await extractRequiredListGoodsData(rawListGoods);
-      var { updatedListGoods } = mergeData(listGoodsIdsAndDisableStatuses, listGoods);
-      await saveListGoodsToDb(userId, updatedListGoods);
+      var { newSkus, updatedSkus } = splitListGoodsByExistence(listGoodsIds, listGoods);
+
+      if (newSkus.length) {
+        await addNewSkusToListGoods(userId, newSkus);
+      }
+
+      await updateSkusFields(userId, updatedSkus);
     }
   }
 };
