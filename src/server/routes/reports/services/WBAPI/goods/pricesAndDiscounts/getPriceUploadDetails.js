85ef98daed055201bdcb4ed136a79815368b890c
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-11-14 09:30:53 +0300

    added new service getPriceUploadDetails to wbapi, implemented func getPriceUploadDetails as wbapi object method

---
+++ src/server/routes/reports/services/WBAPI/goods/pricesAndDiscounts/getPriceUploadDetails.js
@@ -0,0 +1,19 @@
+var { WBAPIError } = require("../../../../../../customError");
+
+var getPriceUploadDetails = async (userId, uploadId, token) => {
+  try {
+    var url = `https://discounts-prices-api.wildberries.ru/api/v2/history/goods/task?limit=1000&uploadId=${uploadId}`;
+    var options = { method: "GET", headers: { "content-type": "application/json", Authorization: "Bearer " + token } };
+
+    var res = await fetch(url, options);
+
+    var json = await res.json();
+    var { historyGoods } = json.data;
+
+    return { historyGoods };
+  } catch (e) {
+    throw new WBAPIError(userId, res.status, e);
+  }
+};
+
+module.exports = getPriceUploadDetails;
--- src/server/routes/reports/services/WBAPI/index.js
+++ src/server/routes/reports/services/WBAPI/index.js
@@ -4,5 +4,6 @@ wbapi.getReports = require("./reports");
 wbapi.getListGoods = require("./goods/goodsInfo/getGoodsListFromWBAPI");
 wbapi.setPricesAndDiscounts = require("./goods/pricesAndDiscounts/setPriceAndDiscount");
 wbapi.gestProcessedPricingInfo = require("./goods/pricesAndDiscounts/getProcessedPricingInfo");
+wbapi.getPriceUploadDetails = require("./goods/pricesAndDiscounts/getPriceUploadDetails");
 wbapi.getPricesAndDiscountsByListGoods = require("./goods/pricesAndDiscounts/getPricesAndDiscountsByListGoods");
 module.exports = wbapi;
