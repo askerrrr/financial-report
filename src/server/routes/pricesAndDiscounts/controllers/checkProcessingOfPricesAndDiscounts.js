0fc11757acf8140259197be379119bedc062948c
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-11-14 13:46:34 +0300

    added logic to checkProcessingOfPricesAndDiscounts controller

--- src/server/routes/pricesAndDiscounts/controllers/checkProcessingOfPricesAndDiscounts.js
+++ src/server/routes/pricesAndDiscounts/controllers/checkProcessingOfPricesAndDiscounts.js
@@ -1,5 +1,17 @@
 var wbapi = require("../../reports/services/WBAPI");
 
-var checkProcessingOfPricesAndDiscounts = async (req, res, next) => {};
+var checkProcessingOfPricesAndDiscounts = async (req, res, next) => {
+  var { getWBTokenByUserId } = req.app.locals.tokenCollectionServices;
+  var { setPriceUpdateTimestampAndUpdateStatus } = req.app.locals.goodsCollectionServices;
+  var { getAllUserWeeklyPricesAndDiscounts } = req.app.locals.weeklyPricesAndDiscountsCollectionServices;
+
+  var data = await getAllUserWeeklyPricesAndDiscounts();
+
+  for (var { userId, uploadId } of data) {
+    var token = await getWBTokenByUserId(userId);
+    var { historyGoods } = await wbapi.getPriceUploadDetails(userId, uploadId, token);
+    await setPriceUpdateTimestampAndUpdateStatus(userId, historyGoods);
+  }
+};
 
 module.exports = checkProcessingOfPricesAndDiscounts;
