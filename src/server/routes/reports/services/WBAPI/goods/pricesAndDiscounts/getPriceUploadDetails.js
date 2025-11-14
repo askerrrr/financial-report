37e7d6c641d3eb1fcf67ee13e2e8896129756cb2
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-11-14 12:55:57 +0300

    fixed url query param in func getPriceUploadDetails : uploadId => uploadID

--- src/server/routes/reports/services/WBAPI/goods/pricesAndDiscounts/getPriceUploadDetails.js
+++ src/server/routes/reports/services/WBAPI/goods/pricesAndDiscounts/getPriceUploadDetails.js
@@ -2,7 +2,7 @@ var { WBAPIError } = require("../../../../../../customError");
 
 var getPriceUploadDetails = async (userId, uploadId, token) => {
   try {
-    var url = `https://discounts-prices-api.wildberries.ru/api/v2/history/goods/task?limit=1000&uploadId=${uploadId}`;
+    var url = `https://discounts-prices-api.wildberries.ru/api/v2/history/goods/task?limit=1000&uploadID=${uploadId}`;
     var options = { method: "GET", headers: { "content-type": "application/json", Authorization: "Bearer " + token } };
 
     var res = await fetch(url, options);
