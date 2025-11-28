29e9d27969f909d5b68c9d4379d7cca925f0f3f9
asker <askersultanov666@gmail.com>
2025-11-28 18:37:26 +0300

    added "skuName" to selected fields into "getSkusLastCostPrice"

--- src/server/database/collections/goods/services/getSkusLastCostPrice.js
+++ src/server/database/collections/goods/services/getSkusLastCostPrice.js
@@ -1,5 +1,5 @@
 var getSkusLastCostPrice = async (collection, userId) => {
-  var data = await collection.findOne({ userId }, { "listGoods.lastCostPrice": 1, "listGoods.id": 1, _id: 0 });
+  var data = await collection.findOne({ userId }, { "listGoods.lastCostPrice": 1, "listGoods.id": 1, "listGoods.skuName": 1, _id: 0 });
 
   return { skusLastCostPrice: data.listGoods };
 };
