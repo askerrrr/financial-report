5c86e7fde93c38a3b4ba652091b458ad4d0cc16c
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-11-28 14:00:23 +0300

    implemented the addition of products that were deleted (including from the shopping cart) from financial reports to the database.

--- src/server/routes/reports/services/different/reportsProcessing.js
+++ src/server/routes/reports/services/different/reportsProcessing.js
@@ -1,6 +1,7 @@
 var wbapi = require("../WBAPI");
 var sortYearsTree = require("./sortYearTree");
 var dbutils = require("../../../../database/collections");
+var addNewSkusToListGoods = require("./addNewSkusToListGoods");
 var insertReportToReportTree = require("../reportTreeBuilder");
 var parseReports = require("../writeAndCalcReportDataFromWBAPI");
 var schemaVersioning = require("../../../../database/migration/schemaVersioning/reportsCollection");
@@ -8,6 +9,7 @@ var schemaVersioning = require("../../../../database/migration/schemaVersioning/
 var reportsProcessing = async (userId, dateFrom, dateTo, session) => {
   var { saveReportToDb } = dbutils.reportCollectionServices;
   var { getWBTokenByUserId } = dbutils.tokenCollectionServices;
+  var { getListGoodsFromDb, saveListGoodsToDb } = dbutils.goodsCollectionServices;
   var { getReportTree, updateReportTree } = dbutils.reportsTreeCollectionServices;
   var { addNewTaxYearToDb, changeTaxParamsToDb } = dbutils.taxParamsCollectionServices;
 
@@ -22,6 +24,10 @@ var reportsProcessing = async (userId, dateFrom, dateTo, session) => {
 
   var { taxRate, paidTaxAmount } = await addNewTaxYearToDb(userId, year, session);
   var { report, skuNamesAndIds } = await parseReports(taxRate, reports);
+
+  var { listGoods } = await getListGoodsFromDb(userId);
+  var { updatedListGoods } = await addNewSkusToListGoods(listGoods, skuNamesAndIds);
+
   paidTaxAmount += report.totalTaxAmount;
 
   report.dateTo = dateTo;
@@ -33,6 +39,7 @@ var reportsProcessing = async (userId, dateFrom, dateTo, session) => {
 
   await saveReportToDb(userId, report, session);
   await updateReportTree(userId, sortedYears, session);
+  await saveListGoodsToDb(userId, updatedListGoods, session);
   await changeTaxParamsToDb(userId, year, session, { paidTaxAmount });
 
   return { reportId, year, month, dateFrom, dateTo, totalTaxAmount: report.totalTaxAmount };
