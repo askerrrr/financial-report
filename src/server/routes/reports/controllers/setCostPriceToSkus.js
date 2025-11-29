9b414d18f2420517458c480adecd45c01648924c
asker <askersultanov666@gmail.com>
2025-11-29 13:38:10 +0300

    implemented controller "setCostPriceToSkus"

--- src/server/routes/reports/controllers/setCostPriceToSkus.js
+++ src/server/routes/reports/controllers/setCostPriceToSkus.js
@@ -1,6 +1,56 @@
+var calc = require("../services/calcServices");
+var { connection } = require("../../../database");
+var setCostPriceToSkuBySkuIndex = require("../services/different/setCostPriceToSkuBySkuIndex");
+
 var setCostPriceToSkus = async (req, res, next) => {
-  console.log(req.body);
-  return res.sendStatus(200);
+  var { userId, reportId, taxYear, costPrices } = req.body;
+  var { saveUpdatedReport, getReportById } = req.app.locals.reportCollectionServices;
+  var { getTaxParamsFromDb, changeTaxParamsToDb } = req.app.locals.taxParamsCollectionServices;
+
+  var session = await connection.startSession();
+
+  var total = {};
+  var skusDataToClient = [];
+  var totalPaidInsuranceFee = 0;
+
+  try {
+    await session.withTransaction(async () => {
+      var taxParams = await getTaxParamsFromDb(userId, taxYear);
+      var { report } = await getReportById(userId, reportId);
+      var { skus, ...totalParams } = report;
+
+      for (var { id, lastCostPrice } of costPrices) {
+        var skuIndex = skus.findIndex((sku) => sku.id === id);
+
+        var { updatedSKUS, updatedSKU } = setCostPriceToSkuBySkuIndex(skus, skuIndex, lastCostPrice);
+        var { skuWithCalculatedParams, insuranceFeePercentage, recalculatedPaidInsuranceFee } = calc.sku.restParams(updatedSKU, taxParams);
+        totalPaidInsuranceFee += recalculatedPaidInsuranceFee;
+
+        updatedSKUS[skuIndex] = skuWithCalculatedParams;
+        var { profitMargin, finalProfit } = skuWithCalculatedParams;
+
+        skusDataToClient.push({ sku: { skuIndex, data: { profitMargin, finalProfit } } });
+      }
+
+      var updatedReport = await calc.total.restParams(totalParams, updatedSKUS);
+      var updatedTaxParams = { insuranceFeePercentage, paidInsuranceFee: totalPaidInsuranceFee };
+
+      await saveUpdatedReport(userId, reportId, updatedReport, session);
+      await changeTaxParamsToDb(userId, taxYear, session, updatedTaxParams);
+
+      var { totalFinalProfit, totalProfitMargin } = updatedReport;
+      total = { totalFinalProfit, totalProfitMargin };
+    });
+
+    return res.json({ skusDataToClient, total });
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(304);
+  } finally {
+    if (session) {
+      await session.endSession();
+    }
+  }
 };
 
 module.exports = setCostPriceToSkus;
