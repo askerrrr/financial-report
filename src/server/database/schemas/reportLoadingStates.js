309bc6376b92235555ce1d05e4bf1c6796bd8d96
asker <askersultanov666@gmail.com>
2025-11-29 14:41:34 +0300

    removed unused properties from "report_loading_states_schema"

--- src/server/database/schemas/reportLoadingStates.js
+++ src/server/database/schemas/reportLoadingStates.js
@@ -3,9 +3,6 @@ var { Schema } = require("mongoose");
 var report_loading_states_schema = new Schema({
   userId: { type: String, required: true },
   requiredReportPeriods: { type: Array, required: false },
-  loadedReports: { type: Array, required: false },
-  remainingReports: { type: Array, required: false },
-  failedReportsQueue: { type: Array, required: false },
   reportsQueue: { type: Array, required: false },
   loadingInProgress: { type: Boolean, default: false },
   abandonedReports: { type: Array, required: false },
