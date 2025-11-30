0cf93b7841bcee6238b2ab4e097754f9366f7610
asker <askersultanov666@gmail.com>
2025-11-30 11:44:04 +0300

    added file/func "splitReportSkusByYear"

---
+++ src/server/routes/reports/services/different/splitReportSkusByYear.js
@@ -0,0 +1,19 @@
+var splitReportSkusByYear = (report) => {
+  var startYearSkus = [];
+  var endYearSkus = [];
+
+  for (var sku of report) {
+    var startYear = sku.date_from.split("-")[0];
+    var saleYear = sku.sale_dt.split("-")[0];
+
+    if (startYear === saleYear) {
+      startYearSkus.push(sku);
+    } else {
+      endYearSkus.push(sku);
+    }
+  }
+
+  return { startYearSkus, endYearSkus };
+};
+
+module.exports = splitReportSkusByYear;
