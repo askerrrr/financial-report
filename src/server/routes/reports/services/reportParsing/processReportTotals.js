be717ffc38f6640969c0f728b5cc5766b686b511
root <root@LAPTOP-345T3R3Q.localdomain>
2025-12-10 20:56:16 +0300

    updated service "processReportTotals"

--- src/server/routes/reports/services/reportParsing/processReportTotals.js
+++ src/server/routes/reports/services/reportParsing/processReportTotals.js
@@ -4,16 +4,16 @@ var truncateTotals = require("./truncateTotals");
 var processReportTotals = async (skus, propPostfix = "") => {
   var report = {};
 
-  report["totalFines" + propPostfix] = calc.sum(skus, "fines" + propPostfix);
-  report["totalProfit" + propPostfix] = calc.sum(skus, "profit" + propPostfix);
-  report["totalTaxAmount" + propPostfix] = calc.sum(skus, "tax" + propPostfix);
-  report["totalDeliveryCost" + propPostfix] = calc.sum(skus, "deliveryCost" + propPostfix);
-  report["totalReturnAmount" + propPostfix] = calc.sum(skus, "returnAmount" + propPostfix);
-  report["totalRetailAmount" + propPostfix] = calc.sum(skus, "retailAmount" + propPostfix);
-  report["totalPaidAcceptance" + propPostfix] = calc.sum(skus, "acceptance" + propPostfix);
-  report["totalAdditionalPayment" + propPostfix] = calc.sum(skus, "additionalPayment" + propPostfix);
-  report["totalDeductionOrPayment" + propPostfix] = calc.sum(skus, "deductionOrPayment" + propPostfix);
-  report["totalSellerPayoutAmount" + propPostfix] = calc.sum(skus, "sellerPayoutAmount" + propPostfix);
+  report["totalFines" + propPostfix] = calc.sum(skus, "fines" + propPostfix, "truncate-on");
+  report["totalProfit" + propPostfix] = calc.sum(skus, "profit" + propPostfix, "truncate-on");
+  report["totalTaxAmount" + propPostfix] = calc.sum(skus, "tax" + propPostfix, "truncate-on");
+  report["totalDeliveryCost" + propPostfix] = calc.sum(skus, "deliveryCost" + propPostfix, "truncate-on");
+  report["totalReturnAmount" + propPostfix] = calc.sum(skus, "returnAmount" + propPostfix, "truncate-on");
+  report["totalRetailAmount" + propPostfix] = calc.sum(skus, "retailAmount" + propPostfix, "truncate-on");
+  report["totalPaidAcceptance" + propPostfix] = calc.sum(skus, "acceptance" + propPostfix, "truncate-on");
+  report["totalAdditionalPayment" + propPostfix] = calc.sum(skus, "additionalPayment" + propPostfix, "truncate-on");
+  report["totalDeductionOrPayment" + propPostfix] = calc.sum(skus, "deductionOrPayment" + propPostfix, "truncate-on");
+  report["totalSellerPayoutAmount" + propPostfix] = calc.sum(skus, "sellerPayoutAmount" + propPostfix, "truncate-on");
 
   report = truncateTotals(report);
 
