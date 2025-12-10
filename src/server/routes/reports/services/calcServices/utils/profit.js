c8d2d96df340bf99e6c937288cd0991e567137f6
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-12-10 13:59:31 +0300

    updated calc service calcProfit added arg propPostfix

--- src/server/routes/reports/services/calcServices/utils/profit.js
+++ src/server/routes/reports/services/calcServices/utils/profit.js
@@ -1,10 +1,10 @@
-var calcProfit = (sku) =>
-  sku.sellerPayoutAmount -
-  sku.fines -
-  sku.acceptance -
-  sku.storageCost -
-  sku.deliveryCost -
-  sku.additionalPayment -
-  sku.averageAdvertisingCost;
+var calcProfit = (sku, propPostfix = "") =>
+  sku["sellerPayoutAmount" + propPostfix] -
+  sku["fines" + propPostfix] -
+  sku["acceptance" + propPostfix] -
+  sku["storageCost" + propPostfix] -
+  sku["deliveryCost" + propPostfix] -
+  sku["additionalPayment" + propPostfix] -
+  sku["averageAdvertisingCost" + propPostfix];
 
 module.exports = calcProfit;
