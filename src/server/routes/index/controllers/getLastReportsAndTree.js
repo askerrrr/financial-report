ede8f72002349b2b7f99b39ae2322234b35edb4d
root <root@LAPTOP-345T3R3Q.localdomain>
2025-12-11 20:58:43 +0300

    deleted func treeIsEmpty

--- src/server/routes/index/controllers/getLastReportsAndTree.js
+++ src/server/routes/index/controllers/getLastReportsAndTree.js
@@ -1,9 +1,14 @@
 var getReportTreeDto = require("../services/getReportTreeDto");
 
-var treeIsEmpty = (tree) => !tree.length && tree.every((i) => !i.months.length);
-var getLastNonEmptyReportIds = (lastYear) => lastYear.months.find((item) => item?.reportIds.length).reportIds.map(({ reportId }) => reportId);
+var getLastNonEmptyReportIds = (lastYear) =>
+  lastYear.months.find((item) => item?.reportIds.length).reportIds.map(({ reportId }) => reportId);
 
-var projectonFields = ["reports.reportId", "reports.totalTaxAmount", "reports.totalFinalProfit", "reports.totalProductCosts"];
+var projectonFields = [
+  "reports.reportId",
+  "reports.totalTaxAmount",
+  "reports.totalFinalProfit",
+  "reports.totalProductCosts",
+];
 
 var getLastReportsAndTree = async (req, res, next) => {
   var userId = req.app.locals.userId;
@@ -13,16 +18,16 @@ var getLastReportsAndTree = async (req, res, next) => {
 
   var { reportTree } = await getReportTree(userId);
 
-  if (!treeIsEmpty(reportTree)) {
+  if (!reportTree.length) {
     return res.json({ lastReports: [], reportTree: [] });
   }
 
   var reportTreeDto = await getReportTreeDto(reportTree);
-  console.log({ reportTreeDto, lastReportIds });
+
   var lastYear = reportTreeDto[0];
   var lastReportIds = getLastNonEmptyReportIds(lastYear);
 
-  if (!lastReportIds.length) {
+  if (!lastReportIds || !lastReportIds.length) {
     return res.json({ lastReports: [], reportTree: [] });
   }
 
