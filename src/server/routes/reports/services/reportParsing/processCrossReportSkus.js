45e746dcb1206b5c5fdab9e16b30e3afd60909ac
root <root@LAPTOP-345T3R3Q.localdomain>
2025-12-13 20:58:50 +0300

    deleted empty console.log from processCrossReportSkus.js

--- src/server/routes/reports/services/reportParsing/processCrossReportSkus.js
+++ src/server/routes/reports/services/reportParsing/processCrossReportSkus.js
@@ -71,8 +71,6 @@ var processCrossReportSkus = async (reports, taxParams) => {
       nextYearPropPostfix
     );
 
-    console.log();
-
     var middleTaxRate = (startYearTaxParams.taxRate + endYearTaxParams.taxRate) / 2;
 
     var totalSkuData = await parseSku(name, totalSold, skuFilteredReport, paidStorageReport, middleTaxRate, {
