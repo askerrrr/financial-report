080d57e6f91f232dd7fde0d7eb68b2c43afbf4be
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-12-18 15:29:07 +0300

    added retail amount recalculation into recalculateTaxPamams func

--- src/server/routes/reports/services/different/recalculateTaxParams.js
+++ src/server/routes/reports/services/different/recalculateTaxParams.js
@@ -1,5 +1,6 @@
 var recalculateTaxParams = function (report, taxParams, propPostfix = "") {
   taxParams.paidTaxAmount += report["totalTaxAmount" + propPostfix];
+  taxParams.retailAmount += report["totalRetailAmount" + propPostfix];
 
   if (taxParams.paidTaxAmount >= 0) {
     taxParams.isInsuranceFeePaid = true;
