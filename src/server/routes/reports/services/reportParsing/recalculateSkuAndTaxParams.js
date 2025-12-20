259b72c1a9193192361411eb6ec211b01d18c556
root <root@LAPTOP-345T3R3Q.localdomain>
2025-12-20 12:56:21 +0300

    return old value "false" to "requiresAdditionalInsuranceFee" property from taxYear schemaj

--- src/server/routes/reports/services/reportParsing/recalculateSkuAndTaxParams.js
+++ src/server/routes/reports/services/reportParsing/recalculateSkuAndTaxParams.js
@@ -1,6 +1,29 @@
+var calcInsuranceFee = require("../calcServices/utils/insuranceFee");
+
 var recalculateSkuAndTaxParams = (sku, taxParams, skuPropPostfix = "") => {
-  taxParams.retailAmount += sku["retailAmount" + skuPropPostfix];
   taxParams.paidTaxAmount += sku["tax" + skuPropPostfix];
+  taxParams.retailAmount += sku["retailAmount" + skuPropPostfix];
+
+  if (taxParams.retailAmount > taxParams.excessIncomeForInsurance) {
+    taxParams.hasExcessIncomeForInsurance = true;
+
+    var oldRetailAmount = taxParams.retailAmount - sku["retailAmount" + skuPropPostfix];
+    var difference = taxParams.excessIncomeForInsurance - oldRetailAmount;
+
+    var additionalInsuranceFeeFromSku;
+
+    if (difference > 0) {
+      difference = sku["retailAmount" + skuPropPostfix] - difference;
+      additionalInsuranceFeeFromSku = calcInsuranceFee(difference, taxParams.excessInsuranceRate);
+    } else {
+      additionalInsuranceFeeFromSku = calcInsuranceFee(
+        sku["retailAmount" + skuPropPostfix],
+        taxParams.excessInsuranceRate
+      );
+    }
+
+    taxParams.additionalInsuranceFee += additionalInsuranceFeeFromSku;
+  }
 
   if (taxParams.paidTaxAmount <= 0) {
     sku["tax" + skuPropPostfix] = 0;
