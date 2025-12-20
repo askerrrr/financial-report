7b8233fdc7e1c0c4d9cfd3bc04bb1736d707f42c
root <root@LAPTOP-345T3R3Q.localdomain>
2025-12-20 12:52:10 +0300

    added property "maxInsuranceFee" to taxYear schema from src/server/database/schemas/taxParams.js

--- src/server/database/schemas/taxParams.js
+++ src/server/database/schemas/taxParams.js
@@ -13,6 +13,7 @@ var taxYear = new Schema(
     additionalInsuranceFee: { type: Number, default: 0 },
     requiresAdditionalInsuranceFee: { type: Boolean, default: false },
     excessIncomeForInsurance: { type: Number },
+    maxInsuranceFee: { type: Number },
     hasExcessIncomeForInsurance: { type: Boolean, default: false },
     excessInsuranceRate: { type: Number, default: 1 },
     schemaVersion: { type: Number },
