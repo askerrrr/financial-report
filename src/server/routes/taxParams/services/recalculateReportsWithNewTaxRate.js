314345f2f9f32fa6cd796c5b180b9b5d7434cb20
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-12-23 11:10:20 +0300

    added file/func recalculateReportsWithNewTaxRate to src\server\routes\taxParams\services\recalculateReportsWithNewTaxRate.js

---
+++ src/server/routes/taxParams/services/recalculateReportsWithNewTaxRate.js
@@ -0,0 +1,20 @@
+var recalculateReportsWithNewTaxRate = (reports, taxParams) => {
+  var finalProfit = 0;
+  var paidTaxAmount = -taxParams.mandatoryInsuranceFee;
+
+  for (var report of reports) {
+    for (sku of report.skus) {
+      sku.tax = calc.taxAmount(sku.retailAmount, newTaxRate);
+      paidTaxAmount += sku.tax;
+
+      if (sku.isCostPriceSet) {
+        sku.finalProfit = calc.finalProfit(sku.preTaxProfit, sku.insuranceFee, sku.tax, sku.additionalInsuranceFee);
+        finalProfit += sku.finalProfit;
+      }
+    }
+  }
+
+  return { finalProfit, paidTaxAmount, reports };
+};
+
+module.exports = recalculateReportsWithNewTaxRate;
