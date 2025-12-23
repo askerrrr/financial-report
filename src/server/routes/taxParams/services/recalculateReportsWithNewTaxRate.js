7ce8ae154fbf83cd6e8feac300522a246c03e02d
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-12-23 14:57:59 +0300

    updated func recalculateReportsWithNewTaxRate: added accounting for the report.crossesTaxYears value

--- src/server/routes/taxParams/services/recalculateReportsWithNewTaxRate.js
+++ src/server/routes/taxParams/services/recalculateReportsWithNewTaxRate.js
@@ -1,17 +1,53 @@
 var calc = require("../../reports/services/calcServices");
 
-var recalculateReportsWithNewTaxRate = (reports, paidTaxAmount, newTaxRate) => {
+var recalculateReportsWithNewTaxRate = (reports, paidTaxAmount, newTaxRate, taxYear) => {
   var finalProfit = 0;
+  var currentYearPostfix = "InCurrentYear";
+  var nextYearPostfix = "InNextYear";
 
   for (var report of reports) {
-    for (sku of report.skus) {
-      sku.tax = calc.taxAmount(sku.retailAmount, newTaxRate);
-      paidTaxAmount += sku.tax;
+    var postfix;
 
+    if (report.crossesTaxYears) {
+      var startYear = +report.dateFrom.split("-")[0];
+      postfix = startYear == taxYear ? currentYearPostfix : nextYearPostfix;
+    }
+
+    for (sku of report.skus) {
       if (sku.isCostPriceSet) {
-        sku.finalProfit = calc.finalProfit(sku.preTaxProfit, sku.insuranceFee, sku.tax, sku.additionalInsuranceFee);
-        finalProfit += sku.finalProfit;
+        if (report.crossesTaxYears) {
+          sku["tax" + postfix] = calc.taxAmount(sku["retailAmount" + postfix], newTaxRate);
+          sku.tax = sku.taxInCurrentYear + sku.taxInNextYear;
+
+          sku["finalProfit" + postfix] = calc.finalProfit(
+            sku["preTaxProfit" + postfix],
+            sku["insuranceFee" + postfix],
+            sku["tax" + postfix],
+            sku["additionalInsuranceFee" + postfix]
+          );
+          sku.finalProfit = sku.finalProfitInCurrentYear + sku.finalProfitInNextYear;
+
+          paidTaxAmount += sku["tax" + postfix];
+          finalProfit += sku["finalProfit" + postfix];
+        } else {
+          sku.tax = calc.taxAmount(sku.retailAmount, newTaxRate);
+          sku.finalProfit = calc.finalProfit(sku.preTaxProfit, sku.insuranceFee, sku.tax, sku.additionalInsuranceFee);
+
+          paidTaxAmount += sku.tax;
+          finalProfit += sku.finalProfit;
+        }
       }
+    }
+
+    if (postfix) {
+      report["totalTaxAmount" + postfix] = calc.sum(report.skus, "tax" + postfix, "truncate-on");
+      report.totalTaxAmount = report.totalTaxAmountInCurrentYear + report.totalTaxAmountInNextYear;
+
+      report["totalFinalProfit" + postfix] = calc.sum(report.skus, "finalProfit" + postfix, "truncate-on");
+      report.totalFinalProfit = report.totalFinalProfitInCurrentYear + report.totalFinalProfitInNextYear;
+    } else {
+      report.totalTaxAmount = calc.sum(report.skus, "tax", "truncate-on");
+      report.totalFinalProfit = calc.sum(report.skus, "finalProfit", "truncate-on");
     }
   }
 
