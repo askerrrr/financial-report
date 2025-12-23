533ed4cd900b5c813ee299d1d9bd8f75228e4c12
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-12-23 12:25:51 +0300

    filtered reports by tax year in the changeTaxParams controller

--- src/server/routes/taxParams/controllers/changeTaxParams.js
+++ src/server/routes/taxParams/controllers/changeTaxParams.js
@@ -8,7 +8,8 @@ var changeTaxParams = async (req, res, next) => {
   var { getReportsByUserId } = req.app.locals.reportCollectionServices;
   var { changeTaxParamsToDb } = req.app.locals.taxParamsCollectionServices;
 
-  var { reports } = await getReportsByUserId(userId);
+  var data = await getReportsByUserId(userId);
+  var reports = data.reports.filter((report) => report.recordTo.year === year);
   var success = await changeTaxParamsToDb(userId, year, (session = null), data);
 
   if (success) {
