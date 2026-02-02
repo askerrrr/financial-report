286989a3392736220711a9a633c2b12f3898c368
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-02-02 12:46:35 +0300

    updated downloadSkusMetrics controller

--- src/server/routes/goods/controllers/downloadSkusMetrics.js
+++ src/server/routes/goods/controllers/downloadSkusMetrics.js
@@ -1,3 +1,10 @@
-var downloadSkusMetrics = async (req, res, next) => {};
+var downloadSkusMetrics = async (req, res, next) => {
+  var { userId } = req.app.locals;
+  var { getListGoodsFromDb } = req.app.locals.goodsCollectionServices;
+
+  var { listGoods } = await getListGoodsFromDb(userId);
+
+  //   var skuMetricsFile = await generageSKuMetricsFile(listGoods);
+};
 
 module.exports = downloadSkusMetrics;
