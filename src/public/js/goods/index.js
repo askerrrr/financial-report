5af328e3ff58ee7f59658ebebedc86c8c21c89d1
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-02-02 12:43:37 +0300

    added file/func downloadSkusMetricsFileButtonHandler and implemented func into src\public\js\goods\index.js

--- src/public/js/goods/index.js
+++ src/public/js/goods/index.js
@@ -1,7 +1,9 @@
 import fileUploadHandler from "./fileUploadHandler.js";
 import showListGoodsTable from "./showListGoodsTable.js";
+import downloadSkusMetricsFileButtonHandler from "./downloadSkusMetricsFileButtonHandler.js";
 import downloadWeeklyPricesFileButtonHandler from "./downloadWeeklyPricesFileButtonHandler.js";
 
 showListGoodsTable();
 fileUploadHandler();
+downloadSkusMetricsFileButtonHandler();
 downloadWeeklyPricesFileButtonHandler();
