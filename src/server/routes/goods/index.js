c6e03f4de24b750367627f690062ec41317044bc
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-02-03 10:31:27 +0300

    fixed http method to /download-skus-metrics rout path: post => get

--- src/server/routes/goods/index.js
+++ src/server/routes/goods/index.js
@@ -10,12 +10,12 @@ var router = Router({ caseSensitive: true, strict: true });
 router.get("/", require("./controllers/getListGoodsPage"));
 router.get("/api/:userId", require("./controllers/getListGoodsAndWeeklyPrices"));
 router.get("/weekly-prices/:userId", require("./controllers/getWeeklyPricesFile"));
+router.get("/download-skus-metrics", require("./controllers/downloadSkusMetrics"));
 
 router.post("/set-price-or-discount", require("./controllers/newPriceApplyController"), require("./controllers/changeWeeklyPricesOrDiscounts"));
 router.post("/", require("./controllers/loadListGoods"));
 router.post("/change-prices-discounts/:userId", require("./controllers/changePricesAndDiscounts"));
 router.post("/change-sku-disable-status", require("./controllers/changeSkuDisableStatus"));
 router.post("/upload-prices-discount-file/:userId", upload.single("file"), require("./controllers/uploadPricesAndDiscountsFile"));
-router.post("/download-skus-metrics", require("./controllers/downloadSkusMetrics"));
 
 module.exports = router;
