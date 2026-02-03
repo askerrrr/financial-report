3b5d335297d5e3008baaaa250d2c528601853d0e
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-02-03 14:47:42 +0300

    added session to setWeeklyPricesAndDiscountsToDb db util

--- src/server/database/collections/weeklyPricesAndDiscounts/services/setWeeklyPricesAndDiscounts.js
+++ src/server/database/collections/weeklyPricesAndDiscounts/services/setWeeklyPricesAndDiscounts.js
@@ -1,5 +1,5 @@
-var setWeeklyPricesAndDiscounts = async (collection, userId, weeklyPricesAndDiscounts) => {
-  var result = await collection.updateOne({ userId }, { $set: { weeklyPricesAndDiscounts } });
+var setWeeklyPricesAndDiscounts = async (collection, userId, weeklyPricesAndDiscounts, session) => {
+  var result = await collection.updateOne({ userId }, { $set: { weeklyPricesAndDiscounts } }, { session: session });
   return result.acknowledged;
 };
 
