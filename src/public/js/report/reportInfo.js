046a2e7439284e3b0b566d3f9994f2dda8c95533
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-02-25 13:03:49 +0300

    updated the reportInfo function: added the display of the report financial accounting status

--- src/public/js/report/reportInfo.js
+++ src/public/js/report/reportInfo.js
@@ -1,10 +1,13 @@
-var reportInfo = ({ dateFrom, dateTo, skus }) => {
+var reportInfo = (report) => {
   var reportPeriodElement = document.getElementById("report-period");
-  var reportPediod = `${dateFrom} ----  ${dateTo}`;
+  var reportPediod = `${report.dateFrom} ----  ${report.dateTo}`;
   reportPeriodElement.textContent = reportPediod;
 
   var reportsItemsCountElement = document.getElementById("report-items-count");
-  reportsItemsCountElement.textContent = skus.length;
+  reportsItemsCountElement.textContent = report.skus.length;
+
+  var financialAccountingStatusElement = document.getElementById("financial-accounting-status");
+  financialAccountingStatusElement.textContent = report.isFinancesAccounted ? "Да" : "Нет";
 };
 
 export default reportInfo;
