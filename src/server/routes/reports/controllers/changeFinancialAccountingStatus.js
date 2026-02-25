5ff380f54f236b56399deaa72c1a8cc52c025539
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-02-25 15:21:35 +0300

    added db service updateReportFinancialAccountingStatus, implemented into changeFinancialAccountingStatus controller

--- src/server/routes/reports/controllers/changeFinancialAccountingStatus.js
+++ src/server/routes/reports/controllers/changeFinancialAccountingStatus.js
@@ -1,3 +1,12 @@
-var changeFinancialAccountingStatus = async (req, res) => {};
+var dbUtils = require("../../../database/collections");
+
+var changeFinancialAccountingStatus = async (req, res) => {
+  var { userId, reportId, newStatus } = req.body;
+
+  var { updateReportFinancialAccountingStatus } = dbUtils.reportCollectionServices;
+
+  await updateReportFinancialAccountingStatus(userId, reportId, newStatus);
+  return res.sendStatus(200);
+};
 
 module.exports = changeFinancialAccountingStatus;
