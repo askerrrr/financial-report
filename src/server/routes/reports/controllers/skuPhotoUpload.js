3f54146b2cf3f56d2101fb94eba321f3eec2e48a
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-03-02 14:16:50 +0300

    implemented joi schema into skuhotopload controller

--- src/server/routes/reports/controllers/skuPhotoUpload.js
+++ src/server/routes/reports/controllers/skuPhotoUpload.js
@@ -1,6 +1,15 @@
+var Joi = require("joi");
 var s3 = require("../services/s3");
 
+var schema = Joi.object({ skuName: Joi.string().required() });
+
 var skuPhotoUpload = async (req, res, next) => {
+  var { error } = schema.validate(req.params);
+
+  if (error) {
+    return res.sendStatus(400);
+  }
+
   var { skuName } = req.params;
   var buffer = req.file.buffer;
   var userId = req.app.locals.userId;
