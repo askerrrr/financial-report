3ccc800ce5b61873f3eac0558b15a2b83622b4d9
asker <askersultanov666@gmail.com>
2026-03-02 20:49:24 +0300

    added joi schema into "changeTaxParams" controller

--- src/server/routes/taxParams/controllers/changeTaxParams.js
+++ src/server/routes/taxParams/controllers/changeTaxParams.js
@@ -1,10 +1,39 @@
+var Joi = require('joi');
 var { dbClient } = require("../../../database");
 var getTaxParamKeyName = require("../services/getTaxParamKeyName");
 var defaultTaxParams = require("../../../database/defaultTaxParams");
 var recalculateReportsWithNewTaxRate = require("../services/recalculateReportsWithNewTaxRate");
 var recalculateReportsWithNewMandatoryInsuranceRate = require("../services/recalculateReportsWithNewMandatoryInsuranceRate");
 
+var dataObjectSchema = Joi.object({ taxRate: Joi.number(), mandatoryInsuranceFeeRate: Joi.number(),mandatoryInsuranceFee: Joi.number()});
+
+var oldTaxParamsObjectSchema = Joi.object({
+	year: Joi.number(),
+	    taxRate: Joi.number(),
+	    mandatoryInsuranceFee: Joi.number(),
+	    paidTaxAmount: Joi.number(),
+	    maxInsuranceFee: Joi.number(),
+	    retailAmount: Joi.number(),
+	    finalProfit: Joi.number(),
+	    isInsuranceFeePaid: Joi.boolean(),
+	    mandatoryInsuranceFeeRate: Joi.number(),
+	    hasExcessIncomeForInsurance: Joi.boolean(),
+	    mandatoryInsuranceFeeIsPaid: Joi.boolean(),
+	    additionalInsuranceFeeIsPaid: Joi.boolean(),
+	    excessInsuranceRate: Joi.number(),
+	    requiresAdditionalInsuranceFee: Joi.boolean(),
+	    additionalInsuranceFee: Joi.number(),
+});
+
+var schema = Joi.object({ year: Joi.number().required(), recalculate: Joi.boolean().required(), data: dataObjectSchema, oldTaxParams: oldTaxParamsObjectSchema});
+
 var changeTaxParams = async (req, res, next) => {
+  var { error } = schema.validate(req.body);
+  if (error) {
+	return res.sendStatus(400);
+  }
+
+
   var userId = req.app.locals.userId;
   var { year, oldTaxParams, recalculate, data } = req.body;
   var { changeTaxParamsToDb } = req.app.locals.taxParamsCollectionServices;
