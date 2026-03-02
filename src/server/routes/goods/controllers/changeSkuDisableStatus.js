e0fafb89d2e8e19d93acc84318dfefc6e4efd6d5
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-03-02 14:42:45 +0300

    implemented joi schema into changeSkuDisableStatus controller

--- src/server/routes/goods/controllers/changeSkuDisableStatus.js
+++ src/server/routes/goods/controllers/changeSkuDisableStatus.js
@@ -1,4 +1,19 @@
+var Joi = require("joi");
+
+var schema = Joi.object({
+  userId: Joi.string().required(),
+  skuName: Joi.string().required(),
+  disableStatus: Joi.boolean().required(),
+  nmID: Joi.number(),
+});
+
 var changeSkuDisableStatus = async (req, res, next) => {
+  var { error } = schema.validate(req.body);
+
+  if (error) {
+    return res.sendStatus(400);
+  }
+
   var { updateSkuDisableStatusToDb } = req.app.locals.goodsCollectionServices;
   var { userId, skuName, disableStatus } = req.body;
   var success = await updateSkuDisableStatusToDb(userId, skuName, disableStatus);
