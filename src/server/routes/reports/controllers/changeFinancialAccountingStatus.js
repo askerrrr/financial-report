62c775f17efc59dc3c74920dcc1492a3c8276939
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-03-02 14:32:06 +0300

    implemented joi schema into changeinancialccountingtatus controller

--- src/server/routes/reports/controllers/changeFinancialAccountingStatus.js
+++ src/server/routes/reports/controllers/changeFinancialAccountingStatus.js
@@ -1,6 +1,15 @@
+var Joi = require("joi");
 var dbUtils = require("../../../database/collections");
 
+var schema = Joi.object({ userId: Joi.string().required(), reportId: Joi.number().required(), newStatus: Joi.boolean().required() });
+
 var changeFinancialAccountingStatus = async (req, res) => {
+  var { error } = schema.validate(req.body);
+
+  if (error) {
+    return res.sendStatus(400);
+  }
+
   var { userId, reportId, newStatus } = req.body;
 
   var { updateReportFinancialAccountingStatus } = dbUtils.reportCollectionServices;
