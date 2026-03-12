e1f84d5db85f82aad2918ccb9fadb443912599b1
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-03-12 09:24:12 +0300

    When requesting tax parameters, I exclude future tax parameters for the next year.

--- src/server/routes/taxParams/controllers/getTaxParams.js
+++ src/server/routes/taxParams/controllers/getTaxParams.js
@@ -1,11 +1,14 @@
+var excludeFutureTaxParams = require("../services/excludeFutureTaxParams");
+
 var getTaxParams = async (req, res, next) => {
   var { getTaxParamsFromDb } = req.app.locals.taxParamsCollectionServices;
 
   var userId = req.app.locals.userId;
 
   var taxParams = await getTaxParamsFromDb(userId);
+  var { filteredTaxParams } = excludeFutureTaxParams(taxParams);
 
-  return res.json({ taxParams });
+  return res.json({ taxParams: filteredTaxParams });
 };
 
 module.exports = getTaxParams;
