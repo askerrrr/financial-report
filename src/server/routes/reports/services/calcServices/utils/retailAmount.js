5ccba841511519abf1bda889f9f0c0dea1a241cc
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-03-13 10:21:42 +0300

    removed filtering by report type in the calcRetailAmount function

--- src/server/routes/reports/services/calcServices/utils/retailAmount.js
+++ src/server/routes/reports/services/calcServices/utils/retailAmount.js
@@ -1,12 +1,10 @@
 var truncateNum = require("../../reportParsing/truncateNum");
 
 var calcRetailAmount = (report) => {
-  var reportFilteredByReportType = report.filter((item) => item.report_type === 1);
   var retailAmount =
-    reportFilteredByReportType.filter((item) => item.doc_type_name === "Продажа").reduce((acc, item) => acc + item.retail_amount, 0) -
-    reportFilteredByReportType.filter((item) => item.doc_type_name === "Возврат").reduce((acc, item) => acc + item.retail_amount, 0);
+    report.filter((item) => item.doc_type_name === "Продажа").reduce((acc, item) => acc + item.retail_amount, 0) -
+    report.filter((item) => item.doc_type_name === "Возврат").reduce((acc, item) => acc + item.retail_amount, 0);
 
-  console.log({ retailAmount });
   return truncateNum(retailAmount);
 };
 
