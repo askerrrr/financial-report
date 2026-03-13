8b9117a1f0b22a3832d074a4a77635a340580887
asker <askersultanov666@gmail.com>
2026-03-13 19:06:16 +0300

    in the "calcFinalProfit" function, I now take into account the value of "otherExpenses"

--- src/server/routes/reports/services/calcServices/utils/finalProfit.js
+++ src/server/routes/reports/services/calcServices/utils/finalProfit.js
@@ -1,7 +1,7 @@
 var truncateNum = require("../../reportParsing/truncateNum");
 
-var calcFinalProfit = (preTaxProfit, insuranceFee, tax = 0, additionalInsuranceFee = 0) => {
-  var finalProfit = preTaxProfit - insuranceFee - tax - additionalInsuranceFee;
+var calcFinalProfit = (preTaxProfit, insuranceFee, tax = 0, additionalInsuranceFee = 0, otherExpenses = 0) => {
+  var finalProfit = preTaxProfit - insuranceFee - tax - additionalInsuranceFee - otherExpenses;
   return truncateNum(finalProfit);
 };
 
