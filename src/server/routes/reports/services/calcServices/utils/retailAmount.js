c4368266ec47344070f1610b4bd720301ed73a8f
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-03-13 10:17:57 +0300

    fixed retailAmount calculation into parseSku func

--- src/server/routes/reports/services/calcServices/utils/retailAmount.js
+++ src/server/routes/reports/services/calcServices/utils/retailAmount.js
@@ -1,10 +1,12 @@
 var truncateNum = require("../../reportParsing/truncateNum");
 
 var calcRetailAmount = (report) => {
+  var reportFilteredByReportType = report.filter((item) => item.report_type === 1);
   var retailAmount =
-    report.filter((item) => item.doc_type_name === "Продажа").reduce((acc, item) => acc + item.retail_amount, 0) -
-    report.filter((item) => item.doc_type_name === "Возврат").reduce((acc, item) => acc + item.retail_amount, 0);
+    reportFilteredByReportType.filter((item) => item.doc_type_name === "Продажа").reduce((acc, item) => acc + item.retail_amount, 0) -
+    reportFilteredByReportType.filter((item) => item.doc_type_name === "Возврат").reduce((acc, item) => acc + item.retail_amount, 0);
 
+  console.log({ retailAmount });
   return truncateNum(retailAmount);
 };
 
