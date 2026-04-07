eedba6054473b262f30bb9511904b9bde28c8ff7
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-04-07 15:21:23 +0300

    fixed prop name: report[totalAdvertisingCost + propPostfix] => report[totalAdvertisingCosts + propPostfix]

---
+++ src/server/routes/reports/services/reportAsXLSXBuffer/services/getZeroedReportTotals.js
@@ -0,0 +1,23 @@
+var getZeroedReportTotals = () => {
+  return {
+    reportIds: "",
+    totalRetailAmount: 0,
+    totalSellerPayoutAmount: 0,
+    totalSold: 0,
+    totalDeliveryCost: 0,
+    totalStorageCost: 0,
+    totalPaidAcceptance: 0,
+    totalFines: 0,
+    totalDeductionOrPayment: 0,
+    totalAdvertisingCosts: 0,
+    totalOtherExpenses: 0,
+    totalProductsCosts: 0,
+    totalTaxableAmount: 0,
+    totalTaxAmount: 0,
+    totalProfitMargin: 0,
+    totalFinalProfit: 0,
+    totalReturnAmount: 0,
+  };
+};
+
+export default getZeroedReportTotals;
--- src/server/routes/reports/services/reportParsing/processReportTotals.js
+++ src/server/routes/reports/services/reportParsing/processReportTotals.js
@@ -14,7 +14,7 @@ var processReportTotals = async (skus, propPostfix = "") => {
   report["totalTaxableAmount" + propPostfix] = calc.sum(skus, "taxableAmount" + propPostfix, "truncate-on");
   report["totalRetailAmount" + propPostfix] = calc.sum(skus, "retailAmount" + propPostfix, "truncate-on");
   report["totalPaidAcceptance" + propPostfix] = calc.sum(skus, "acceptance" + propPostfix, "truncate-on");
-  report["totalAdvertisingCost" + propPostfix] = calc.sum(skus, "averageAdvertisingCost" + propPostfix, "truncate-on");
+  report["totalAdvertisingCosts" + propPostfix] = calc.sum(skus, "averageAdvertisingCost" + propPostfix, "truncate-on");
   report["totalAdditionalPayment" + propPostfix] = calc.sum(skus, "additionalPayment" + propPostfix, "truncate-on");
   report["totalDeductionOrPayment" + propPostfix] = calc.sum(skus, "deductionOrPayment" + propPostfix, "truncate-on");
   report["totalSellerPayoutAmount" + propPostfix] = calc.sum(skus, "sellerPayoutAmount" + propPostfix, "truncate-on");
