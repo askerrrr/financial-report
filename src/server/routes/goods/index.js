696fe17cc4a90cb4da5e4df3fe2cc3740da32668
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-06 15:45:40 +0300

    renamed controller: uploadToWBAPINewPricesAndDiscounts => setNewPricesAndDiscountsToSku

--- src/server/routes/goods/index.js
+++ src/server/routes/goods/index.js
@@ -7,7 +7,7 @@ import getListGoodsPage from "./controllers/getListGoodsPage.js";
 import getSkusMetricsFile from "./controllers/getSkusMetricsFile.js";
 import getWeeklyPricesFile from "./controllers/getWeeklyPricesFile.js";
 import changeSkuDisableStatus from "./controllers/changeSkuDisableStatus.js";
-import uploadToWBAPINewPricesAndDiscounts from "./controllers/uploadToWBAPINewPricesAndDiscounts.js";
+import setNewPricesAndDiscountsToSku from "./controllers/setNewPricesAndDiscountsToSku.js";
 import getListGoodsAndWeeklyPrices from "./controllers/getListGoodsAndWeeklyPrices.js";
 import downloadPricesAndDiscountsFile from "./controllers/downloadPricesAndDiscountsFile.js";
 import changeWeeklyPricesOrDiscounts from "./controllers/changeWeeklyPricesOrDiscounts.js";
@@ -26,6 +26,6 @@ router.get("/prices-discounts/file/:userId", getWeeklyPricesFile);
 router.post("/", loadListGoods);
 router.post("/sku-disable-status", changeSkuDisableStatus);
 router.post("/prices-discounts/download/:userId", upload.single("file"), downloadPricesAndDiscountsFile);
-router.post("/prices-discounts/upload", uploadToWBAPINewPricesAndDiscounts, changeWeeklyPricesOrDiscounts);
+router.post("/prices-discounts/upload", setNewPricesAndDiscountsToSku, changeWeeklyPricesOrDiscounts);
 
 export default router;
