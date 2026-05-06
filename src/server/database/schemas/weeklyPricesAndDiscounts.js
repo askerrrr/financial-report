fad748622328cc9830e7d8514225823f7b771c06
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-06 13:30:48 +0300

    return changes into weeklyPricesAndDiscountsSchema.js

--- src/server/database/schemas/weeklyPricesAndDiscounts.js
+++ src/server/database/schemas/weeklyPricesAndDiscounts.js
@@ -1,6 +1,6 @@
 import { Schema } from "mongoose";
 
-var priceAndDiscountSchema = new Schema(
+var weekDaySchema = new Schema(
   {
     nmID: { type: Number, required: true },
     price: { type: Number, required: true },
@@ -9,21 +9,10 @@ var priceAndDiscountSchema = new Schema(
   { _id: false },
 );
 
-var skuSchema = new Schema(
-  {
-    needToUpdate: { type: Boolean, default: true },
-    lastUpdatedTimestamp: { type: Date, default: 0 },
-    priceOrDiscountUpdateInterval: { type: String, default: "5m" },
-    priceOrDiscountUpdateIntervalInMs: { type: Number, default: 300000 },
-    data: { type: priceAndDiscountSchema, required: false },
-  },
-  { _id: false },
-);
-
 var weeklyPricesAndDiscountsSchema = new Schema({
   userId: { type: String, required: true },
   uploadId: { type: Number, required: false },
-  weeklyPricesAndDiscounts: [{ type: [skuSchema], required: false }],
+  weeklyPricesAndDiscounts: [{ type: [weekDaySchema], required: false }],
 });
 
 export default weeklyPricesAndDiscountsSchema;
