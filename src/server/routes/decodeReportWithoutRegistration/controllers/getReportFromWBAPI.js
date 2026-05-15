af65a1144d0eec842c9d4cf45dc9694bec0d87cd
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-15 09:29:28 +0300

    Changes in the src\server\routes\decodeReportWithoutRegistration\controllers\getReportFromWBAPI.js controller: - renamed the id value to userId - I no longer save reports in req.app.locals; instead, reports will be stored on the client in localStorage - fixed getting the reportId from the weekly financial report

--- src/server/routes/decodeReportWithoutRegistration/controllers/getReportFromWBAPI.js
+++ src/server/routes/decodeReportWithoutRegistration/controllers/getReportFromWBAPI.js
@@ -37,7 +37,6 @@ var getReportFromWBAPI = async (req, res, next) => {
   }
 
   var { dateFrom, dateTo, token, taxRate } = req.body;
-
   var existReportData = req.app.locals?.reports?.find((item) => item.report.dateFrom === dateFrom);
 
   if (existReportData) {
