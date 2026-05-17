cae782ed4bafa08e14a4d9115c8d05f37a87bb62
asker <askersultanov666@gmail.com>
2026-05-17 21:58:24 +0300

    updated wbapi service "getWeeklyFinancialReportFromWBAPI"

--- src/server/routes/reports/services/WBAPI/reports/getWeeklyFinancialReportFromWBAPI.js
+++ src/server/routes/reports/services/WBAPI/reports/getWeeklyFinancialReportFromWBAPI.js
@@ -22,17 +22,54 @@ var requriedFields = [
   "additionalPayment",
 ];
 
-var getWeeklyFinancialReportFromWBAPI = async (dateFrom, dateTo, token, userId) => {
-  var url = "https://finance-api.wildberries.ru/api/finance/v1/sales-reports/detailed";
+var period = "weekly";
+var maxNumberOfRows = 100_000;
+var NEXT_REQUEST_DELAY_MS = 65_000;
+var url = "https://finance-api.wildberries.ru/api/finance/v1/sales-reports/detailed";
+var nextRequestDelay = async () => new Promise((res) => setTimeout(res, NEXT_REQUEST_DELAY_MS));
 
-  var res = await fetch(url, {
+var doRequest = async (token, dateFrom, dateTo, period, rrdId, limit) =>
+  await fetch(url, {
     method: "POST",
-    body: JSON.stringify({ dateFrom, dateTo, fields: requriedFields }),
+    body: JSON.stringify({ dateFrom, dateTo, period, rrdId, limit }),
     headers: { Authorization: "Bearer " + token, "Content-Type": "application/json" },
   });
 
+var getWeeklyFinancialReportFromWBAPI = async (dateFrom, dateTo, token, userId) => {
+  var defaultRowNumber = 0;
+
+  var res = await doRequest(token, dateFrom, dateTo, period, defaultRowNumber, maxNumberOfRows);
+
   if (res.status === 200) {
     var report = await res.json();
+
+    var reportRowCount = report.length;
+
+    if (reportRowCount > maxNumberOfRows) {
+      var lastRowId = report[report.length - 1].rrdId;
+      defaultRowNumber = lastRowId;
+
+      var remainingReportPart = [];
+
+      var timerId = setInterval(async () => {
+        await nextRequestDelay();
+
+        res = await doRequest(token, dateFrom, dateTo, period, defaultRowNumber, maxNumberOfRows);
+
+        if (res.status === 200) {
+          remainingReportPart = await res.json();
+
+          lastRowId = remainingReportPart[remainingReportPart.length - 1].rrdId;
+          defaultRowNumber = lastRowId;
+
+          report.push(...remainingReportPart);
+        } else if (res.status === 204) {
+          timerId = null;
+
+          return report;
+        }
+      });
+    }
 
     return report;
   } else if (res.status === 204) {
