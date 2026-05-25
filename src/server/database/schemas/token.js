5e7b6c4f70df0bf5c7ddcf994e54d32080ca4ee2
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-25 13:33:29 +0300

    added new property tokenHasBeenRemoved to tokens collection

--- src/server/database/schemas/token.js
+++ src/server/database/schemas/token.js
@@ -3,6 +3,7 @@ import { Schema } from "mongoose";
 var tokenSchema = new Schema({
   userId: { type: String, required: true },
   token: { type: String, required: false, default: "" },
+  tokenHasBeenRemoved: { type: Boolean, default: false, required: true },
   schemaVersion: { type: Number },
 });
 
