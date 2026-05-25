52be00c05a1f8af02962524a0c4920e3cf969ba2
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-25 13:35:09 +0300

    updated db service removeTokenFromDb: now when deleting a token, the tokenHasBeenRemoved field is updated to true

--- src/server/database/collections/tokens/services/removeTokenFromDb.js
+++ src/server/database/collections/tokens/services/removeTokenFromDb.js
@@ -1,3 +1,3 @@
-var removeTokenFromDb = async (collection, userId) => await collection.updateOne({ userId }, { $set: { token: "" } });
+var removeTokenFromDb = async (collection, userId) => await collection.updateOne({ userId }, { $set: { token: "", tokenHasBeenRemoved: true } });
 
 export default removeTokenFromDb;
