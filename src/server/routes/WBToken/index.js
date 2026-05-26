eee5a36fd53b7f95b69bad4f01c0f9b00d3d8df7
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-26 13:54:46 +0300

    Added the url path for getting information about the token using the getTokenData controller

--- src/server/routes/WBToken/index.js
+++ src/server/routes/WBToken/index.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import schema from "./joiSchemas/index.js";
 import saveToken from "./controllers/saveToken.js";
 import removeToken from "./controllers/removeToken.js";
+import getTokenData from "./controllers/getTokenData.js";
 import tokenValidator from "./controllers/tokenValidator.js";
 import getWbTokenPage from "./controllers/getWbTokenPage.js";
 import checkTokenExists from "./controllers/checkTokenExists.js";
@@ -10,6 +11,7 @@ import joiSchemaValidator from "../../middleware/joiSchemaValidator.js";
 var router = Router({ caseSensitive: true, strict: true });
 
 router.get("/", getWbTokenPage);
+router.get("/:userId", getTokenData);
 router.post("/", tokenValidator, saveToken);
 router.get("/check-exist/:userId", checkTokenExists);
 router.delete("/", joiSchemaValidator(schema.removeToken), removeToken);
