48ee8e5fd4a7253cea630d133f09c3caa9c39e82
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-26 13:38:17 +0300

    Added the getWbTokenPage controller to display the page src\public\html\wbToken\index.html on the path url /wbtoken

--- src/server/routes/WBToken/index.js
+++ src/server/routes/WBToken/index.js
@@ -3,11 +3,13 @@ import schema from "./joiSchemas/index.js";
 import saveToken from "./controllers/saveToken.js";
 import removeToken from "./controllers/removeToken.js";
 import tokenValidator from "./controllers/tokenValidator.js";
+import getWbTokenPage from "./controllers/getWbTokenPage.js";
 import checkTokenExists from "./controllers/checkTokenExists.js";
 import joiSchemaValidator from "../../middleware/joiSchemaValidator.js";
 
 var router = Router({ caseSensitive: true, strict: true });
 
+router.get("/", getWbTokenPage);
 router.get("/:userId", checkTokenExists);
 router.post("/", tokenValidator, saveToken);
 router.delete("/", joiSchemaValidator(schema.removeToken), removeToken);
