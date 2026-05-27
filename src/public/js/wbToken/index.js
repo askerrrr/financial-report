6c9f36b29d1b9fcf4ef593409db05d64060e0b06
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-27 13:05:27 +0300

    Implemented a call to the getTokenData function in the main function in the file src\public\js\wbToken\index.js

--- src/public/js/wbToken/index.js
+++ src/public/js/wbToken/index.js
@@ -1,5 +1,9 @@
+import getTokenData from "./getTokenData.js";
+
 var userId = document.cookie.split("=")[1];
 
-var main = async () => {};
+var main = async () => {
+  var { tokenData } = await getTokenData(userId);
+};
 
 main();
