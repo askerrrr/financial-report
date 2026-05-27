2dcc4f92899631ed8bb2a88c2f76335b794ee5e2
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-27 15:07:23 +0300

    Removed the buttons and functions for working with the token from the main page

--- src/public/js/index/WBTokenLoaderHandler.js
+++
@@ -1,5 +0,0 @@
-import openWBTokenLoader from "./services/WBTokenLoader/index.js";
-
-var WBTokenLoaderHandler = () => (document.getElementById("token-button").onclick = () => openWBTokenLoader());
-
-export default WBTokenLoaderHandler;
