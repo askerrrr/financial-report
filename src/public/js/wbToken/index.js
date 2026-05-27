25956aecf707bdbaa76a26a5f6d639ec50dd3a24
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-27 13:43:02 +0300

    Added logic to functions from the file src\public\js\wbToken\index.js: - provided that the value of the tokenIsExist property of the TokenData object is then included in the table and data is entered into it

--- src/public/js/wbToken/index.js
+++ src/public/js/wbToken/index.js
@@ -1,9 +1,16 @@
 import getTokenData from "./getTokenData.js";
+import insertDataToTable from "./insertDataToTable.js";
+import { enableTokenDataTable } from "./toggleVisibilityOfTokenDataTable.js";
 
 var userId = document.cookie.split("=")[1];
 
 var main = async () => {
   var { tokenData } = await getTokenData(userId);
+
+  if (tokenData.tokenIsExist) {
+    enableTokenDataTable();
+    insertDataToTable(tokenData);
+  }
 };
 
 main();
