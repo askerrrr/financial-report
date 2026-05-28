3bb80184915f5f6b3adc1418dbdcae158c1624cf
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-28 14:23:39 +0300

    In the setNewPricesAndDiscountsToSku controller, I no longer use the updateLastUsedTimestamp database service - the lastUsed property is now updated when a token is requested.

--- src/server/routes/goods/controllers/setNewPricesAndDiscountsToSku.js
+++ src/server/routes/goods/controllers/setNewPricesAndDiscountsToSku.js
@@ -2,9 +2,11 @@ import { dbClient } from "../../../database/index.js";
 import wbapi from "../../reports/services/WBAPI/index.js";
 import dbUtils from "../../../database/collections/index.js";
 
+var updateLastUsedTimestampNow = true;
+
 var setNewPricesAndDiscountsToSku = async (req, res, next) => {
   var { updateSingleSku } = dbUtils.goodsCollectionServices;
-  var { getWBTokenByUserId, updateLastUsedTimestamp } = dbUtils.tokenCollectionServices;
+  var { getWBTokenByUserId } = dbUtils.tokenCollectionServices;
   var { userId, sku, setNewPriceNow, expectedPriceExists } = req.body;
 
   var session = await dbClient.startSession();
@@ -12,11 +14,10 @@ var setNewPricesAndDiscountsToSku = async (req, res, next) => {
   try {
     await session.withTransaction(async () => {});
     if (setNewPriceNow) {
-      var { token } = await getWBTokenByUserId(userId, session);
+      var { token } = await getWBTokenByUserId(userId, session, updateLastUsedTimestampNow);
       var data = [{ ...sku }];
 
       await updateSingleSku(userId, sku, session);
-      await updateLastUsedTimestamp(userId, session);
       await wbapi.setPricesAndDiscounts(userId, token, data);
     }
   } catch (e) {
