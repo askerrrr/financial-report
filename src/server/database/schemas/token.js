b1e8f216be72522e513aef2512743a4f4fdec70a
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-28 13:04:51 +0300

    added property lastUsed to tokenSchema

--- src/server/database/schemas/token.js
+++ src/server/database/schemas/token.js
@@ -3,6 +3,7 @@ import { Schema } from "mongoose";
 var tokenSchema = new Schema({
   userId: { type: String, required: true },
   token: { type: String, required: false, default: "" },
+  lastUsed: { type: Date, required: false },
   tokenHasBeenRemoved: { type: Boolean, default: false, required: true },
   schemaVersion: { type: Number },
 });
