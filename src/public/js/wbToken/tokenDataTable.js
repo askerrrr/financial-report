99250248253dbdca195023219f081fedf989c71c
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-28 13:30:20 +0300

    added function parseLastUsedDate, used to lastUsed property

--- src/public/js/wbToken/tokenDataTable.js
+++ src/public/js/wbToken/tokenDataTable.js
@@ -7,10 +7,34 @@ var tokenIsExpiredTdElement = document.getElementById("token-is-expired");
 var yes = "да";
 var no = "нет";
 
+var parseLastUsedData = (lastUsedDate) => {
+  if (!lastUsedDate) {
+    return "-";
+  }
+
+  var date = new Date();
+
+  var currentDay = date.getDate();
+  var currentYear = date.getFullYear();
+  var currentMonth = date.getMonth() + 1;
+
+  console.log({ currentMonth, currentYear, currentDay });
+
+  var splitedDate = lastUsedDate.split("T");
+  var [year, month, day] = splitedDate[0].split("-");
+  var hourAndMinAndSec = splitedDate[1].split(".")[0];
+
+  if (year == currentYear && month == currentMonth && day == currentDay) {
+    return hourAndMinAndSec + " " + "сегодня";
+  } else {
+    return hourAndMinAndSec + " " + day + "." + month + "." + year;
+  }
+};
+
 var insertDataToTokenDataTable = (data) => {
   tokenIdTdElement.textContent = data.id;
   tokenDaysLeftTdElement.textContent = data.daysLeft;
-  tokenLastusedTdElement.textContent = "-";
+  tokenLastusedTdElement.textContent = parseLastUsedData(data.lastUsed);
   tokenValidUntilTdElement.textContent = data.validUntil;
   tokenIsExpiredTdElement.textContent = data.isExpired ? yes : no;
 };
