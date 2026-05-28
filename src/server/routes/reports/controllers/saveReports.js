b360573fd6b2c7c8d6bd74b25a453d80347754b5
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-28 13:03:05 +0300

    Added the maxTimeMS property equal to 150 seconds, and used the SessionOptions object as the session setting.

--- src/server/routes/reports/controllers/saveReports.js
+++ src/server/routes/reports/controllers/saveReports.js
@@ -1,13 +1,13 @@
 import { dbClient } from "../../../database/index.js";
 import reportsProcessing from "../services/different/reportsProcessing.js";
 
-var sessionOptions = {};
+var sessionOptions = { maxTimeMs: 150_000 };
 
 var saveReports = async (req, res, next) => {
   var { dateTo, dateFrom, userId } = req.body;
 
   try {
-    var session = await dbClient.startSession();
+    var session = await dbClient.startSession(sessionOptions);
     await session.withTransaction(async () => {
       var reportData = await reportsProcessing(userId, dateFrom, dateTo, session);
       return res.json(reportData);
