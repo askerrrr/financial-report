860cf9a2df98331623e49708b806a59f8ef682f0
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-28 11:50:57 +0300

    For the download button, I added a check for the presence of a modal window, if there is none, then we create a modal window.

--- src/public/js/wbToken/loadTokenButtonHander.js
+++ src/public/js/wbToken/loadTokenButtonHander.js
@@ -2,6 +2,14 @@ import openTokenLoaderModal from "./openTokenLoaderModal.js";
 
 var loadTokenButton = document.getElementById("token-button");
 
-var loadTokenButtonHander = (userId) => (loadTokenButton.onclick = () => openTokenLoaderModal(userId));
+var loadTokenButtonHander = (userId) => {
+  loadTokenButton.onclick = () => {
+    var tokenModalIsExist = document.getElementById("token-modal");
+
+    if (!tokenModalIsExist) {
+      openTokenLoaderModal(userId);
+    }
+  };
+};
 
 export default loadTokenButtonHander;
