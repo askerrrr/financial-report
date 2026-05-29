b301772bf71488bc2ea8d7bfa5e41ab165a9941a
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-29 13:17:25 +0300

    Added a new database service updateReportLoadingStoppedStatus

--- src/server/database/collections/reportLoadingStates/index.js
+++ src/server/database/collections/reportLoadingStates/index.js
@@ -5,6 +5,7 @@ import prependToReportsQueue from "./services/prependToReportsQueue.js";
 import getLoadingProgressStatus from "./services/getLoadingProgressStatus.js";
 import deleteReportLoadingStates from "./services/deleteReportLoadingStates.js";
 import setLastReportRequestTimestamp from "./services/setLastReportRequestTimestamp.js";
+import updateReportLoadingStoppedStatus from "./services/updateReportLoadingStoppedStatus.js";
 
 var reportLoadingStatesCollectionServices = {
   getReportLoadingState: (userId, session) => getReportLoadingState(reportLoadingStatesCollection, userId, session),
@@ -12,6 +13,9 @@ var reportLoadingStatesCollectionServices = {
   deleteReportLoadingStates: (userId, session) => deleteReportLoadingStates(reportLoadingStatesCollection, userId, session),
   prependToReportsQueue: (userId, dateFrom, dateTo) => prependToReportsQueue(reportLoadingStatesCollection, userId, dateFrom, dateTo),
   setLastReportRequestTimestamp: (userId, session) => setLastReportRequestTimestamp(reportLoadingStatesCollection, userId, session),
+
+  updateReportLoadingStoppedStatus: (userId, newStatus, session) =>
+    updateReportLoadingStoppedStatus(reportLoadingStatesCollection, userId, newStatus, session),
 };
 
 export default reportLoadingStatesCollectionServices;
