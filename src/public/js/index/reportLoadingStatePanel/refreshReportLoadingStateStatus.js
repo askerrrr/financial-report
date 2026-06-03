deab765622c970dbc5d61483d155789e275a1f5a
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-03 11:45:02 +0300

    In the refreshReportLoadingStateStatus function, I implemented overwriting tables with fresh data.

--- src/public/js/index/reportLoadingStatePanel/refreshReportLoadingStateStatus.js
+++ src/public/js/index/reportLoadingStatePanel/refreshReportLoadingStateStatus.js
@@ -1,15 +1,29 @@
-var NEXT_REQUEST_DELAY_MS = 90_000;
+import insertDataToTable from "./insertDataToTable.js";
+
+var NEXT_REQUEST_DELAY_MS = 5_000;
 var nextRequestDelay = async () => new Promise((res) => setTimeout(res, NEXT_REQUEST_DELAY_MS));
 
+var reportsQueueTbodyId = "reports-queue-tbody";
+var abandonedReportsTbodyId = "abandoned-reports-tbody";
+
 var refreshReportLoadingStateStatus = async (url) => {
+  var reportsQueueTbody = document.getElementById(reportsQueueTbodyId);
+  var abandonedReportsTbody = document.getElementById(abandonedReportsTbodyId);
+
   while (true) {
     await nextRequestDelay();
 
     var res = await fetch(url);
 
-    var data = await res.json();
+    var { reportsQueue, abandonedReports, loadingInProgress } = await res.json();
 
-    if (!data.loadingInProgress) {
+    reportsQueueTbody.innerHTML = "";
+    abandonedReportsTbody.innerHTML = "";
+
+    insertDataToTable(reportsQueue, reportsQueueTbodyId);
+    insertDataToTable(abandonedReports, abandonedReportsTbodyId);
+
+    if (!loadingInProgress) {
       break;
     }
   }
