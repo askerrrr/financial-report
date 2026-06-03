1d59872982994027c307159107b4743a530033e5
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-03 15:30:02 +0300

    implemented db service pushToReportsQueue and function sendResumeAbandonedReportsLoadingRequest into resumeAbandonedReportsLoading controller

--- src/server/routes/reports/controllers/resumeAbandonedReportsLoading.js
+++ src/server/routes/reports/controllers/resumeAbandonedReportsLoading.js
@@ -1,3 +1,14 @@
-var resumeAbandonedReportsLoading = async (req, res) => {};
+import reportLoadingStateCollectionServices from "../../../database/collections/reportLoadingStates/index.js";
+import sendResumeAbandonedReportsLoadingRequest from "../services/different/sendResumeAbandonedReportsLoadingRequest.js";
+
+var resumeAbandonedReportsLoading = async (req, res) => {
+  var { userId, abandonedReportPeriods } = req.body;
+
+  await reportLoadingStateCollectionServices.pushToReportsQueue(userId, abandonedReportPeriods);
+
+  var success = await sendResumeAbandonedReportsLoadingRequest(req.body);
+
+  return res.sendStatus(200);
+};
 
 export default resumeAbandonedReportsLoading;
