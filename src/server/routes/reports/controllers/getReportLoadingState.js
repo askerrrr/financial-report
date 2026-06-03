8d715f3ecdae4d92398db50e20f471a6cedae3cf
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-03 10:03:55 +0300

    Added logic to the getReportLoadingState controller for receiving the status of loading reports and sending them to the client

--- src/server/routes/reports/controllers/getReportLoadingState.js
+++ src/server/routes/reports/controllers/getReportLoadingState.js
@@ -1,3 +1,11 @@
-var getReportLoadingState = async (req, res, next) => {};
+import reportLoadingStatesCollectionServices from "../../../database/collections/reportLoadingStates/index.js";
+
+var getReportLoadingState = async (req, res, next) => {
+  var { userId } = req.params;
+  var { reportsQueue, abandonedReports, loadingInProgress, isReportLoadingDelayed } =
+    await reportLoadingStatesCollectionServices.getReportLoadingState(userId);
+
+  return res.json({ reportsQueue, abandonedReports, loadingInProgress, isReportLoadingDelayed });
+};
 
 export default getReportLoadingState;
