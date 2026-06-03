aa38eb9fda4d8abaebbae324fd2788263484f852
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-03 12:40:47 +0300

    Implemented the enableParentReportLoadingStatePanel function in the reportLoadingStatePanelBuilder function

--- src/public/js/index/reportLoadingStatePanel/index.js
+++ src/public/js/index/reportLoadingStatePanel/index.js
@@ -1,9 +1,11 @@
 import insertDataToTable from "./insertDataToTable.js";
 import showReportLoadingStatePanel from "./showReportLoadingStatePanel.js";
 import refreshReportLoadingStateStatus from "./refreshReportLoadingStateStatus.js";
+import { enableParentReportLoadingStatePanel } from "./toggleVisibilityOfParentReportLoadingStatePanel.js";
 
 var reportLoadingStatePanelBuilder = async (userId, url, { reportsQueue, abandonedReports, loadingInProgress, isReportLoadingDelayed }) => {
   if (loadingInProgress || isReportLoadingDelayed) {
+    enableParentReportLoadingStatePanel()
     await showReportLoadingStatePanel();
 
     var reportsQueueTbodyId = "reports-queue-tbody";
