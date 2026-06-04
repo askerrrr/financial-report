0d2b6c38e4e35e600f236b982e4be028bcc40253
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-04 13:42:59 +0300

    For the sendResumeAbandonedReportsLoadingRequest function, I left only the userId argument.

--- src/server/routes/reports/controllers/resumeAbandonedReportsLoading.js
+++ src/server/routes/reports/controllers/resumeAbandonedReportsLoading.js
@@ -1,15 +1,18 @@
 import reportLoadingStateCollectionServices from "../../../database/collections/reportLoadingStates/index.js";
 import sendResumeAbandonedReportsLoadingRequest from "../services/different/sendResumeAbandonedReportsLoadingRequest.js";
 
+var session = null;
+var needToResetAbandonedReports = true;
+
 var resumeAbandonedReportsLoading = async (req, res) => {
   var { userId, abandonedReports, needToResumeLoading } = req.body;
 
   var success;
 
   if (needToResumeLoading) {
-    await reportLoadingStateCollectionServices.pushToReportsQueue(userId, abandonedReports);
+    await reportLoadingStateCollectionServices.pushToReportsQueue(userId, abandonedReports, session, needToResetAbandonedReports);
 
-    success = await sendResumeAbandonedReportsLoadingRequest(req.body);
+    success = await sendResumeAbandonedReportsLoadingRequest(userId);
   } else {
     success = (await reportLoadingStateCollectionServices.resetAbandonedReports(userId)).modifiedCount;
   }
--- src/server/routes/reports/services/different/sendResumeAbandonedReportsLoadingRequest.js
+++ src/server/routes/reports/services/different/sendResumeAbandonedReportsLoadingRequest.js
@@ -4,9 +4,9 @@ var NEXT_REQUEST_INTERVAL_MS = 5000;
 var url = process.env.REPORT_LOADER_URL_TO_RESUME_ABANDONED_LOADING;
 var nextRequestDelay = async () => new Promise((res) => setTimeout(res, NEXT_REQUEST_INTERVAL_MS));
 
-var doRequest = async (data) => await fetch(url, { method: "POST", body: JSON.stringify(data), headers: { "Content-Type": "application/json" } });
+var doRequest = async (userId) => await fetch(url, { method: "POST", body: JSON.stringify(userId), headers: { "Content-Type": "application/json" } });
 
-var sendResumeAbandonedReportsLoadingRequest = async (data) => {
+var sendResumeAbandonedReportsLoadingRequest = async (userId) => {
   var attempt = 0;
   var success = false;
 
