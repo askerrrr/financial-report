a750ebe9422ddcac265b25ce584ebe46df6d7f49
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-04 14:15:28 +0300

    Fixed the pushToReportsQueue database service:

--- src/server/database/collections/reportLoadingStates/services/pushToReportsQueue.js
+++ src/server/database/collections/reportLoadingStates/services/pushToReportsQueue.js
@@ -2,7 +2,11 @@ var pushToReportsQueue = async (collection, userId, periods, session, needToRese
   var sessionOpt = session ? { session } : {};
 
   if (needToResetAbandonedReports) {
-    await collection.updateOne({ userId }, { $push: { reportsQueue: { $each: [...periods] }, abandonedReports: [] } }, { session: session });
+    await collection.updateOne(
+      { userId },
+      { $push: { reportsQueue: { $each: [...periods] }, $set: { abandonedReports: [] } } },
+      { session: session },
+    );
   } else {
     await collection.updateOne({ userId }, { $push: { reportsQueue: { $each: [...periods] } } }, { session: session });
   }
