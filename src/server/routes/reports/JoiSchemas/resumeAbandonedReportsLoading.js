6741869910468e61b8cf41b507dfe2815f621669
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-04 11:08:42 +0300

    Updated the abandonedReportPeriodsSchema schema, now the array can be empty

--- src/server/routes/reports/JoiSchemas/resumeAbandonedReportsLoading.js
+++ src/server/routes/reports/JoiSchemas/resumeAbandonedReportsLoading.js
@@ -1,11 +1,13 @@
 import Joi from "joi";
 
-var abandonedReportPeriodsSchema = Joi.array().items({
-  index: Joi.number().required(),
-  dateTo: Joi.string().required(),
-  dateFrom: Joi.string().required(),
-  failedCount: Joi.number().required(),
-});
+var abandonedReportPeriodsSchema = Joi.array()
+  .items({
+    index: Joi.number().required(),
+    dateTo: Joi.string().required(),
+    dateFrom: Joi.string().required(),
+    failedCount: Joi.number().required(),
+  })
+  .min(0);
 
 var schema = Joi.object({
   userId: Joi.string().required(),
