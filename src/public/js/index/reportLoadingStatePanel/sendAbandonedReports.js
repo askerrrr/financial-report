5765babe4cc86e7e3305f3f38f7137364ab5cd47
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-04 11:17:59 +0300

    updated sendAbandonedReportsfunction: wrap request to try/catch block

--- src/public/js/index/reportLoadingStatePanel/sendAbandonedReports.js
+++ src/public/js/index/reportLoadingStatePanel/sendAbandonedReports.js
@@ -1,13 +1,18 @@
 var url = "/reports/loading-state/abandoned/";
 
 var sendAbandonedReports = async (userId, abandonedReportPeriods, needToResumeLoading) => {
-  var res = await fetch(url, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ userId, abandonedReportPeriods, needToResumeLoading }),
-  });
+  try {
+    var res = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ userId, abandonedReportPeriods, needToResumeLoading }),
+    });
 
-  return res.status;
+    return res.status === 200;
+  } catch (e) {
+    console.log({ errMsg: e.message });
+    return false;
+  }
 };
 
 export default sendAbandonedReports;
