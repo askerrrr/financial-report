8d9a0869448d8975e4d6931c8e3c8a596f7a4999
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-05 10:18:34 +0300

    added the lastLoadedReportSchema subcircuit for the new lastLoadedReport property of the reportLoadingStatesSchema schema. The new property will be used in the future to display information that will be received by the loader.

--- src/server/database/schemas/reportLoadingStates.js
+++ src/server/database/schemas/reportLoadingStates.js
@@ -1,5 +1,17 @@
 import { Schema } from "mongoose";
 
+var lastLoadedReportSchema = new Schema(
+  {
+    periodIndex: { type: Number },
+    reportId: { type: Number, required: true },
+    totalTaxAmount: { type: Number, default: 0, required: true },
+    totalFinalProfit: { type: Number, default: 0, required: true },
+    totalProductCosts: { type: Number, default: 0, required: true },
+    isFinancesAccounted: { type: Boolean, default: false, required: true },
+  },
+  { _id: false },
+);
+
 var reportLoadingStatesSchema = new Schema({
   userId: { type: String, required: true },
   reportsQueue: { type: Array, required: false },
@@ -7,6 +19,7 @@ var reportLoadingStatesSchema = new Schema({
   abandonedReports: { type: Array, required: false },
   lastReportRequestTimestamp: { type: Number, default: 0 },
   freshReportPeriodIndex: { type: Number, required: false },
+  lastLoadedReport: { type: lastLoadedReportSchema, required: false },
   isReportLoadingDelayed: { type: Boolean, requred: true, default: false },
   isReportLoadingisStopped: { type: Boolean, requred: true, default: false },
 });
