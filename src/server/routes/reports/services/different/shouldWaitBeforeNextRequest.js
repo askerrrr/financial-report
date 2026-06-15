9e2181c28a71a3ccf0bc6ef863fa5b302893a76f
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-15 14:22:39 +0300

    updated function shouldWaitBeforeNextRequest

--- src/server/routes/reports/services/different/shouldWaitBeforeNextRequest.js
+++ src/server/routes/reports/services/different/shouldWaitBeforeNextRequest.js
@@ -1,4 +1,5 @@
-var oneMinuteMs = 60 * 1000;
+var oneMinuteMs = 60_000;
+var mskTimeOffsetInMs = 3 * 60 * 60 * 1000;
 
 var shouldWaitBeforeNextRequest = (lastReportRequestTimestamp) => {
   var nextRequestDelayMs = 0;
@@ -7,7 +8,7 @@ var shouldWaitBeforeNextRequest = (lastReportRequestTimestamp) => {
     return { nextRequestDelayMs };
   }
 
-  var currentTimeMs = new Date().getTime();
+  var currentTimeMs = Date.now() + mskTimeOffsetInMs;
   var difference = currentTimeMs - lastReportRequestTimestamp;
   var hasMinutePassed = difference > oneMinuteMs;
 
