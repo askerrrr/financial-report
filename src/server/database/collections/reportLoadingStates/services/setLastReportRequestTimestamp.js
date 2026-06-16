7ef4832fede5ff3ba317a073c49b933007724024
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-16 15:06:40 +0300

    Correction of the setLastReportRequestTimestamp db method: - now the time is recorded taking into account the Moscow offset

--- src/server/database/collections/reportLoadingStates/services/setLastReportRequestTimestamp.js
+++ src/server/database/collections/reportLoadingStates/services/setLastReportRequestTimestamp.js
@@ -1,4 +1,6 @@
+var mskTimeOffsetInMs = 3 * 60 * 60 * 1000;
+
 var setLastReportRequestTimestamp = async (collection, userId, session) =>
-  await collection.updateOne({ userId }, { $set: { lastReportRequestTimestamp: new Date().getTime() } }, { session: session });
+  await collection.updateOne({ userId }, { $set: { lastReportRequestTimestamp: Date.now() + mskTimeOffsetInMs } }, { session: session });
 
 export default setLastReportRequestTimestamp;
