312685e413a4db50206e8a81466bf50e4f4486f0
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-17 15:10:37 +0300

    updated db method resetUserData, implemented updated db method into resetUserData controller

--- src/server/database/collections/users/services/resetUserData.js
+++ src/server/database/collections/users/services/resetUserData.js
@@ -1,11 +1,39 @@
 import { dbClient } from "../../../index.js";
+import {
+  goodsCollection,
+  reportCollection,
+  taxParamsCollection,
+  reportsTreeCollection,
+  reportLoadingStatesCollection,
+  weeklyPricesAndDiscountsCollection,
+} from "../../../connections/index.js";
+
+var defaultReportLoadingState = {
+  queueLength: 0,
+  queueCapacity: 0,
+  loadingInProgress: false,
+  lastReportRequestTimestamp: 0,
+  isReportLoadingDelayed: false,
+  isReportLoadingIsStopped: false,
+  loadingStopReason: "",
+  emptyReportPeriodsIndexes: [],
+  reportsQueue: [],
+  abandonedReports: [],
+};
 
 var resetUserData = async (userId) => {
   var success = true;
   var session = await dbClient.startSession();
 
   try {
-    await session.withTransaction(async () => {});
+    await session.withTransaction(async () => {
+      await reportCollection.updateOne({ userId }, { $set: { reports: [] } }, { session });
+      await goodsCollection.updateOne({ userId }, { $set: { listGoods: [] } }, { session });
+      await taxParamsCollection.updateOne({ userId }, { $set: { years: [] } }, { session });
+      await reportsTreeCollection.updateOne({ userId }, { $set: { years: [] } }, { session });
+      await weeklyPricesAndDiscountsCollection.updateOne({ userId }, { $set: { weeklyPricesAndDiscounts: [] } });
+      await reportLoadingStatesCollection.updateOne({ userId }, { $set: { ...defaultReportLoadingState } }, { session });
+    });
   } catch {
     success = false;
   } finally {
--- src/server/routes/personalAccount/controllers/resetUserData.js
+++ src/server/routes/personalAccount/controllers/resetUserData.js
@@ -1,3 +1,9 @@
-var resetUserData = async (req, res) => {};
+import dbUtils from "../../../database/collections/index.js";
+
+var resetUserData = async (req, res) => {
+  var { success } = await dbUtils.userCollectionServices.resetUserData(req.body.userId);
+
+  return success ? res.sendStatus(200) : res.sendStatus(304);
+};
 
 export default resetUserData;
