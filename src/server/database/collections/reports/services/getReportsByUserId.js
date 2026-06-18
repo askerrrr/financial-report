4641de8d3a66ee2bf52098d3411622b2602fa1bc
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-18 10:47:01 +0300

    Updated getReportsByUserId db method: - removed block try\catch - renamed argument: projectQueries  => selectedFields

--- src/server/database/collections/reports/services/getReportsByUserId.js
+++ src/server/database/collections/reports/services/getReportsByUserId.js
@@ -1,53 +1,48 @@
-import { DatabaseError } from "../../../../customError/index.js";
-var getReportsByUserId = async (collection, userId, session, projectQueries, reportIds) => {
-  try {
-    if (reportIds) {
-      var projectFields = {};
+var getReportsByUserId = async (collection, userId, session, selectedFields, reportIds) => {
+  if (reportIds) {
+    var projectFields = {};
 
-      projectQueries.map((field) => {
-        var key = field.split(".")[1];
-        projectFields[key] = "$$r." + key;
-      });
+    selectedFields.map((field) => {
+      var key = field.split(".")[1];
+      projectFields[key] = "$$r." + key;
+    });
 
-      var data = await collection.aggregate([
-        {
-          $match: {
-            userId,
-            "reports.reportId": { $in: reportIds },
-          },
+    var data = await collection.aggregate([
+      {
+        $match: {
+          userId,
+          "reports.reportId": { $in: reportIds },
         },
-        {
-          $project: {
-            reports: {
-              $map: {
-                input: {
-                  $filter: {
-                    input: "$reports",
-                    cond: { $in: ["$$this.reportId", reportIds] },
-                  },
+      },
+      {
+        $project: {
+          reports: {
+            $map: {
+              input: {
+                $filter: {
+                  input: "$reports",
+                  cond: { $in: ["$$this.reportId", reportIds] },
                 },
-                as: "r",
-                in: projectFields,
               },
+              as: "r",
+              in: projectFields,
             },
           },
         },
-      ]);
+      },
+    ]);
 
-      return { reports: data[0].reports };
-    }
-
-    if (projectQueries) {
-      var { reports } = await collection.findOne({ userId }).select(projectQueries);
-
-      return { reports };
-    }
-
-    var data = await collection.findOne({ userId }, null, { session, session });
-
-    return { reports: data.toObject().reports };
-  } catch (e) {
-    throw new DatabaseError(userId, e);
+    return { reports: data[0].reports };
   }
+
+  if (selectedFields) {
+    var { reports } = await collection.findOne({ userId }).select(selectedFields);
+
+    return { reports };
+  }
+
+  var data = await collection.findOne({ userId }, null, { session, session });
+
+  return { reports: data.toObject().reports };
 };
 export default getReportsByUserId;
