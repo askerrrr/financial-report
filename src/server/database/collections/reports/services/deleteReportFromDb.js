1770d7e20c2dab4c32ced8452966d3160872ba06
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-18 16:29:02 +0300

    Updated the deleteReportFromDb database service: - now, instead of using two methods, I use one findOneAndUpdate

--- src/server/database/collections/reports/services/deleteReportFromDb.js
+++ src/server/database/collections/reports/services/deleteReportFromDb.js
@@ -1,13 +1,7 @@
-import { DatabaseError } from "../../../../customError/index.js";
 var deleteReportFromDb = async (collection, userId, reportId, session) => {
-  try {
-    var doc = await collection.findOne({ userId, "reports.reportId": reportId }, { "reports.$": 1 }, { session: session });
-    await collection.updateOne({ userId, "reports.reportId": reportId }, { $pull: { reports: { reportId } } }, { session: session });
-    var report = doc.reports[0].toObject();
-    return report;
-  } catch (e) {
-    throw new DatabaseError(userId, e);
-  }
+  var doc = await collection.findOneAndUpdate({ userId }, { $pull: { reports: { reportId } } }, { returnDocument: "before", session: session });
+
+  return { reportBeforeDeletion: doc.reports[0].toObject() };
 };
 
 export default deleteReportFromDb;
