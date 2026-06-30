ccb0ec664b210ef7f649e812343b472c2fa36112
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-30 13:50:46 +0300

    added file/function createCheckboxForParticipationInPromo

---
+++ src/public/js/goods/utils/createCheckboxForParticipationInPromo.js
@@ -0,0 +1,35 @@
+var yes = "да";
+var no = "нет";
+
+var createCheckboxForParticipationInPromo = (skuName, changePriceIfInPromo, needWrapIntoFieldset = false) => {
+  var input = document.createElement("input");
+  input.type = "checkbox";
+  input.id = "checkbox-" + skuName;
+
+  var label = document.createElement("label");
+  label.htmlFor = "checkbox-" + skuName;
+  label.className = skuName;
+  label.textContent = changePriceIfInPromo ? yes : no;
+
+  input.addEventListener("click", async (e) => {
+    var newtextContent = input.checked ? yes : no;
+    var labels = document.querySelectorAll("." + skuName);
+    labels.forEach((label) => (label.textContent = newtextContent));
+  });
+
+  var div = document.createElement("div");
+  div.append(input, label);
+
+  if (needWrapIntoFieldset) {
+    var legend = document.createElement("legend");
+    legend.append("Изменить цену если в акции");
+
+    var fieldset = document.createElement("fieldset");
+    fieldset.append(legend, div);
+    return fieldset;
+  } else {
+    return div;
+  }
+};
+
+export default createCheckboxForParticipationInPromo;
