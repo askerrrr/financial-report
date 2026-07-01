80e60d386f3734605b9b6b3f8493b4efc1c251b7
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-07-01 15:51:06 +0300

    Updated the deleteReportFromDb database method: - now, when deleting a report, information about it is deleted from the reportsWithAccountedFinances array

--- src/server/database/collections/reports/services/deleteReportFromDb.js
+++ src/server/database/collections/reports/services/deleteReportFromDb.js
@@ -1,5 +1,9 @@
 var deleteReportFromDb = async (collection, userId, reportId, session) => {
-  var doc = await collection.findOneAndUpdate({ userId }, { $pull: { reports: { reportId } } }, { returnDocument: "before", session: session });
+  var doc = await collection.findOneAndUpdate(
+    { userId },
+    { $pull: { reports: { reportId } }, $pull: { reportsWithAccountedFinances: { reportId } } },
+    { returnDocument: "before", session: session },
+  );
 
   return { reportBeforeDeletion: doc.reports[0].toObject() };
 };
