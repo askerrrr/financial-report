2fbaf6c89cde1322095a82f9789c8c207adf271a
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-07-03 12:23:11 +0300

    added controller getAdminMainPageData

--- src/server/routes/admin/index.js
+++ src/server/routes/admin/index.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import getAdminMainPage from "./controllers/getAdminMainPage.js";
 import getAdminAuthPage from "./controllers/getAdminAuthPage.js";
 import checkAuthAdminData from "./controllers/checkAuthAdminData.js";
+import getAdminMainPageData from "./controllers/getAdminMainPageData.js";
 
 var router = Router({ caseSensitive: true, strict: true });
 
 router.get("/", getAdminMainPage);
+router.get("/api", getAdminMainPageData);
 router.get("/auth", getAdminAuthPage);
 router.post("/", checkAuthAdminData);
 
