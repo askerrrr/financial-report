759639ec8f449f4c25c2f09de7b6000884abe53d
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-07-03 12:41:27 +0300

    For the users collection, I added an array of valid enumerated values for the role field.

--- src/server/database/schemas/users.js
+++ src/server/database/schemas/users.js
@@ -5,7 +5,7 @@ var usersSchema = new Schema({
   login: { type: String, required: true },
   passwd: { type: String, required: true },
   registeredAt: { type: Date, required: true },
-  role: { type: String, required: true, default: "user" },
+  role: { type: String, required: true, default: "user", enum: ["user", "admin"] },
   schemaVersion: { type: Number },
 });
 
