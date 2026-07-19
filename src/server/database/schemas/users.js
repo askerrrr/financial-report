7160e1bf6fe8bb4560dc4c62d8b9ead8caf771a4
AskerSultanov <asker_sultanov1999@mail.ru>
2026-07-19 12:18:43 +0300

    Implemented encryption of database fields using the "queryableEncryption" type

--- src/server/database/schemas/users.js
+++ src/server/database/schemas/users.js
@@ -1,12 +1,16 @@
 import { Schema } from "mongoose";
+import { dataKeyId } from "../keyManager.js";
 
-var usersSchema = new Schema({
-  userId: { type: String, required: true },
-  login: { type: String, required: true },
-  passwd: { type: String, required: true },
-  registeredAt: { type: Date, required: true },
-  role: { type: String, required: true, default: "user", enum: ["user", "admin"] },
-  schemaVersion: { type: Number },
-});
+var usersSchema = new Schema(
+  {
+    schemaVersion: { type: Number },
+    login: { type: String, required: true },
+    userId: { type: String, required: true },
+    registeredAt: { type: Date, required: true },
+    role: { type: String, required: true, default: "user", enum: ["user", "admin"] },
+    passwd: { type: String, required: true, encrypt: { keyId: [dataKeyId], algorithm: "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic" } },
+  },
+  { encryptionType: "queryableEncryption" },
+);
 
 export default usersSchema;
