c3e10f322b6fb9c78766bf16c83b95d947ed82f5
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-07-21 14:28:02 +0300

    Now the fields that need encryption will be located in the file. encryptedFieldsSchemaMap.js

--- src/server/database/schemas/token.js
+++ src/server/database/schemas/token.js
@@ -1,15 +1,12 @@
 import { Schema } from "mongoose";
 import { dataKeyId } from "../keyManager.js";
 
-var tokenSchema = new Schema(
-  {
-    userId: { type: String, required: true },
-    lastUsed: { type: Date, required: false },
-    tokenHasBeenRemoved: { type: Boolean, default: false, required: true },
-    schemaVersion: { type: Number },
-    token: { type: String, required: false, default: "", encrypt: { keyId: [dataKeyId], algorithm: "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic" } },
-  },
-  { encryptionType: "queryableEncryption" },
-);
+var tokenSchema = new Schema({
+  userId: { type: String, required: true },
+  lastUsed: { type: Date, required: false },
+  token: { type: String, required: false, default: "" },
+  tokenHasBeenRemoved: { type: Boolean, default: false, required: true },
+  schemaVersion: { type: Number },
+});
 
 export default tokenSchema;
