1bc9223578ec3b38d6bb116c58623b1b2e882c1f
AskerSultanov <asker_sultanov1999@mail.ru>
2026-08-06 20:33:04 +0300

    updated joi schema to report deletion router: allow empty array for empty report deletion

--- src/server/routes/reports/JoiSchemas/deleteReport.js
+++ src/server/routes/reports/JoiSchemas/deleteReport.js
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
-var skuNamesArraySchema = Joi.array().items(Joi.string().required());
+var skuNamesArraySchema = Joi.array().items(Joi.string().required()).empty(Joi.array().length(0 ));
 
 var schema = Joi.object({ userId: Joi.string().required(), reportId: Joi.number().required(), skuNames: skuNamesArraySchema });
 
