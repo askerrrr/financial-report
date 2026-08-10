92eea7672f5b3cb63cbc28fe62ceec0bf90ce152
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-08-10 12:29:04 +0300

    Added the getEmptyReportPeriods database method to get empty accounting periods and implemented it in the checkReportExists controller

--- src/server/database/collections/reportLoadingStates/index.js
+++ src/server/database/collections/reportLoadingStates/index.js
@@ -4,12 +4,14 @@ import pushToReportsQueue from "./services/pushToReportsQueue.js";
 import getReportLoadingState from "./services/getReportLoadingState.js";
 import resetAbandonedReports from "./services/resetAbandonedReports.js";
 import prependToReportsQueue from "./services/prependToReportsQueue.js";
+import getEmptyReportPeriods from "./services/getEmptyReportPeriods.js";
 import deleteReportLoadingStates from "./services/deleteReportLoadingStates.js";
 import setLastReportRequestTimestamp from "./services/setLastReportRequestTimestamp.js";
 import addReportToEmptyReportPeriods from "./services/addReportToEmptyReportPeriods.js";
 import updateReportLoadingStoppedStatus from "./services/updateReportLoadingStoppedStatus.js";
 
 var reportLoadingStatesCollectionServices = {
+  getEmptyReportPeriods: (userId) => getEmptyReportPeriods(reportLoadingStatesCollection, userId),
   getReportLoadingState: (userId, session, selectedFields) => getReportLoadingState(reportLoadingStatesCollection, userId, session, selectedFields),
   deleteReportLoadingStates: (userId, session) => deleteReportLoadingStates(reportLoadingStatesCollection, userId, session),
   prependToReportsQueue: (userId, dateFrom, dateTo) => prependToReportsQueue(reportLoadingStatesCollection, userId, dateFrom, dateTo),
