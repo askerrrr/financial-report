bcb150096563a1317d89fbb06c21381ef9daead1
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-09-07 18:49:17 +0300

    Updated the createUserToDb database method: - now I dont create a token by default

--- src/server/database/modelsUtil/users/utils/createUserToDb.js
+++ src/server/database/modelsUtil/users/utils/createUserToDb.js
@@ -7,13 +7,23 @@ var createUserToDb = async (user, session) => {
   var registeredAt = new Date();
   var hashedPasswd = await argon2.hash(passwd + "", process.env.SECRET_KEY);
 
-  await models.tokenModel.create([{ userId }], { session: session });
   await models.taxParamModel.create([{ userId }], { session: session });
-  await models.reportLoadingStateModel.create([{ userId }], { session: session });
-  await models.goodsModel.create([{ userId, listGoods: [] }], { session: session });
-  await models.reportPeriodModel.create([{ userId, reportPeriods: [] }], { session: session });
-  await models.weeklyPricesAndDiscountsModel.create([{ userId }], { session: session });
-  await models.userModel.create([{ login, userId, role, registeredAt, passwd: hashedPasswd }], { session: session });
+  await models.reportLoadingStateModel.create([{ userId }], {
+    session: session,
+  });
+  await models.goodsModel.create([{ userId, listGoods: [] }], {
+    session: session,
+  });
+  await models.reportPeriodModel.create([{ userId, reportPeriods: [] }], {
+    session: session,
+  });
+  await models.weeklyPricesAndDiscountsModel.create([{ userId }], {
+    session: session,
+  });
+  await models.userModel.create(
+    [{ login, userId, role, registeredAt, passwd: hashedPasswd }],
+    { session: session },
+  );
 };
 
 export default createUserToDb;
