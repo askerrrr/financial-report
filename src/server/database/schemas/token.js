496dbadbdca0290c911db79a9746a6492ade4ed7
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-09-07 15:27:23 +0300

    An update in the tokenSchema scheme for the tokens collection: - deleted the priceSetToken and reportReadToken fields - added the type field with the enum property: [read, set]

--- src/server/database/schemas/token.js
+++ src/server/database/schemas/token.js
@@ -1,12 +1,11 @@
 import { Schema } from "mongoose";
 
 var tokenSchema = new Schema({
-  userId: { type: String, required: true, unique: true },
+  userId: { type: String, required: true },
   lastUsed: { type: Date, required: false },
   token: { type: String, required: false, default: "" },
-  priceSetToken: { type: String, required: false, default: "" },
-  reportReadToken: { type: String, required: false, default: "" },
   tokenHasBeenRemoved: { type: Boolean, default: false, required: true },
+  type: { type: String, required: false, default: "", enum: ["read", "set"] },
 });
 
 export default tokenSchema;
