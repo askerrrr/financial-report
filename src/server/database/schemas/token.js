c7c994ba6eaa74c96f4fffa21e8ee72886e184fa
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-09-17 14:51:50 +0300

    Designated indexing for the tokens collection: - the index will consist of the value {userId: 1, type: 1}

--- src/server/database/schemas/token.js
+++ src/server/database/schemas/token.js
@@ -2,16 +2,13 @@ import { Schema } from "mongoose";
 
 var tokenSchema = new Schema({
   userId: { type: String, required: true },
-  addedAt: { type: Date, required: false },
+  addedAt: { type: Date, required: true },
   lastUsed: { type: Date, required: false },
-  token: { type: String, required: false, default: "" },
+  token: { type: String, required: true },
+  type: { type: String, required: true, enum: ["read", "set"] },
   tokenHasBeenRemoved: { type: Boolean, default: false, required: true },
-  type: {
-    type: String,
-    required: false,
-    default: "read",
-    enum: ["read", "set"],
-  },
 });
+
+tokenSchema.index({ userId: 1, type: 1 }, { unique: true });
 
 export default tokenSchema;
